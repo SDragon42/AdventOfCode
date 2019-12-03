0f1018fad23a1f12c823d2c824cf85565762aa99
nathen.silver@ruralsourcing.com <nathen.silver@ruralsourcing.com>
2019-12-02 20:55:19 -0700

    Inital commit after starting

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+
+namespace Advent_of_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var tl = asm.GetTypes()
+                .Where(t => typeof(IPuzzle).IsAssignableFrom(t))
+                .Where(t => !t.IsInterface)
+                .Where(t => !t.IsAbstract)
+                .OrderBy(t => t.Name)
+                .ToList();
+
+            foreach (var item in tl)
+            {
+                var puzzle = (IPuzzle)Activator.CreateInstance(item);
+                if (puzzle == null)
+                    continue;
+                Line();
+                puzzle?.Run();
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+
+        static void Line () => Console.WriteLine(string.Empty.PadRight(60, '-'));
+    }
+}
