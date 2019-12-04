7ab7ca3d4de116d15a98227dfb59eb12b111017d
SDragon42 <nathen.silver@gmail.com>
2019-12-03 21:54:13 -0700

    Moved comments.  Added Day 4

---
+++ Day04/Day04Common.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent_of_Code.Day04
+{
+    static class Day04Common
+    {
+        static Day04Common()
+        {
+
+        }
+    }
+}
