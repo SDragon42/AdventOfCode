5ddfebc3710dad202116a9827ac6d2ff53210acc
SDragon42 <nathen.silver@gmail.com>
2019-12-07 00:04:43 -0700

    Added Day 7

---
+++ Day07/Day07Common.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent_of_Code.Day07
+{
+    static class Day07Common
+    {
+        static Day07Common()
+        {
+
+        }
+
+
+    }
+}
---
+++ Day07/Day07Puzzle2.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Advent_of_Code.Day07
+//{
+//    class Day07Puzzle2 : IPuzzle
+//    {
+//        public void Run()
+//        {
+
+//        }
+//    }
+//}
