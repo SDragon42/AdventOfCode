2506023e53609902e3f05606e677d94f22c7a634
nathen.silver@ruralsourcing.com <nathen.silver@ruralsourcing.com>
2019-12-11 10:52:42 -0700

    Finished D7P2

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace Advent_of_Code
                 .ToList();
 
             var typeList = allPuzzleTypes
-                .Where(t => t.Name.StartsWith("Day10Puzzle1"))
+                .Where(t => t.Name.StartsWith("Day07Puzzle2"))
                 //.Where(t => t.Equals(allPuzzleTypes.LastOrDefault())) // Run last puzzle
                 ;
 
