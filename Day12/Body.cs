db2db6c4350a5ba8abd31bd05e5770013684644d
nathen.silver@ruralsourcing.com <nathen.silver@ruralsourcing.com>
2019-12-18 09:41:47 -0700

    separated classes

---
+++ Day12/Body.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent_of_Code.Day12
+{
+    class Body
+    {
+        public Body(int x, int y, int z)
+        {
+            Position = new Point3D(x, y, z);
+            Velocity = new Point3D();
+        }
+
+        public Point3D Position { get; private set; }
+        public Point3D Velocity { get; private set; }
+
+        public int KineticEnergy => Velocity.Energy;
+        public int PotentialEnergy => Position.Energy;
+        public int TotalEnergy => PotentialEnergy * KineticEnergy;
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Body;
+            if (other == null) return false;
+            return Position.Equals(other.Position) && Velocity.Equals(other.Velocity);
+        }
+        public override int GetHashCode()
+        {
+            return Position.GetHashCode() ^ Velocity.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"pos={Position}, vel={Velocity}";
+        }
+
+    }
+
+}
---
+++ Day12/Point3D.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent_of_Code.Day12
+{
+    class Point3D
+    {
+        public Point3D() : this(0, 0, 0) { }
+        public Point3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public int Energy => Math.Abs(X) + Math.Abs(Y) + Math.Abs(Z);
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Point3D;
+            if (other == null) return false;
+            return X.Equals(other.X) && Y.Equals(other.Y) && Z.Equals(other.Z);
+        }
+        public override int GetHashCode()
+        {
+            return X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"<x={X,5}, y={Y,5}, z={Z,5}>";
+        }
+
+    }
+}
