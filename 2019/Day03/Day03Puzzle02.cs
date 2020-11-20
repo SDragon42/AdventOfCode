ac8991468cdd34e2e43e5f041c0c303378aa581d
SDragon42 <nathen.silver@gmail.com>
2020-11-19 19:02:14 -0700

    Moved into 2019 folder

---
+++ 2019/Day03/Day03Puzzle02.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Advent_of_Code.Day03
+{
+    /*
+    It turns out that this circuit is very timing-sensitive; you actually need 
+    to minimize the signal delay.
+
+    To do this, calculate the number of steps each wire takes to reach each 
+    intersection; choose the intersection where the sum of both wires' steps is 
+    lowest. If a wire visits a position on the grid multiple times, use the 
+    steps value from the first time it visits that position when calculating 
+    the total value of a specific intersection.
+
+    The number of steps a wire takes is the total number of grid squares the 
+    wire has entered to get to that location, including the intersection being 
+    considered. Again consider the example from above:
+
+    ...........
+    .+-----+...
+    .|.....|...
+    .|..+--X-+.
+    .|..|..|.|.
+    .|.-X--+.|.
+    .|..|....|.
+    .|.......|.
+    .o-------+.
+    ...........
+    In the above example, the intersection closest to the central port is 
+    reached after 8+5+5+2 = 20 steps by the first wire and 7+6+4+3 = 20 steps 
+    by the second wire for a total of 20+20 = 40 steps.
+
+    However, the top-right intersection is better: the first wire takes only 
+    8+5+2 = 15 and the second wire takes only 7+6+2 = 15, a total of 15+15 = 30 
+    steps.
+
+    Here are the best steps for the extra examples from above:
+
+    - R75,D30,R83,U83,L12,D49,R71,U7,L72
+      U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps
+    - R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+      U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps
+
+    What is the fewest combined steps the wires must take to reach an intersection? 
+    */
+    class Day03Puzzle02 : IPuzzle
+    {
+        public void Run()
+        {
+            Console.WriteLine("--- Day 3: Crossed Wires (Part 2) ---");
+
+            var line1Segments = BuildLineSegments(Day03Common.Line1Commands).ToList();
+            var line2Segments = BuildLineSegments(Day03Common.Line2Commands).ToList();
+
+
+            var intersectionPoints = new List<PointSteps>();
+            var line1Length = 0;
+            foreach (var line1 in line1Segments)
+            {
+                var line2Length = 0;
+                foreach (var line2 in line2Segments)
+                {
+                    var pos = FindIntersection(line1, line2);
+                    if (pos != null)
+                    {
+                        var length = line1Length + line2Length + CalcDistance(line1.P1, pos.Value) + CalcDistance(line2.P1, pos.Value);
+                        var item = new PointSteps(pos.Value, length);
+                        intersectionPoints.Add(item);
+                    }
+
+                    line2Length += CalcDistance(line2.P1, line2.P2);
+                }
+                line1Length += CalcDistance(line1.P1, line1.P2);
+            }
+
+            var minDist = 0;
+            minDist = intersectionPoints
+                .Where(p => p.IntersectPoint != Point.Empty)
+                .Distinct()
+                .Select(p => p.Distance)
+                .Min();
+
+            Console.WriteLine($"Closest distance: {minDist}");
+            if (Day03Common.DesiredWireDistance >= 0)
+                Console.WriteLine("    " + (Day03Common.DesiredWireDistance == minDist ? "CORRECT" : "You done it wrong!"));
+            Console.WriteLine();
+        }
+
+        private int CalcDistance(Point p1, Point p2) => Math.Abs(p1.X - p2.X) + Math.Abs(p1.Y - p2.Y);
+
+        IEnumerable<PointPair> BuildLineSegments(IEnumerable<string> commands)
+        {
+            var startPoint = new Point(0, 0);
+
+            foreach (var cmd in commands)
+            {
+                var dir = cmd[0];
+                var dist = Convert.ToInt32(cmd.Substring(1));
+
+                var endPoint = default(Point);
+                switch (dir)
+                {
+                    case 'U': endPoint = new Point(startPoint.X, startPoint.Y + dist); break;
+                    case 'D': endPoint = new Point(startPoint.X, startPoint.Y - dist); break;
+                    case 'L': endPoint = new Point(startPoint.X - dist, startPoint.Y); break;
+                    case 'R': endPoint = new Point(startPoint.X + dist, startPoint.Y); break;
+                }
+
+                yield return new PointPair(startPoint, endPoint);
+                startPoint = endPoint;
+            }
+
+        }
+
+        private Point? FindIntersection(PointPair line1, PointPair line2)
+        {
+            var delta = (line1.A * line2.B) - (line2.A * line1.B);
+            if (delta == 0)
+                return null;
+
+            var newX = ((line2.B * line1.C) - (line1.B * line2.C)) / delta;
+            var newY = ((line1.A * line2.C) - (line2.A * line1.C)) / delta;
+
+            var intersect = new Point(newX, newY);
+
+            if (!IsOnLine(line1, intersect))
+                return null;
+            if (!IsOnLine(line2, intersect))
+                return null;
+
+            return intersect;
+        }
+
+
+        private bool IsOnLine(PointPair line, Point intersect)
+        {
+            if (IsSame(line.P1.X, line.P2.X, intersect.X))
+                return IsBetween(line.P1.Y, line.P2.Y, intersect.Y);
+            else if (IsSame(line.P1.Y, line.P2.Y, intersect.Y))
+                return IsBetween(line.P1.X, line.P2.X, intersect.X);
+            return false;
+        }
+        private bool IsSame(int a, int b, int value)
+        {
+            return (a == b && b == value);
+        }
+        private bool IsBetween(int a, int b, int value)
+        {
+            return (a <= b)
+                ? (a <= value && value <= b)
+                : (a >= value && value >= b);
+        }
+
+
+
+        class PointPair
+        {
+            public PointPair(Point p1, Point p2)
+            {
+                P1 = p1;
+                P2 = p2;
+
+                A = P2.Y - P1.Y;
+                B = P2.X - P1.X;
+                C = (A * P1.X) + (B * P1.Y);
+            }
+
+            public Point P1 { get; private set; }
+            public Point P2 { get; private set; }
+
+            public int A { get; private set; }
+            public int B { get; private set; }
+            public int C { get; private set; }
+        }
+
+        class PointSteps
+        {
+            public PointSteps(Point intersectPoint, int distance)
+            {
+                IntersectPoint = intersectPoint;
+                Distance = distance;
+            }
+
+            public Point IntersectPoint { get; private set; }
+            public int Distance { get; private set; }
+        }
+
+    }
+}
---
+++ 2019/Day10/Day10Puzzle2.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Advent_of_Code.Day10
+{
+    /*
+    --- Part Two ---
+    Once you give them the coordinates, the Elves quickly deploy an Instant Monitoring Station to
+    the location and discover the worst: there are simply too many asteroids.
+
+    The only solution is complete vaporization by giant laser.
+
+    Fortunately, in addition to an asteroid scanner, the new monitoring station also comes
+    equipped with a giant rotating laser perfect for vaporizing asteroids. The laser starts by
+    pointing up and always rotates clockwise, vaporizing any asteroid it hits.
+
+    If multiple asteroids are exactly in line with the station, the laser only has enough power
+    to vaporize one of them before continuing its rotation. In other words, the same asteroids
+    that can be detected can be vaporized, but if vaporizing one asteroid makes another one
+    detectable, the newly-detected asteroid won't be vaporized until the laser has returned to
+    the same position by rotating a full 360 degrees.
+
+    For example, consider the following map, where the asteroid with the new monitoring station
+    (and laser) is marked X:
+
+    .#....#####...#..
+    ##...##.#####..##
+    ##...#...#.#####.
+    ..#.....X...###..
+    ..#.#.....#....##
+
+    The first nine asteroids to get vaporized, in order, would be:
+
+    .#....###24...#..
+    ##...##.13#67..9#
+    ##...#...5.8####.
+    ..#.....X...###..
+    ..#.#.....#....##
+
+    Note that some asteroids (the ones behind the asteroids marked 1, 5, and 7) won't have a
+    chance to be vaporized until the next full rotation. The laser continues rotating; the next
+    nine to be vaporized are:
+
+    .#....###.....#..
+    ##...##...#.....#
+    ##...#......1234.
+    ..#.....X...5##..
+    ..#.9.....8....76
+
+    The next nine to be vaporized are then:
+
+    .8....###.....#..
+    56...9#...#.....#
+    34...7...........
+    ..2.....X....##..
+    ..1..............
+
+    Finally, the laser completes its first full rotation (1 through 3), a second rotation (4
+    through 8), and vaporizes the last asteroid (9) partway through its third rotation:
+
+    ......234.....6..
+    ......1...5.....7
+    .................
+    ........X....89..
+    .................
+
+    In the large example above (the one with the best monitoring station location at 11,13):
+
+    The 1st asteroid to be vaporized is at 11,12.
+    The 2nd asteroid to be vaporized is at 12,1.
+    The 3rd asteroid to be vaporized is at 12,2.
+    The 10th asteroid to be vaporized is at 12,8.
+    The 20th asteroid to be vaporized is at 16,0.
+    The 50th asteroid to be vaporized is at 16,9.
+    The 100th asteroid to be vaporized is at 10,16.
+    The 199th asteroid to be vaporized is at 9,6.
+    The 200th asteroid to be vaporized is at 8,2.
+    The 201st asteroid to be vaporized is at 10,9.
+    The 299th and final asteroid to be vaporized is at 11,1.
+
+    The Elves are placing bets on which will be the 200th asteroid to be vaporized. Win the bet
+    by determining which asteroid that will be; what do you get if you multiply its X coordinate
+    by 100 and then add its Y coordinate? (For example, 8,2 becomes 802.)
+     */
+    class Day10Puzzle2 : IPuzzle
+    {
+
+
+        public Day10Puzzle2()
+        {
+            string[] rawMapData;
+            var zoneList = new List<MapZone>();
+
+            //rawMapData = Helper.GetFileContentAsLines("D10P2-Test1.txt");
+            //Width = rawMapData[0].Length;
+            //Height = rawMapData.Length;
+            //BuildData(rawMapData, zoneList, ref Origin);
+            //WinningBet = CalcBet(8, 2); // 200th asteroid
+
+            //rawMapData = Helper.GetFileContentAsLines("D10P1-Test5.txt");
+            //Width = rawMapData[0].Length;
+            //Height = rawMapData.Length;
+            //BuildData(rawMapData, zoneList, ref Origin);
+            //if (Origin == null)
+            //    Origin = zoneList.Where(z => z.Location.X == 11 && z.Location.Y == 13).FirstOrDefault();
+            //WinningBet = CalcBet(8, 2); // 200th asteroid
+
+
+            rawMapData = Helper.GetFileContentAsLines(@"Day10\Data.txt");
+            Width = rawMapData[0].Length;
+            Height = rawMapData.Length;
+            BuildData(rawMapData, zoneList, ref Origin);
+            if (Origin == null)
+                Origin = zoneList.Where(z => z.Location.X == 26 && z.Location.Y == 29).FirstOrDefault();
+            WinningBet = 1419;
+
+
+            //BuildDebugData(zoneList, ref Origin);
+
+            Map = zoneList;
+
+        }
+
+        private void BuildData(string[] rawMapData, List<MapZone> zoneList, ref MapZone origin)
+        {
+            for (var y = 0; y < Height; y++)
+            {
+                for (var x = 0; x < Width; x++)
+                {
+                    var hasBase = (rawMapData[y][x] == 'X');
+                    var hasRoid = (rawMapData[y][x] == '#') || hasBase;
+                    if (!hasRoid)
+                        continue;
+                    var zone = new MapZone(x, y);
+                    zoneList.Add(zone);
+
+                    if (hasBase)
+                        origin = zone;
+                }
+            }
+        }
+        private void BuildDebugData(List<MapZone> zoneList, ref MapZone origin)
+        {
+            var x = 0;
+            var y = 0;
+
+            zoneList.Clear();
+            origin = new MapZone(x, y);
+            zoneList.Add(Origin);
+            
+            zoneList.Add(new MapZone(x + 0, y + -2));
+            zoneList.Add(new MapZone(x + 1, y + -2));
+            zoneList.Add(new MapZone(x + 2, y + -2));
+            zoneList.Add(new MapZone(x + 2, y + -1));
+            zoneList.Add(new MapZone(x + 2, y + 0));
+            zoneList.Add(new MapZone(x + 2, y + 1));
+            zoneList.Add(new MapZone(x + 2, y + 2));
+            zoneList.Add(new MapZone(x + 1, y + 2));
+            zoneList.Add(new MapZone(x + 0, y + 2));
+            zoneList.Add(new MapZone(x + -1, y + 2));
+            zoneList.Add(new MapZone(x + -2, y + 2));
+            zoneList.Add(new MapZone(x + -2, y + 1));
+            zoneList.Add(new MapZone(x + -2, y + 0));
+            zoneList.Add(new MapZone(x + -2, y + -1));
+            zoneList.Add(new MapZone(x + -2, y + -2));
+            zoneList.Add(new MapZone(x + -1, y + -2));
+        }
+
+        readonly IReadOnlyList<MapZone> Map;
+        readonly MapZone Origin;
+        readonly int? WinningBet;
+
+        readonly int Width;
+        readonly int Height;
+
+
+        public void Run()
+        {
+            Console.WriteLine("--- Day 10: Monitoring Station (part 2) ---");
+
+            var allAsteroidZones = Map
+                .Where(z => z.HasAsteroid)
+                .Where(z => z != Origin)
+                ;
+            Console.WriteLine($"# Asteroids: {allAsteroidZones.Count()}");
+
+            foreach (var z in allAsteroidZones)
+                z.Angle = Math.Round(CalcAngle(Origin.Location, z.Location), 5);
+            
+            Console.WriteLine(Origin);
+            
+            var numOrder = 0;
+            var foundBet = -1;
+            var toVaporize = allAsteroidZones
+                .OrderBy(z => z.Angle)
+                .ThenBy(z => z.Distance(Origin));
+
+            while (toVaporize.Count() > 0)
+            {
+                double lastAngle = -1.0;
+                foreach (var z in toVaporize)
+                {
+                    if (lastAngle != z.Angle)
+                    {
+                        numOrder++;
+                        z.Vaporize();
+                        Console.WriteLine($"[{numOrder,3}]: {z}");
+                        if (numOrder == 200)
+                            foundBet = CalcBet(z.Location.X, z.Location.Y);
+                    }
+                    lastAngle = z.Angle;
+                }
+            }
+
+            Console.WriteLine($"Winning Bet: {foundBet}");
+            if (foundBet.Equals(WinningBet))
+            {
+                Console.WriteLine("\tCorrect");
+            }
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// https://math.stackexchange.com/a/714423
+        /// </remarks>
+        double CalcAngle(Point p1, Point p2)
+        {
+            var y1 = (p1.Y > p2.Y) ? p1.Y : p2.Y;
+            var y2 = (p1.Y > p2.Y) ? p2.Y : p1.Y;
+
+            var numerator = y1 - y2;
+            var denominator = Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(y1 - y2, 2));
+            
+            var angle = Math.Abs(Math.Acos(numerator / denominator) * (180 / Math.PI));
+
+            var deltaX = p2.X - p1.X;
+            var deltaY = p1.Y - p2.Y;
+
+            angle =
+                  (deltaX >= 0 && deltaY >= 0) ? angle
+                : (deltaX >= 0 && deltaY <= 0) ? (180 - angle)
+                : (deltaX <= 0 && deltaY <= 0) ? (180 + angle)
+                : (360 - angle);
+
+            return angle;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// https://stackoverflow.com/a/17590923/6136
+        /// </remarks>
+        bool CalcIfOnLine(Point a, Point b, Point p)
+        {
+            Func<Point, Point, double> calc = (q, w) => Math.Sqrt(Math.Pow(w.X - q.X, 2) + Math.Pow(w.Y - q.Y, 2));
+
+            var ab = calc(a, b);
+            var ap = calc(a, p);
+            var pb = calc(p, b);
+
+            var apb = ap + pb;
+            ab = Math.Round(ab, 5);
+            apb = Math.Round(apb, 5);
+            var result = ab == apb;
+            return result;
+        }
+
+
+        static int CalcBet(int x, int y) => (x * 100) + y;
+
+        class MapZone
+        {
+            public MapZone(int x, int y)
+            {
+                Location = new Point(x, y);
+                HasAsteroid = true;
+            }
+
+            public Point Location { get; private set; }
+            public bool HasAsteroid { get; private set; }
+            public double Angle { get; set; }
+
+            public void Vaporize()
+            {
+                HasAsteroid = false;
+            }
+
+            public override string ToString()
+            {
+                return $"({Location.X,2},{Location.Y,2}) - {(HasAsteroid ? "#" : ".")}   {Angle}°";
+            }
+
+            public override bool Equals(object obj)
+            {
+                var other = obj as MapZone;
+                if (other == null)
+                    return false;
+                return Location.Equals(other.Location);
+            }
+            public override int GetHashCode()
+            {
+                return Location.GetHashCode();
+            }
+
+            public int Distance(MapZone other) => Distance(other.Location);
+            public int Distance(Point other) => Math.Abs(other.X - Location.X) + Math.Abs(other.Y - Location.Y);
+        }
+    }
+}
