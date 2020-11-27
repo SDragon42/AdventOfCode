49c8e357e4421ccdaf16d52876e2ba2cc8611013
SDragon42 <nathen.silver@gmail.com>
2020-11-27 14:14:48 -0700

    Added in test senarios for day 1 puzzle 1

--- 2019-py/d01p1.py
+++ 2019-py/d01p1.py
@@ -13,7 +13,13 @@ if __name__ == "__main__":
     print()
 
     totalFuel = 0
-    input = get_input_d1()
+
+    # input = [12] # test 1: required fule = 2
+    # input = [14] # test 2: required fule = 2
+    # input = [1969] # test 2: required fule = 654
+    # input = [100756] # test 2: required fule = 33583
+    input = get_input_d1() # correct answer = 3373568
+    
     for x in input:
         totalFuel = totalFuel + calc_fuel(x)
 
