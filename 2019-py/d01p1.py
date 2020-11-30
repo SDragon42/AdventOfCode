98860b1d189ce0b18aeb79af1b131db522946a51
SDragon42 <nathen.silver@gmail.com>
2020-11-30 10:25:06 -0700

    Added correct answers to the input data file

--- 2019-py/d01p1.py
+++ 2019-py/d01p1.py
@@ -1,5 +1,5 @@
 import math
-from inputData import get_input_d1
+from inputData import get_input_d01, get_answer_d01p1
 
 
 def calc_fuel(mass):
@@ -18,9 +18,11 @@ if __name__ == "__main__":
     # input = [14] # test 2: required fule = 2
     # input = [1969] # test 3: required fule = 654
     # input = [100756] # test 4: required fule = 33583
-    input = get_input_d1() # correct answer = 3373568
+    input = get_input_d01() # correct answer = 3373568
     
     for x in input:
         totalFuel = totalFuel + calc_fuel(x)
 
     print(f"Total Fuel needed: {totalFuel}")
+    if get_answer_d01p1() == totalFuel:
+        print("   CORRECT")
