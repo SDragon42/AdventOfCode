d5c5395162b9405e05356219c923787408f41e44
SDragon42 <nathen.silver@gmail.com>
2020-12-01 21:41:51 -0700

    re-worked day 1 puzzle 2 to support n number compares.

---
+++ 2020-py/d01p2-exp.py
@@ -0,0 +1,49 @@
+import utils
+
+
+def get_value(num_splits: int, testInput: list[int], testValues: list[int]) -> int:
+    if num_splits == 0:
+        for x in testInput:
+            sumTotal = x
+            multTotal = x
+            for v in testValues:
+                sumTotal += v
+                multTotal *= v
+            if sumTotal == 2020:
+                return multTotal
+        return -1
+
+    idx = 0
+    while idx < len(testInput) - num_splits:
+        testValues.append(testInput[idx])
+        result = get_value(num_splits - 1, testInput[idx+1:], testValues)
+        if result != -1:
+            return result
+        testValues.pop()
+        idx += 1
+    
+    return -1
+
+
+def run(title: str, input: list[int], correctResult: int):
+    result = get_value(2, input, [])
+    print(f"{title}  --  Result: {result}")
+    if correctResult == result:
+        print("   CORRECT")
+    else:
+        print("   WRONG")
+    print()
+
+
+if __name__ == "__main__":
+    print("Day 1 Puzzle 2 - experiment")
+    print("------------------------------------------------------------")
+    print()
+
+    run("Test case 1",
+        [1721,979,366,299,675,1456],
+        241861950)
+
+    run("problem",
+        utils.read_input_as_int_list(1),
+        230057040)
