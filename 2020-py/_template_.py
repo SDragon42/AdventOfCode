d3ffe74c991dff7c34470b2a6997f5b28ff22ee3
SDragon42 <nathen.silver@gmail.com>
2020-12-01 21:49:42 -0700

    updated the template script

--- 2020-py/_template_.py
+++ 2020-py/_template_.py
@@ -12,9 +12,8 @@ def run(title, input, correctResult):
 
 
 if __name__ == "__main__":
-    print("Day x Puzzle y")
-    print("------------------------------------------------------------")
-    print()
+    utils.show_title(0, 0)
+    # utils.show_title_extended(0, 0, "template")
 
     run("Test Case #",
         utils.read_input_as_int_list(0),
