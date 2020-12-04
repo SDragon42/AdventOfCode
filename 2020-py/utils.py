869a04638005962098ed517b49677e9b83211b4c
SDragon42 <nathen.silver@gmail.com>
2020-12-03 23:38:00 -0700

    Day 4

--- 2020-py/utils.py
+++ 2020-py/utils.py
@@ -32,6 +32,19 @@ def read_input_as_list(day: int):
     return data
 
 
+def read_input_as_list2(filename: str):
+    """ Reads the input file as a list of ints """
+    filename = f'.\\data\\{filename}.txt'
+    data = []
+    with open(filename) as dataFile:
+        while True:
+            line = dataFile.readline()
+            if not line:
+                break
+            data.append(line.strip())
+    return data
+
+
 def show_title(day: int, puzzle: int, title: str):
     """ Shows the Day/Puzzle/title header """
     print("------------------------------------------------------------")
