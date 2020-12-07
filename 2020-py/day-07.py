90f61d4186cf4f7faaa0dc1214d78350830f28bc
SDragon42 <nathen.silver@gmail.com>
2020-12-07 00:19:53 -0700

    Day 7

---
+++ 2020-py/day-07.py
@@ -0,0 +1,110 @@
+import utils
+
+#-------------------------------------------------------------------------------
+
+class BagInfo:
+    bag: str
+    count: int
+
+    def __init__(self, bag, count):
+        self.bag = bag
+        self.count = count
+
+
+class BagDetails:
+    bag: str
+    contents: list[BagInfo]
+
+    def __init__(self, bag):
+        self.bag = bag
+        self.contents = []
+
+#-------------------------------------------------------------------------------
+
+def clean_bag_name(name: str) -> str:
+    name = name.replace(" bags", "")
+    name = name.replace(" bag", "")
+    name = name.strip()
+    return name
+
+
+def parse_rules(input: list[str]) -> dict[str, BagDetails]:
+    rules: dict[str, BagDetails] = {}
+
+    for line in input:
+        lineParts = line.split(" contain ")
+        bag = clean_bag_name(lineParts[0])
+        details = BagDetails(bag)
+
+        subBags = lineParts[1].split(",")
+        for subBag in subBags:
+            subBag = clean_bag_name(subBag.strip(" ,."))
+            if subBag == "no other":
+                continue
+
+            subBagParts = subBag.split(" ", 1)
+            subBagName = subBagParts[1]
+            subBagCount = int(subBagParts[0])
+            info = BagInfo(subBagName, subBagCount)
+            details.contents.append(info)
+
+        rules[details.bag] = details
+
+    return rules
+
+
+def count_bags_containing(rules: dict[str, BagDetails], bag: str, foundBags: list[str]):
+    for ruleKey in rules:
+        for contentBag in rules[ruleKey].contents:
+            if contentBag.bag == bag:
+                found = rules[ruleKey].bag
+                if found not in foundBags:
+                    foundBags.append(found)
+                    count_bags_containing(rules, found, foundBags)
+
+
+def count_bags_in(rules: dict[str, BagDetails], bag: str) -> int:
+    result = 0
+    if bag in rules:
+        detailBag = rules[bag]
+        for contentBag in detailBag.contents:
+            result += contentBag.count + (contentBag.count * count_bags_in(rules, contentBag.bag))
+
+    return result
+
+
+def run_part1(title: str, input: list[str], correctResult: int):
+    rules = parse_rules(input)
+    foundBags: list[str] = []
+    count_bags_containing(rules, "shiny gold", foundBags)
+    result = len(foundBags)
+    utils.validate_result(title, result, correctResult)
+
+
+def run_part2(title: str, input: list[str], correctResult: int):
+    rules = parse_rules(input)
+    result = count_bags_in(rules, "shiny gold")
+    utils.validate_result(title, result, correctResult)
+
+
+if __name__ == "__main__":
+    print("---- Day 7: Handy Haversacks ----")
+
+    # run_part1("Test Case 1",
+    #     utils.read_input_as_list("day07-example1"),
+    #     4)
+    run_part1("problem",
+        utils.read_input_as_list("day07"),
+        205)
+
+    print("---- part 2 ----")
+
+    # run_part2("Test Case 1",
+    #     utils.read_input_as_list("day07-example1"),
+    #     32)
+    # run_part2("Test Case 2",
+    #     utils.read_input_as_list("day07-example2"),
+    #     126)
+    run_part2("problem",
+        utils.read_input_as_list("day07"),
+        80902)
