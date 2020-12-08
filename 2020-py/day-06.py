06927ed3a0dba66c7e7d119d4c0bc5fcaaa8898d
SDragon42 <nathen.silver@gmail.com>
2020-12-07 18:36:01 -0700

    Consolidated Day 6

---
+++ 2020-py/day-06.py
@@ -0,0 +1,77 @@
+# from typing import AnyStr, Callable
+from typing import Callable
+import utils
+
+
+def count_unique_answers(lines: list[str]) -> int:
+    answers = []
+
+    for line in lines:
+        for c in line:
+            if answers.count(c) == 0:
+                answers.append(c)
+    
+    return len(answers)
+
+
+def count_unique_unanimous_answers(lines: list[str]) -> int:
+    answers = {}
+    pCount = 0
+
+    for line in lines:
+        pCount += 1
+        for c in line:
+            if c in answers:
+                answers[c] = answers[c] + 1
+            else:
+                answers[c] = 1
+
+    result = 0
+    for a in answers:
+        if answers[a] == pCount:
+            result += 1
+    
+    return result
+
+
+def run(title: str, input: list[str], process: Callable[[list[str]], int], correctResult: int):
+    result = 0
+
+    groupStart = 0
+    groupEnd = 0
+    while groupStart < len(input):
+        try:
+            groupEnd = input.index("", groupStart)
+        except:
+            groupEnd = len(input)
+
+        lines = input[groupStart:groupEnd]
+        result += process(lines)
+
+        groupStart = groupEnd + 1
+
+    utils.validate_result(title, result, correctResult)
+
+
+if __name__ == "__main__":
+    print("---- Day 6: Custom Customs ----")
+
+    # run("Test Case 1",
+    #     utils.read_input_as_list("day06-example1"),
+    #     count_unique_answers,
+    #     11)
+    run("Problem",
+        utils.read_input_as_list("day06"),
+        count_unique_answers,
+        6726)
+
+    print("---- part 2 ----")
+
+    # run("Test Case 1",
+    #     utils.read_input_as_list("day06-example1"),
+    #     count_unique_unanimous_answers,
+    #     6)
+    run("Problem",
+        utils.read_input_as_list("day06"),
+        count_unique_unanimous_answers,
+        3316)
