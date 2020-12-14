337a872d05864a883967a2f01d7d6ea8dce92ecc
Nathen Silver <nathen.silver@gmail.com>
2020-12-14 08:14:08 -0700

    Day 14 part 2 completed

--- 2020-py/day-14.py
+++ 2020-py/day-14.py
@@ -1,4 +1,5 @@
 import utils
+import math
 import re
 
 bitWidth: int = 36
@@ -24,7 +25,6 @@ def set_mask(line: str):
 
 def apply_mask_v1(bValue: list[str]):
     global mask
-
     i = 0
     while i < len(mask):
         if mask[i] != "X":
@@ -35,7 +35,6 @@ def apply_mask_v1(bValue: list[str]):
 def set_memory_v1(line: str):
     global mask
     global memory
-
     x = line.index("]")
     address = int(line[4:x])
     x += 4
@@ -50,24 +49,41 @@ def set_memory_v1(line: str):
 
 def apply_mask_v2(bValue: list[str]):
     global mask
-
     i = 0
     while i < len(mask):
-        if mask[i] != "X":
+        if mask[i] != "0":
             bValue[i] = mask[i]
         i += 1
 
 
-def get_all_possible_addresses(bValue: list[str]) -> list[int]:
-    addresses: list[int] = []
+def flip_address_bits(bValue: list[str], replacements: str) -> list[str]:
+    i = 0
+    while i < len(bValue):
+        if bValue[i] == "X":
+            bValue[i] = replacements[0]
+            replacements = replacements[1:]
+        i += 1
+    return bValue
 
+
+def get_all_possible_addresses(bValue: list[str]) -> list[int]:
+    global mask
+    addresses: list[int] = []
+    count = mask.count("X")
+    num = int(math.pow(2, count))
+    rangeNums = range(num)
+    for r in rangeNums:
+        br = bin(r)[2:].rjust(count, "0")
+        newAddr = flip_address_bits(bValue.copy(), br)
+        newAddr2 = binary_to_int(newAddr)
+        addresses.append(newAddr2)
+        pass
     return addresses
 
 
 def set_memory_v2(line: str):
     global mask
     global memory
-
     x = line.index("]")
     address = int(line[4:x])
     x += 4
@@ -82,7 +98,6 @@ def set_memory_v2(line: str):
 
 def run_part1(title: str, input: list[str], correctResult: int):
     global memory
-
     memory.clear()
 
     actions = {
@@ -103,7 +118,23 @@ def run_part1(title: str, input: list[str], correctResult: int):
 
 
 def run_part2(title: str, input: list[str], correctResult: int):
+    global memory
+    memory.clear()
+
+    actions = {
+        "mas" : set_mask,
+        "mem" : set_memory_v2
+    }
+
+    for line in input:
+        key = line[:3]
+        actions[key](line)
+
     result = 0
+    for key in memory:
+        result += memory[key]
+        pass
+
     utils.validate_result(title, result, correctResult)
 
 
@@ -120,9 +151,9 @@ if __name__ == "__main__":
 
     print("---- part 2 ----")
 
-    # run_part2("Test Case 1",
-    #     utils.read_input_as_list(day, "example1"),
-    #     0)
-    # run_part2("problem",
-    #     utils.read_input_as_list(day, "input"),
-    #     0)
+    run_part2("Test Case 1",
+        utils.read_input_as_list(day, "example2"),
+        208)
+    run_part2("problem",
+        utils.read_input_as_list(day, "input"),
+        2308180581795)
