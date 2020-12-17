78498b8e1f34d62927e934a8bf6604474d9e7d0b
nathen.silver@ruralsourcing.com <nathen.silver@ruralsourcing.com>
2020-12-17 13:23:33 -0700

    Day 17 part 1 working

--- 2020-py/day-17.py
+++ 2020-py/day-17.py
@@ -18,6 +18,11 @@ class Location3D:
         self.y += offset.y
         self.z += offset.z
 
+    def create_shifted(self, offset: 'Location3D'):
+        newLocation = Location3D(self.x, self.y, self.z)
+        newLocation.shift(offset)
+        return newLocation
+
 #-------------------------------------------------------------------------------
 
 class ConwayCube:
@@ -29,6 +34,7 @@ class ConwayCube:
         y = len(inputList) // 2
         xStart = (len(inputList[0])) // 2 * -1
 
+        self.cubeSpace.clear()
         for line in inputList:
             x = xStart
             for c in line:
@@ -36,7 +42,17 @@ class ConwayCube:
                 self.cubeSpace[str(addr)] = c
                 x += 1
             y -= 1
-        pass
+
+        # Build SHIFTS
+        self.SHIFTS.clear()
+        for x in range(3):
+            for y in range(3):
+                for z in range(3):
+                    if x == 1 and y == 1 and z == 1:
+                        continue
+                    loc = Location3D(x - 1, y - 1, z - 1)
+                    self.SHIFTS.append(loc)
+
 
     def create_location3d(self, xyz: str) -> Location3D:
         p = xyz.split(",")
@@ -93,15 +109,38 @@ class ConwayCube:
             low = self.get_lowest(low, addr.x)
             high = self.get_highest(high, addr.x)
         return self.make_int_list(low, high)
-        
-            
-    def get_cube(self, addr: Location3D) -> str:
+
+
+    # def get_cube(self, addr: Location3D) -> str:
+    #     sAddr = str(addr)
+    #     if sAddr in self.cubeSpace:
+    #         return self.cubeSpace[sAddr]
+    #     return "."
+
+    def get_cube(self, addr: Location3D, cubes: dict[str, str]) -> str:
         sAddr = str(addr)
-        if sAddr in self.cubeSpace:
-            return self.cubeSpace[sAddr]
-        return "."
-    
-    def show_cube_space(self): #, z: int):
+        if sAddr in cubes:
+            return cubes[sAddr]
+        return self.INACTIVE
+
+    def set_cube(self, addr: Location3D, newState: str):
+        sAddr = str(addr)
+        if newState == self.ACTIVE:
+            self.cubeSpace[sAddr] = newState
+            return
+
+        # if sAddr in self.cubeSpace:
+        self.cubeSpace.pop(sAddr, None)
+
+
+    def inflate_range(self, indexes: list[int]):
+        indexes.insert(0, indexes[0] - 1)
+        indexes.append(indexes[len(indexes) - 1] + 1)
+
+
+    def show_cube_space(self, title: str): #, z: int):
+        print(title)
+        print()
         zList = self.get_z_range()
         yList = self.get_y_range()
         xList = self.get_x_range()
@@ -114,27 +153,51 @@ class ConwayCube:
                 line = ""
                 for x in xList:
                     addr = Location3D(x, y, z)
-                    line += self.get_cube(addr)
+                    line += self.get_cube(addr, self.cubeSpace)
                     # if addr in self.cubeSpace:
                     #     line += self.cubeSpace[addr]
                     # else:
-                    #     line += "."
+                    #     line += self.INACTIVE
                 print(line)
-            print("")
-    
+            print()
+        print()
+
+
     def rule_active_cube(self, addr: Location3D, cubes: dict[str, str]):
         """
-        If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains active. 
+        If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains active.
         Otherwise, the cube becomes inactive.
         """
-        pass
+        numActive = 0
+        for offset in self.SHIFTS:
+            tmpAddr = addr.create_shifted(offset)
+            state = self.get_cube(tmpAddr, cubes)
+            if state == self.ACTIVE:
+                numActive += 1
+
+        newState = self.INACTIVE
+        if numActive == 2 or numActive == 3:
+            newState = self.ACTIVE
+        self.set_cube(addr, newState)
+
 
     def rule_inactive_cube(self, addr: Location3D, cubes: dict[str, str]):
         """
         If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active.
         Otherwise, the cube remains inactive.
         """
-        pass
+        numActive = 0
+        for offset in self.SHIFTS:
+            tmpAddr = addr.create_shifted(offset)
+            state = self.get_cube(tmpAddr, cubes)
+            if state == self.ACTIVE:
+                numActive += 1
+
+        newState = self.INACTIVE
+        if numActive == 3:
+            newState = self.ACTIVE
+        self.set_cube(addr, newState)
+
 
     def apply_rules(self):
         oldCubeSpace = self.cubeSpace.copy()
@@ -143,33 +206,49 @@ class ConwayCube:
         yList = self.get_y_range()
         xList = self.get_x_range()
 
+        self.inflate_range(zList)
+        self.inflate_range(yList)
+        self.inflate_range(xList)
+
         for z in zList:
             for y in yList:
                 for x in xList:
                     addr = Location3D(x, y, z)
+                    state = self.get_cube(addr, oldCubeSpace)
+                    self.RULES[state](self, addr, oldCubeSpace)
 
-                    pass
-        pass
+
+    def get_num_active_cubes(self) -> int:
+        result = 0
+        for key in self.cubeSpace:
+            if self.cubeSpace[key] == self.ACTIVE:
+                result += 1
+        return result
+
+    ACTIVE = "#"
+    INACTIVE = "."
 
     RULES = {
-        ".": rule_inactive_cube,
-        "#": rule_active_cube
+        INACTIVE: rule_inactive_cube,
+        ACTIVE: rule_active_cube,
     }
+
+    SHIFTS: list[Location3D] = []
 
 #-------------------------------------------------------------------------------
 
 
-def run_part1(title: str, inputList: list[str], correctResult: int):
+def run_part1(title: str, inputList: list[str], numCycles: int, correctResult: int):
     cc = ConwayCube(inputList)
-    cc.show_cube_space()
+    # cc.show_cube_space("Before any cycles:")
 
-    # cycle = 1
-    # while cycle <= 3:
-    #     cc.apply_rules()
+    cycle = 1
+    while cycle <= numCycles:
+        cc.apply_rules()
+        # cc.show_cube_space(f"After {cycle} cycle:")
+        cycle += 1
 
-    #     cycle += 1
-
-    result = 0
+    result = cc.get_num_active_cubes()
     utils.validate_result(title, result, correctResult)
 
 
@@ -184,10 +263,12 @@ if __name__ == "__main__":
 
     run_part1("Test Case 1",
         utils.read_input_as_list(day, "example1"),
-        0)
-    # run_part1("problem",
-    #     utils.read_input_as_list(day, "input"),
-    #     0)
+        6,
+        112)
+    run_part1("problem",
+        utils.read_input_as_list(day, "input"),
+        6,
+        362)
 
     # print("---- part 2 ----")
 
