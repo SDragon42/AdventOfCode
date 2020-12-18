8edab2288217933b1302af11e028379b0a38c656
Nathen Silver <nathen.silver@gmail.com>
2020-12-18 00:01:54 -0700

    Day 18 complete

--- 2020-py/day-18.py
+++ 2020-py/day-18.py
@@ -12,7 +12,6 @@ def find_last(text: str, toFind: str) -> int:
     return result
 
 def evaluate(equation: str) -> int:
-
     while True:
         endP = equation.find(")")
         if endP < 0:
@@ -23,11 +22,9 @@ def evaluate(equation: str) -> int:
         subEQ = equation[startP:endP+1]
         result = evaluate(innerEQ)
         equation = equation.replace(subEQ, str(result))
-        pass
 
-    # ----------------
     equationParts = equation.split()
-    value = 0
+    result = 0
     op = ""
     for p in equationParts:
         if p in OPS:
@@ -36,20 +33,64 @@ def evaluate(equation: str) -> int:
 
         currValue = int(p)
         if op == "":
-            value = currValue
+            result = currValue
             continue
 
         if op == "+":
-            value += currValue
+            result += currValue
             op = ""
             continue
 
         if op == "*":
-            value *= currValue
+            result *= currValue
             op = ""
             continue
 
-    result = value
+    return result
+
+def evaluate2(equation: str) -> int:
+    while True:
+        endP = equation.find(")")
+        if endP < 0:
+            break
+        startP = find_last(equation[:endP], "(")
+
+        innerEQ = equation[startP+1:endP]
+        subEQ = equation[startP:endP+1]
+        result = evaluate2(innerEQ)
+        equation = equation.replace(subEQ, str(result))
+
+    equationParts = equation.split()
+    
+    i = 0
+    while i < len(equationParts):
+        if equationParts[i] == "+":
+            a = int(equationParts[i - 1])
+            b = int(equationParts[i + 1])
+            equationParts[i - 1] = str(a + b)
+            equationParts.pop(i)
+            equationParts.pop(i)
+            i = 0
+            continue
+        i += 1
+
+    result = 0
+    op = ""
+    for p in equationParts:
+        if p in OPS:
+            op = p
+            continue
+
+        currValue = int(p)
+        if op == "":
+            result = currValue
+            continue
+
+        if op == "*":
+            result *= currValue
+            op = ""
+            continue
+
     return result
 
 def run_part1(title: str, input: list[str], correctResult: int):
@@ -63,6 +104,11 @@ def run_part1(title: str, input: list[str], correctResult: int):
 
 def run_part2(title: str, input: list[str], correctResult: int):
     result = 0
+    for line in input:
+        total = evaluate2(line)
+        result += total
+        # print(f"{total:-6} = {line}")
+
     utils.validate_result(title, result, correctResult)
 
 
@@ -70,18 +116,18 @@ if __name__ == "__main__":
     day = 18
     print(f"---- Day {day}: Operation Order ----")
 
-    run_part1("Test Case 1",
-        utils.read_input_as_list(day, "example1"),
-        26457)
+    # run_part1("Test Case 1",
+    #     utils.read_input_as_list(day, "example1"),
+    #     26457)
     run_part1("problem",
         utils.read_input_as_list(day, "input"),
         75592527415659)
 
-    # print("---- part 2 ----")
+    print("---- part 2 ----")
 
     # run_part2("Test Case 1",
     #     utils.read_input_as_list(day, "example1"),
-    #     0)
-    # run_part2("problem",
-    #     utils.read_input_as_list(day, "input"),
-    #     0)
+    #     694173)
+    run_part2("problem",
+        utils.read_input_as_list(day, "input"),
+        360029542265462)
