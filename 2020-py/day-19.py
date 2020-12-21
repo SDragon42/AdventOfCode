8718fe90447b27c880736e953a7e6a5c2be6ddd3
Nathen Silver <nathen.silver@gmail.com>
2020-12-21 15:10:14 -0700

    Day 19 part 1 working

--- 2020-py/day-19.py
+++ 2020-py/day-19.py
@@ -24,22 +24,44 @@ def build_rule_dict(input: list[str]) -> dict[str, RuleChains]:
     return rules
 
 
-def process_image(imageData: str, rules: dict[str, RuleChains], charIdx: int, ruleKey: str) -> bool:
-
+def process_image(imageData: str, rules: dict[str, RuleChains], charIdx: int, ruleKey: str) -> int:
     chain: RuleChains = rules[ruleKey]
-
-
-    return False
+    utils.dprint(imageData[charIdx:])
+    utils.dprint(f"rule: {ruleKey}  -  {chain}")
+    for orChain in chain:
+        utils.dprint(f"    {orChain}")
+        successChars = 0
+        for x in orChain:
+            if x == "a" or x == "b":
+                if x == imageData[charIdx + successChars]:
+                    utils.dprint("    MATCH")
+                    utils.dprint("")
+                    return 1
+                else:
+                    return 0
+            else:
+                shift = process_image(imageData, rules, charIdx + successChars, x)
+                successChars += shift
+                if shift <= 0:
+                    successChars = 0
+                    break
+        
+        if successChars > 0:
+            return successChars
+    return 0
 
 
 def run_part1(title: str, input: list[str], correctResult: int):
     end = input.index("")
     rules = build_rule_dict(input[:end])
-    # images = input[end+1:]
-    # result = 0
-    # for imageData in images:
-    #     if process_image(imageData, rules, 0, "0"):
-    #         result += 1
+    images = input[end+1:]
+    result = 0
+    for imageData in images:
+        # charIdx = 0
+        utils.dprint(f"IMG: {imageData}")
+        charsMatched = process_image(imageData, rules, 0, "0") 
+        if charsMatched == len(imageData):
+            result += 1
     utils.validate_result(title, result, correctResult)
 
 
@@ -48,6 +70,7 @@ def run_part2(title: str, input: list[str], correctResult: int):
     utils.validate_result(title, result, correctResult)
 
 
+# utils.showDebug = True
 if __name__ == "__main__":
     day = 19
     print(f"---- Day {day}: Monster Messages ----")
@@ -55,9 +78,9 @@ if __name__ == "__main__":
     run_part1("Test Case 1",
         utils.read_input_as_list(day, "example1"),
         2)
-    # run_part1("problem",
-    #     utils.read_input_as_list(day, "input"),
-    #     0)
+    run_part1("problem",
+        utils.read_input_as_list(day, "input"),
+        222)
 
     # print("---- part 2 ----")
 
