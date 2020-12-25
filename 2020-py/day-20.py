b7b7bac00d8a7d244b368ed203b76429090f8e70
Nathen Silver <nathen.silver@gmail.com>
2020-12-25 13:05:47 -0900

    day 20 p2 wip

--- 2020-py/day-20.py
+++ 2020-py/day-20.py
@@ -12,7 +12,7 @@ class TileData:
         self.id = int(tmp[5:tmp.index(":")])
         self.tile = lines[1:]
         self.tileSize = len(self.tile)
-    
+
     def get_edge_top(self) -> str:
         return self.tile[0]
 
@@ -52,6 +52,12 @@ class TileData:
         self.tile.clear()
         self.tile = newTile
 
+    def flip_tile(self):
+        y = 0
+        while y < self.tileSize:
+            self.tile[y] = self.tile[y][::-1]
+            y += 1
+
     def display_tile(self):
         print(f"Tile {self.id}:")
         print("\n".join(self.tile))
@@ -73,25 +79,8 @@ def does_edge_match(tileList: list[TileData], edge: str) -> bool:
     return False
 
 
-def get_num_matched_edges(tileList: list[TileData], index: int) -> int:
-    count = 0
-    thisTile = tileList[index]
-    for side in thisTile.get_all_edges():
-        i = 0
-        while i < len(tileList):
-            if i != index:
-                for otherSide in tileList[i].get_all_edges():
-                    if otherSide == side:
-                        count += 1
-
-            i += 1
-    return count
-
-
-def run_part1(title: str, inputData: str, correctResult: int):
-    tileList = create_puzzle_tiles(inputData)
-
-    cornerTiles: list[TileData] = []
+def get_open_side_tiles(tileList: list[TileData], numOpenSides: int) -> list[TileData]:
+    openTiles: list[TileData] = []
     for tile in tileList:
         otherTiles = tileList.copy()
         otherTiles.remove(tile)
@@ -101,45 +90,52 @@ def run_part1(title: str, inputData: str, correctResult: int):
             if not does_edge_match(otherTiles, edge):
                 num += 1
                 continue
-        if num == 2:
-            cornerTiles.append(tile)
+        if num == numOpenSides:
+            openTiles.append(tile)
+    return openTiles
 
 
-    # tiles[0].display_tile()
-    # print("--------------------")
-    # for s in tiles[0].get_all_edges():
-    #     print(s)
-    # tiles[0].rotate_tile()
-    # tiles[0].display_tile()
-
+def run_part1(title: str, inputData: str, correctResult: int):
+    tileList = create_puzzle_tiles(inputData)
+    cornerTiles = get_open_side_tiles(tileList, 2)
     result = 1
     for corner in cornerTiles:
         result *= corner.id
-        print(corner.id)
+        utils.dprint(f"tile id: {corner.id}")
     utils.validate_result(title, result, correctResult)
 
 
 def run_part2(title: str, inputData: str, correctResult: int):
-    result = 0
+    tileList = create_puzzle_tiles(inputData)
+    cornerTiles = get_open_side_tiles(tileList, 2)
+    sideTiles = get_open_side_tiles(tileList, 1)
+    for t in cornerTiles: tileList.remove(t)
+    for t in sideTiles: tileList.remove(t)
+
+    result = 1
+    for corner in cornerTiles:
+        result *= corner.id
+        utils.dprint(f"tile id: {corner.id}")
     utils.validate_result(title, result, correctResult)
 
 
+# utils.showDebug = True
 if __name__ == "__main__":
     day = 20
     print(f"---- Day {day}: Jurassic Jigsaw ----")
 
-    run_part1("Test Case 1",
+    # run_part1("Test Case 1",
+    #     utils.read_input(day, "example1"),
+    #     20899048083289)
+    # run_part1("problem",
+    #     utils.read_input(day, "input"),
+    #     28057939502729)
+
+    print("---- part 2 ----")
+
+    run_part2("Test Case 1",
         utils.read_input(day, "example1"),
-        20899048083289)
-    run_part1("problem",
-        utils.read_input(day, "input"),
-        28057939502729)
-
-    # print("---- part 2 ----")
-
-    # run_part2("Test Case 1",
-    #     utils.read_input_as_list(day, "example1"),
-    #     0)
+        273)
     # run_part2("problem",
-    #     utils.read_input_as_list(day, "input"),
+    #     utils.read_input(day, "input"),
     #     0)
