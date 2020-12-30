583350e16b68e1f408fd3c78c85060c0e174e99d
Nathen Silver <nathen.silver@gmail.com>
2020-12-29 20:58:30 -0900

    reworked part 1

--- 2020-py/day-24.py
+++ 2020-py/day-24.py
@@ -1,37 +1,37 @@
 import utils
 import collections
 
-# flippedTiles: set['LobbyTile'] = set()
 
-directions: dict[str, tuple[int, int]] = {'e':  (2,  0), 'w':  (-2,  0),
-                               'ne': (1,  1), 'nw': (-1,  1),
-                               'se': (1, -1), 'sw': (-1, -1)}
+class SpatialAddress:
+    coordinates: list[int] = []
 
-allTiles: dict[str, bool] = collections.defaultdict(bool)
-# flippedTileKeys: set[str] = set()
+    def __init__(self, coordinates: list[int]) -> None:
+        self.coordinates = coordinates
 
-# class LobbyTile:
-#     isBlack: bool = False
-#     # adjacentTiles: dict[str, 'LobbyTile'] = {}
+    def __str__(self) -> str:
+        s = [str(value) for value in self.coordinates]
+        return ",".join(s)
 
-#     def __init__(self) -> None:
-#         # global flippedTiles
-#         self.isBlack = False
-#         # self.adjacentTiles = {}
-#         # flippedTiles.add(self)
-
-#     def flip(self) -> None:
-#         global flippedTiles
-#         self.isBlack = not self.isBlack
-#         flippedTiles.add(self)
-#         # utils.dprint(f"Black? {self.isBlack}  -  {self}")
-
-#     def get_tile(self, dir: str) -> 'LobbyTile':
-#         if dir not in self.adjacentTiles:
-#             self.adjacentTiles[dir] = LobbyTile()
-#         return self.adjacentTiles[dir]
+    def shift(self, offset: 'SpatialAddress') -> None:
+        i = 0
+        while i < len(self.coordinates):
+            self.coordinates[i] += offset.coordinates[i]
+            i += 1
 
 #-------------------------------------------------------------------------------
+
+
+directionOffsets: dict[str, 'SpatialAddress'] = {
+    "e": SpatialAddress([2, 0]),
+    "w": SpatialAddress([-2, 0]),
+    "ne": SpatialAddress([1, 1]),
+    "nw": SpatialAddress([-1, 1]),
+    "se": SpatialAddress([1, -1]),
+    "sw": SpatialAddress([-1, -1]),
+}
+
+TileDict = dict[str, bool]
+
 
 def parse_move_line(line: str) -> list[str]:
     moveList = []
@@ -42,60 +42,40 @@ def parse_move_line(line: str) -> list[str]:
             toCopy = 2
         moveList.append(line[i:i+toCopy])
         i += toCopy
-    # utils.dprint(f"moves: {moveList}")
-    # utils.dprint(line)
-    # utils.dprint(moveList)
-    # utils.dprint("")
     return moveList
 
 
-def get_addr_key(x: int, y: int) -> str:
-    key = f"{x}|{y}"
-    return key
-
-
-def get_addr(moveList: list[str]) -> tuple[int, int]:
-    x = 0
-    y = 0
+def get_addr(moveList: list[str]) -> SpatialAddress:
+    addr = SpatialAddress([0, 0])
     for move in moveList:
-        offset = directions[move]
-        x += offset[0]
-        y += offset[1]
-    return (x,y)
+        offset = directionOffsets[move]
+        addr.shift(offset)
+    return addr
 
 
-def count_black_tiles() -> int:
+def count_black_tiles(tileFloor: TileDict) -> int:
     count = 0
-    # for key in flippedTileKeys:
-    #     if allTiles[key]:
-    #         count += 1
-    for v in allTiles.values():
+    for v in tileFloor.values():
         if v:
             count += 1
-    # for t in flippedTiles:
-    #     if t.isBlack:
-    #         count += 1
     return count
 
 
-def run_part1(title: str, input: list[str], correctResult: int):
-    # origin = LobbyTile()
-    allTiles.clear()
+def build_tile_floor(input: list[str]) -> TileDict:
+    tileFloor: TileDict = collections.defaultdict(bool)
     for line in input:
         moveList = parse_move_line(line)
         if len(moveList) == 0:
             continue
-
         addr = get_addr(moveList)
-        addrKey = get_addr_key(addr[0], addr[1])
+        addrKey = str(addr)
+        tileFloor[addrKey] = not tileFloor[addrKey]
+    return tileFloor
 
-        allTiles[addrKey] = not allTiles[addrKey]
-        # tile = origin
-        # for move in moveList:
-        #     tile = tile.get_tile(move)
-        # tile.flip()
 
-    result = count_black_tiles()
+def run_part1(title: str, input: list[str], correctResult: int):
+    tileFloor = build_tile_floor(input)
+    result = count_black_tiles(tileFloor)
     utils.validate_result(title, result, correctResult)
 
 
