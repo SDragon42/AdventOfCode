240de9f007c1627d077a2a969ca60707fddb47b5
SDragon42 <nathen.silver@gmail.com>
2021-10-09 00:52:23 -0600

    Removed old day1-13. Added new Day1 class.

---
+++ CSharp.Common/IPuzzle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.CSharp.Common
+{
+    public interface IPuzzle
+    {
+        void Run();
+    }
+
+    public interface IPuzzle2
+    {
+        void Part1(string input, string expectedAnswer);
+        void Part2(string input, string expectedAnswer);
+    }
+}
---
+++ CSharp.Common/PuzzleRunner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.CSharp.Common
+{
+    public static class PuzzleRunner
+    {
+
+        public static IPuzzle GetPuzzle(int day)
+        {
+            var puzzleType = Assembly.GetEntryAssembly()
+                .GetTypes()
+                .Where(p => p.Name == $"Day{day:00}")
+                .FirstOrDefault();
+            return (IPuzzle)Activator.CreateInstance(puzzleType);
+        }
+    }
+}
