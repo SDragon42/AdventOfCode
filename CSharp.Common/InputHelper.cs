5b7bb1fde47c2537fb9203dea5601327c060ebe5
SDragon42 <nathen.silver@gmail.com>
2021-10-11 10:01:44 -0600

    Added AsLines() extension

--- CSharp.Common/InputHelper.cs
+++ CSharp.Common/InputHelper.cs
@@ -25,7 +25,7 @@ namespace AdventOfCode.CSharp.Common
         }
 
 
-
+        #region Old Code
         ///// <summary>Returns the file as a single string.
         ///// </summary>
         ///// <param name="name">The name of the data file to load.</param>
@@ -37,50 +37,50 @@ namespace AdventOfCode.CSharp.Common
         //    return result;
         //}
 
-        /// <summary>Returns each line from the file as an enumeration of type T.
-        /// </summary>
-        /// <param name="name">The name of the data file to load.</param>
-        /// <typeparam name="T">The data type to convert each line to.</typeparam>
-        /// <returns></returns>
-        public static IEnumerable<T> LoadLines<T>(string name)
-        {
-            var filename = GenerateFilename(name);
-            var result = File.ReadLines(filename)
-                .Select(l => l.Trim())
-                .Where(l => l.Length > 0)
-                .Select(l => (T)Convert.ChangeType(l, typeof(T)));
-            return result;
-        }
+        ///// <summary>Returns each line from the file as an enumeration of type T.
+        ///// </summary>
+        ///// <param name="name">The name of the data file to load.</param>
+        ///// <typeparam name="T">The data type to convert each line to.</typeparam>
+        ///// <returns></returns>
+        //public static IEnumerable<T> LoadLines<T>(string name)
+        //{
+        //    var filename = GenerateFilename(name);
+        //    var result = File.ReadLines(filename)
+        //        .Select(l => l.Trim())
+        //        .Where(l => l.Length > 0)
+        //        .Select(l => (T)Convert.ChangeType(l, typeof(T)));
+        //    return result;
+        //}
 
-        /// <summary>Returns each line from the file as an enumeration of arrays of type T.
-        /// </summary>
-        /// <param name="name">The name of the data file to load.</param>
-        /// <typeparam name="T">The data type to convert each element of the array to.</typeparam>
-        /// <returns></returns>
-        public static IEnumerable<T[]> LoadLinesAsCSV<T>(string name)
-        {
-            var lines = LoadLines<string>(name);
-            foreach (var line in lines)
-            {
-                var result = line
-                    .Split(',')
-                    .Select(i => (T)Convert.ChangeType(i, typeof(T)))
-                    .ToArray();
-                yield return result;
-            }
-        }
+        ///// <summary>Returns each line from the file as an enumeration of arrays of type T.
+        ///// </summary>
+        ///// <param name="name">The name of the data file to load.</param>
+        ///// <typeparam name="T">The data type to convert each element of the array to.</typeparam>
+        ///// <returns></returns>
+        //public static IEnumerable<T[]> LoadLinesAsCSV<T>(string name)
+        //{
+        //    var lines = LoadLines<string>(name);
+        //    foreach (var line in lines)
+        //    {
+        //        var result = line
+        //            .Split(',')
+        //            .Select(i => (T)Convert.ChangeType(i, typeof(T)))
+        //            .ToArray();
+        //        yield return result;
+        //    }
+        //}
 
-        /// <summary>Generates the standard path to a data file.
-        /// </summary>
-        /// <param name="name">The name of the data file.</param>
-        /// <returns></returns>
-        static string GenerateFilename(string name)
-        {
-            var filename = $@".\input\{name}.txt";
-            return filename;
-        }
+        ///// <summary>Generates the standard path to a data file.
+        ///// </summary>
+        ///// <param name="name">The name of the data file.</param>
+        ///// <returns></returns>
+        //static string GenerateFilename(string name)
+        //{
+        //    var filename = $@".\input\{name}.txt";
+        //    return filename;
+        //}
 
-
+        #endregion
 
     }
 }
