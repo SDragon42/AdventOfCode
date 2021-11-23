b195984daf4df9a8e8b6ea8ae0ffd77f26709eac
SDragon42 <nathen.silver@gmail.com>
2021-11-23 09:47:04 -0700

    Initial conver to classes

--- 2020/Python/day01.py
+++ 2020/Python/day01.py
@@ -1,65 +1,77 @@
-# import utils
 import sys
 from typing import List
 
 sys.path.append('../../Python.Common')
 import helper
 import inputHelper
+from puzzleBase import PuzzleBase
 
 
-def get_value(num_splits: int, testInput: List[int], testValues: List[int]) -> int:
-    if num_splits == 0:
-        for x in testInput:
-            sumTotal = x
-            multTotal = x
-            for v in testValues:
-                sumTotal += v
-                multTotal *= v
-            if sumTotal == 2020:
-                return multTotal
+class Puzzle(PuzzleBase):
+
+    def get_value(self, num_splits: int, testInput: List[int], testValues: List[int]) -> int:
+        if num_splits == 0:
+            for x in testInput:
+                sumTotal = x
+                multTotal = x
+                for v in testValues:
+                    sumTotal += v
+                    multTotal *= v
+                if sumTotal == 2020:
+                    return multTotal
+            return -1
+
+        idx = 0
+        while idx < len(testInput) - num_splits:
+            testValues.append(testInput[idx])
+            result = self.get_value(num_splits - 1, testInput[idx+1:], testValues)
+            if result != -1:
+                return result
+            testValues.pop()
+            idx += 1
+        
         return -1
 
-    idx = 0
-    while idx < len(testInput) - num_splits:
-        testValues.append(testInput[idx])
-        result = get_value(num_splits - 1, testInput[idx+1:], testValues)
-        if result != -1:
-            return result
-        testValues.pop()
-        idx += 1
-    
-    return -1
 
+    def do_it(self, title: str, num_splits: int, input: List[int], correctResult: int) -> str:
+        result = self.get_value(num_splits, input, [])
+        return helper.validate_result2(title, result, correctResult)
 
-def run(title: str, num_splits: int, input: List[int], correctResult: int):
-    result = get_value(num_splits, input, [])
-    helper.validate_result(title, result, correctResult)
+    # def run_example(o: utils.PuzzleOptions, title: str, num_splits: int, input: List[int], correctResult: int) -> None:
+    #     if (o.runExamples):
+    #         run(title, num_splits, input, correctResult)
 
-# def run_example(o: utils.PuzzleOptions, title: str, num_splits: int, input: List[int], correctResult: int) -> None:
-#     if (o.runExamples):
-#         run(title, num_splits, input, correctResult)
+    def example1(self) -> str:
+        return self.do_it(" Ex. 1)", 1,
+            inputHelper.read_input_as_int_list(1,"example1"),
+            514579)
 
-# def solve(o: utils.PuzzleOptions):
-def solve():
-    print("Day 1: Report Repair")
-    print("")
-    # run("Test case 1", 1,
-    #     inputHelper.read_input_as_int_list(1,"example1"),
-    #     514579)
-    run("Part 1)", 1,
-        inputHelper.read_input_as_int_list(1, "input"),
-        969024)
+    def part1(self) -> str:
+        return self.do_it("Part 1)", 1,
+            inputHelper.read_input_as_int_list(1, "input"),
+            969024)
 
-    # print("---- part 2 ----")
-    print("")
+    def example2(self) -> str:
+        return self.do_it(" Ex. 2)", 2,
+            inputHelper.read_input_as_int_list(1, "example1"),
+            241861950)
+    def part2(self) -> str:
+        return self.do_it("Part 2)", 2,
+            inputHelper.read_input_as_int_list(1, "input"),
+            230057040)
 
-    # run("Test case 1", 2,
-    #     inputHelper.read_input_as_int_list(1, "example1"),
-    #     241861950)
-    run("Part 2)", 2,
-        inputHelper.read_input_as_int_list(1, "input"),
-        230057040)
+    # def solve(o: utils.PuzzleOptions):
+    def solve(self):
+        print("Day 1: Report Repair")
+        print("")
 
+        self.run_example(self.example1)
+        self.run(self.part1)
 
-if __name__ == "__main__":
-    solve()
+        print("")
+
+        # run("Test case 1", 2,
+        #     inputHelper.read_input_as_int_list(1, "example1"),
+        #     241861950)
+        self.run_example(self.example2)
+        self.run(self.part1)
--- Python.Common/helper.py
+++ Python.Common/helper.py
@@ -6,6 +6,11 @@ def validate_result(title: str, value, correct_value) -> None:
     print(f"{title}  Result: {value}    {success}")
 
 
+def validate_result2(title: str, value, correct_value) -> str:
+    success = 'CORRECT' if correct_value == value else 'WRONG'
+    return f"{title}  Result: {value}    {success}"
+
+
 showDebug = False
 
 def dprint(x) -> None:
