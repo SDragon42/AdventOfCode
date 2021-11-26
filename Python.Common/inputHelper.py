aec868be46b74f41109734c61a8ed7374f23afc1
SDragon42 <nathen.silver@gmail.com>
2021-11-26 09:23:00 -0700

    reworked day 3 fixed a bug with the ImportHelper

--- Python.Common/inputHelper.py
+++ Python.Common/inputHelper.py
@@ -4,54 +4,54 @@ from typing import List
 def load_input_file(day: int, name: str) -> list[str]:
     filename = f".\\..\\input\\day{day:02d}\\{name}.txt"
     with open(filename) as dataFile:
-        return dataFile.readlines()
+        return dataFile.read().splitlines()
 
 
 def load_answer_file(day: int, part: int, name: str) -> list[str]:
     filename = f".\\..\\input\\day{day:02d}\\{name}-answer{part}.txt"
     try:
         with open(filename) as dataFile:
-            return dataFile.readlines()
+            return dataFile.read().splitlines()
     except OSError:
         return None
 
 
-def generate_filename(day: int, name: str) -> str:
-    filename = f".\\..\\input\\day{day:02d}\\{name}.txt"
-    return filename
+# def generate_filename(day: int, name: str) -> str:
+#     filename = f".\\..\\input\\day{day:02d}\\{name}.txt"
+#     return filename
 
 
-def read_input_as_int_list(day: int, name: str) -> List[int]:
-    """ Reads the input file as a List of ints """
-    filename = generate_filename(day, name)
-    data: List[int] = []
-    with open(filename) as dataFile:
-        while True:
-            line = dataFile.readline()
-            if not line:
-                break
-            line = line.strip()
-            value = int(line)
-            data.append(value)
-    return data
+# def read_input_as_int_list(day: int, name: str) -> List[int]:
+#     """ Reads the input file as a List of ints """
+#     filename = generate_filename(day, name)
+#     data: List[int] = []
+#     with open(filename) as dataFile:
+#         while True:
+#             line = dataFile.readline()
+#             if not line:
+#                 break
+#             line = line.strip()
+#             value = int(line)
+#             data.append(value)
+#     return data
 
 
-def read_input_as_list(day: int, name: str) -> List[str]:
-    """ Reads the input file as a List of strings """
-    filename = generate_filename(day, name)
-    data: List[str] = []
-    with open(filename) as dataFile:
-        while True:
-            line = dataFile.readline()
-            if not line:
-                break
-            data.append(line.strip())
-    return data
+# def read_input_as_list(day: int, name: str) -> List[str]:
+#     """ Reads the input file as a List of strings """
+#     filename = generate_filename(day, name)
+#     data: List[str] = []
+#     with open(filename) as dataFile:
+#         while True:
+#             line = dataFile.readline()
+#             if not line:
+#                 break
+#             data.append(line.strip())
+#     return data
 
 
-def read_input(day: int, name: str) -> str:
-    """ Reads the input file as a single string """
-    filename = generate_filename(day, name)
-    with open(filename) as dataFile:
-        result = dataFile.read()
-        return result
+# def read_input(day: int, name: str) -> str:
+#     """ Reads the input file as a single string """
+#     filename = generate_filename(day, name)
+#     with open(filename) as dataFile:
+#         result = dataFile.read()
+#         return result
