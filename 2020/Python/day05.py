3ad95a8cc9101f55bb08a0ab662034c1b410d790
SDragon42 <nathen.silver@gmail.com>
2021-11-26 10:10:14 -0700

    day 5 rework

--- 2020/Python/day05.py
+++ 2020/Python/day05.py
@@ -1,79 +1,84 @@
-import sys
 from typing import List
 
-sys.path.append('../../Python.Common')
 import helper
 import inputHelper
+from puzzleBase import PuzzleBase
 
 
-def get_row(lowCode: str, highCode: str, lowStart: int, highStart: int, input: str) -> int:
-    low = lowStart
-    high = highStart
-    for x in input:
-        byHalf = (high - low + 1) / 2
-        if x == highCode:
-            high -= byHalf
-        elif x == lowCode:
-            low += byHalf
-    return int(low - 1)
+
+class InputData:
+    input: List[str]
+    expectedAnswer: int
+
+    def __init__(self, name: str, part: int) -> None:
+        day = 5
+        self.input = inputHelper.load_input_file(day, name)
+        
+        lines = inputHelper.load_answer_file(day, part, name)
+        self.expectedAnswer = int(lines[0]) if lines is not None else None
 
 
-def get_seat_id(boardingPass: str) -> int:
-    rowCode = boardingPass[:7]
-    colCode = boardingPass[7:]
 
-    row = get_row("B", "F", 1, 128, rowCode)
-    col = get_row("R", "L", 1, 8, colCode)
-    seatId = (row * 8) + col
-    
-    return seatId
+class Puzzle(PuzzleBase):
 
-
-def run_part1(title: str, input: List[str], correctResult: int):
-    result = 0
-    for bPass in input:
-        seatId = get_seat_id(bPass)
-        if seatId > result:
-            result = seatId
-    helper.validate_result(title, result, correctResult)
+    def get_row(self, lowCode: str, highCode: str, lowStart: int, highStart: int, input: str) -> int:
+        low = lowStart
+        high = highStart
+        for x in input:
+            byHalf = (high - low + 1) / 2
+            if x == highCode:
+                high -= byHalf
+            elif x == lowCode:
+                low += byHalf
+        return int(low - 1)
 
 
-def run_part2(title: str, input: List[str], correctResult: int):
-    seats = []
-    for x in range(1024):
-        seats.append(x)
-    
-    for bPass in input:
-        seatId = get_seat_id(bPass)
-        seats[seatId] = 0
+    def get_seat_id(self, boardingPass: str) -> int:
+        rowCode = boardingPass[:7]
+        colCode = boardingPass[7:]
 
-    result = -1
-
-    for x in seats[1:-1]:
-        if x != 0 and seats[x - 1] == 0 and seats[x + 1] == 0:
-            result = x
-            break
-
-    helper.validate_result(title, result, correctResult)
+        row = self.get_row("B", "F", 1, 128, rowCode)
+        col = self.get_row("R", "L", 1, 8, colCode)
+        seatId = (row * 8) + col
+        
+        return seatId
 
 
-def solve():
-    print("Day 5: Binary Boarding")
-    print("")
-
-    # run_part1("Test Case 1",
-    #     inputHelper.read_input_as_list(5, "example1"),
-    #     820)
-    run_part1("Part 1)",
-        inputHelper.read_input_as_list(5, "input"),
-        970)
-
-    print("")
-
-    run_part2("Part 2)",
-        inputHelper.read_input_as_list(5, "input"),
-        587)
+    def run_part1(self, data: InputData) -> str:
+        result = 0
+        for bPass in data.input:
+            seatId = self.get_seat_id(bPass)
+            if seatId > result:
+                result = seatId
+        return helper.validate_result2('What is the highest seat ID on a boarding pass?', result, data.expectedAnswer)
 
 
-if __name__ == "__main__":
-    solve()
+    def run_part2(self, data: InputData) -> str:
+        seats = []
+        for x in range(1024):
+            seats.append(x)
+        
+        for bPass in data.input:
+            seatId = self.get_seat_id(bPass)
+            seats[seatId] = 0
+
+        result = -1
+
+        for x in seats[1:-1]:
+            if x != 0 and seats[x - 1] == 0 and seats[x + 1] == 0:
+                result = x
+                break
+
+        return helper.validate_result2('What is the ID of your seat?', result, data.expectedAnswer)
+
+
+    def solve(self):
+        print("Day 5: Binary Boarding")
+        print("")
+
+        self.run_example(lambda: "P1 Ex1) " + self.run_part1(InputData('example1', 1)))
+        self.run_problem(lambda: "Part 1) " + self.run_part1(InputData('input', 1)))
+
+        print("")
+
+        self.run_problem(lambda: "Part 2) " + self.run_part2(InputData('input', 2)))
