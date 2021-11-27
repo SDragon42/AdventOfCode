d036a1bb0b0587c34f127f451f045e7f4a7d36fa
SDragon42 <nathen.silver@gmail.com>
2021-11-27 09:10:36 -0700

    Reworked day 10

--- 2020/Python/day10.py
+++ 2020/Python/day10.py
@@ -1,94 +1,93 @@
-import sys
 from typing import List, Dict
 
-sys.path.append('../../Python.Common')
 import helper
 import inputHelper
-
-
-def get_jolt_differences(adapters: List[int]) -> Dict[int, int]:
-    diffDict: Dict[int, int] = {}
-    
-    last = 0
-    for a in adapters:
-        diff = a - last
-        if diff in diffDict:
-            diffDict[diff] = diffDict[diff] + 1
-        else:
-            diffDict[diff] = 1
-        last = a
-    return diffDict
-
-
-def get_next_possible_adapters(current: int, adapters: List[int]) -> List[int]:
-    result: List[int] = []
-    for x in adapters:
-        if x > current and x <= current + 3:
-            result.append(x)
-    return result
-
-
-def count_adapterChains(current:int, adapters: List[int], chainsUnder: Dict[int, int]) -> int:
-    if current in chainsUnder:
-        return chainsUnder[current]
-
-    nextAdapters = get_next_possible_adapters(current, adapters)
-    if len(nextAdapters) == 0:
-        return 1
-
-    result = 0
-    for x in nextAdapters:
-        result += count_adapterChains(x, adapters, chainsUnder)
-    chainsUnder[current] = result
-    return result
-
-
-def run_part1(title: str, adapters: List[int], correctResult: int):
-    adapters = sorted(adapters)
-    adapters.append(max(adapters) + 3)
-
-    diffCounts = get_jolt_differences(adapters)
-    result = diffCounts[1] * diffCounts[3]
-
-    helper.validate_result(title, result, correctResult)
-
-
-def run_part2(title: str, adapters: List[int], correctResult: int):
-    chainsUnder: Dict[int, int] = {}
-    adapters = sorted(adapters)
-    
-    result = count_adapterChains(0, adapters, chainsUnder)
-    
-    helper.validate_result(title, result, correctResult)
+from puzzleBase import PuzzleBase
 
 
 
-def solve():
-    print("Day 10: Adapter Array")
-    print("")
+class InputData:
+    input: List[int] = []
+    expectedAnswer: int = None
 
-    # run_part1("Test Case 1",
-    #     inputHelper.read_input_as_int_list(10, "example1"),
-    #     35)
-    # run_part1("Test Case 2",
-    #     inputHelper.read_input_as_int_list(10, "example2"),
-    #     220)
-    run_part1("Part 1)",
-        inputHelper.read_input_as_int_list(10, "input"),
-        2414)
-
-    print("")
-
-    # run_part2("Test Case 1",
-    #     inputHelper.read_input_as_int_list(10, "example1"),
-    #     8)
-    # run_part2("Test Case 2",
-    #     inputHelper.read_input_as_int_list(10, "example2"),
-    #     19208)
-    run_part2("Part 2)",
-        inputHelper.read_input_as_int_list(10, "input"),
-        21156911906816)
+    def __init__(self, name: str, part: int) -> None:
+        day = 10
+        self.input = [int(l) for l in inputHelper.load_input_file(day, name)]
+        
+        lines = inputHelper.load_answer_file(day, part, name)
+        self.expectedAnswer = int(lines[0]) if lines is not None else None
 
 
-if __name__ == "__main__":
-    solve()
+
+class Puzzle(PuzzleBase):
+
+    def get_jolt_differences(self, adapters: List[int]) -> Dict[int, int]:
+        diffDict: Dict[int, int] = {}
+        
+        last = 0
+        for a in adapters:
+            diff = a - last
+            if diff in diffDict:
+                diffDict[diff] = diffDict[diff] + 1
+            else:
+                diffDict[diff] = 1
+            last = a
+        return diffDict
+
+
+    def get_next_possible_adapters(self, current: int, adapters: List[int]) -> List[int]:
+        result: List[int] = []
+        for x in adapters:
+            if x > current and x <= current + 3:
+                result.append(x)
+        return result
+
+
+    def count_adapterChains(self, current:int, adapters: List[int], chainsUnder: Dict[int, int]) -> int:
+        if current in chainsUnder:
+            return chainsUnder[current]
+
+        nextAdapters = self.get_next_possible_adapters(current, adapters)
+        if len(nextAdapters) == 0:
+            return 1
+
+        result = 0
+        for x in nextAdapters:
+            result += self.count_adapterChains(x, adapters, chainsUnder)
+        chainsUnder[current] = result
+        return result
+
+
+    def run_part1(self, data: InputData) -> str:
+        adapters = sorted(data.input)
+        adapters.append(max(adapters) + 3)
+
+        diffCounts = self.get_jolt_differences(adapters)
+        result = diffCounts[1] * diffCounts[3]
+
+        return helper.validate_result2('What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?', result, data.expectedAnswer)
+
+
+    def run_part2(self, data: InputData) -> str:
+        chainsUnder: Dict[int, int] = {}
+        adapters = sorted(data.input)
+        
+        result = self.count_adapterChains(0, adapters, chainsUnder)
+        
+        return helper.validate_result2('What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?', result, data.expectedAnswer)
+
+
+
+    def solve(self):
+        print("Day 10: Adapter Array")
+        print("")
+
+        self.run_example(lambda: "P1 Ex1) " + self.run_part1(InputData('example1', 1)))
+        self.run_example(lambda: "P1 Ex2) " + self.run_part1(InputData('example2', 1)))
+        self.run_problem(lambda: "Part 1) " + self.run_part1(InputData('input', 1)))
+
+        print("")
+
+        self.run_example(lambda: "P2 Ex1) " + self.run_part2(InputData('example1', 2)))
+        self.run_example(lambda: "P2 Ex2) " + self.run_part2(InputData('example2', 2)))
+        self.run_problem(lambda: "Part 2) " + self.run_part2(InputData('input', 2)))
