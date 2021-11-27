06f3d1f1f4aca3e0a2e6f09fff6ea5c298c44eb6
SDragon42 <nathen.silver@gmail.com>
2021-11-27 14:29:43 -0700

    Day 12 rework

--- 2020/Python/day12.py
+++ 2020/Python/day12.py
@@ -1,213 +1,216 @@
-import sys
 from typing import List
 
-sys.path.append('../../Python.Common')
 import helper
 import inputHelper
+from puzzleBase import PuzzleBase
+
+
+
+class InputData:
+    input: List[str]
+    expectedAnswer: int
+
+    def __init__(self, name: str, part: int) -> None:
+        day = 12
+        self.input = inputHelper.load_input_file(day, name)
+        
+        lines = inputHelper.load_answer_file(day, part, name)
+        self.expectedAnswer = int(lines[0]) if lines is not None else None
+
 
 
 class WaypointPosition:
-    x = 0
-    y = 0
+    x: int
+    y: int
 
     def __init__(self, x: int, y:int):
         self.x = x
         self.y = y
 
 
+
 class Position:
-    x = 0
-    y = 0
-    heading = ""
-    waypoint: WaypointPosition = WaypointPosition(0,0)
+    x: int
+    y: int
+    heading: str
+    waypoint: WaypointPosition
 
     def __init__(self, x: int, y:int, heading: str):
         self.x = x
         self.y = y
         self.heading = heading
+        self.waypoint = WaypointPosition(0,0)
 
 
-#-------------------------------------------------------------------------------
+
+class Puzzle(PuzzleBase):
+    cardinals = ["N", "E", "S", "W"]
 
 
-def move_north(currentPos: Position, value: int) -> Position:
-    newPos = Position(currentPos.x, currentPos.y + value, currentPos.heading)
-    return newPos
+    def move_north(self, currentPos: Position, value: int) -> Position:
+        newPos = Position(currentPos.x, currentPos.y + value, currentPos.heading)
+        return newPos
 
-def move_south(currentPos: Position, value: int) -> Position:
-    newPos = Position(currentPos.x, currentPos.y - value, currentPos.heading)
-    return newPos
+    def move_south(self, currentPos: Position, value: int) -> Position:
+        newPos = Position(currentPos.x, currentPos.y - value, currentPos.heading)
+        return newPos
 
-def move_east(currentPos: Position, value: int) -> Position:
-    newPos = Position(currentPos.x + value, currentPos.y, currentPos.heading)
-    return newPos
+    def move_east(self, currentPos: Position, value: int) -> Position:
+        newPos = Position(currentPos.x + value, currentPos.y, currentPos.heading)
+        return newPos
 
-def move_west(currentPos: Position, value: int) -> Position:
-    newPos = Position(currentPos.x - value, currentPos.y, currentPos.heading)
-    return newPos
+    def move_west(self, currentPos: Position, value: int) -> Position:
+        newPos = Position(currentPos.x - value, currentPos.y, currentPos.heading)
+        return newPos
 
-def get_cardinal(heading: str, steps: int) -> str:
-    i = cardinals.index(heading)
-    i += steps
-    if i < 0:
-        i += len(cardinals)
-    elif i >= len(cardinals):
-        i -= len(cardinals)
-    return cardinals[i]
+    def get_cardinal(self, heading: str, steps: int) -> str:
+        i = self.cardinals.index(heading)
+        i += steps
+        if i < 0:
+            i += len(self.cardinals)
+        elif i >= len(self.cardinals):
+            i -= len(self.cardinals)
+        return self.cardinals[i]
 
-def turn_left(currentPos: Position, value: int) -> Position:
-    steps = int((value / 360) * 4) * -1
-    newCardinal = get_cardinal(currentPos.heading, steps)
-    return Position(currentPos.x, currentPos.y, newCardinal)
+    def turn_left(self, currentPos: Position, value: int) -> Position:
+        steps = int((value / 360) * 4) * -1
+        newCardinal = self.get_cardinal(currentPos.heading, steps)
+        return Position(currentPos.x, currentPos.y, newCardinal)
 
-def turn_right(currentPos: Position, value: int) -> Position:
-    steps = int((value / 360) * 4)
-    newCardinal = get_cardinal(currentPos.heading, steps)
-    return Position(currentPos.x, currentPos.y, newCardinal)
-
-
-#-------------------------------------------------------------------------------
+    def turn_right(self, currentPos: Position, value: int) -> Position:
+        steps = int((value / 360) * 4)
+        newCardinal = self.get_cardinal(currentPos.heading, steps)
+        return Position(currentPos.x, currentPos.y, newCardinal)
 
 
-def move_waypoint_north(currentPos: Position, value: int) -> Position:
-    wp = currentPos.waypoint
-    currentPos.waypoint = WaypointPosition(wp.x, wp.y + value)
-    return currentPos
-
-def move_waypoint_south(currentPos: Position, value: int) -> Position:
-    wp = currentPos.waypoint
-    currentPos.waypoint = WaypointPosition(wp.x, wp.y - value)
-    return currentPos
-
-def move_waypoint_east(currentPos: Position, value: int) -> Position:
-    wp = currentPos.waypoint
-    currentPos.waypoint = WaypointPosition(wp.x + value, wp.y)
-    return currentPos
-
-def move_waypoint_west(currentPos: Position, value: int) -> Position:
-    wp = currentPos.waypoint
-    currentPos.waypoint = WaypointPosition(wp.x - value, wp.y)
-    return currentPos
+    #-------------------------------------------------------------------------------
 
 
-def rotate_waypoint_left(currentPos: Position, value: int) -> Position:
-    wp = currentPos.waypoint
-    if value == 270:
-        return rotate_waypoint_right(currentPos, 90)
-    elif value == 180:
-        currentPos.waypoint = WaypointPosition(wp.x * -1, wp.y * -1)
-    else:
-        currentPos.waypoint = WaypointPosition(wp.y * -1, wp.x)
+    def move_waypoint_north(self, currentPos: Position, value: int) -> Position:
+        wp = currentPos.waypoint
+        currentPos.waypoint = WaypointPosition(wp.x, wp.y + value)
+        return currentPos
 
-    return currentPos
+    def move_waypoint_south(self, currentPos: Position, value: int) -> Position:
+        wp = currentPos.waypoint
+        currentPos.waypoint = WaypointPosition(wp.x, wp.y - value)
+        return currentPos
 
-def rotate_waypoint_right(currentPos: Position, value: int) -> Position:
-    wp = currentPos.waypoint
-    if value == 270:
-        return rotate_waypoint_left(currentPos, 90)
-    elif value == 180:
-        currentPos.waypoint = WaypointPosition(wp.x * -1, wp.y * -1)
-    else:
-        currentPos.waypoint = WaypointPosition(wp.y, wp.x * -1)
+    def move_waypoint_east(self, currentPos: Position, value: int) -> Position:
+        wp = currentPos.waypoint
+        currentPos.waypoint = WaypointPosition(wp.x + value, wp.y)
+        return currentPos
 
-    return currentPos
+    def move_waypoint_west(self, currentPos: Position, value: int) -> Position:
+        wp = currentPos.waypoint
+        currentPos.waypoint = WaypointPosition(wp.x - value, wp.y)
+        return currentPos
 
 
-def full_steam_ahead(currentPos: Position, value: int) -> Position:
-    currentPos.x += currentPos.waypoint.x * value
-    currentPos.y += currentPos.waypoint.y * value
-    return currentPos
+    def rotate_waypoint_left(self, currentPos: Position, value: int) -> Position:
+        wp = currentPos.waypoint
+        if value == 270:
+            return self.rotate_waypoint_right(currentPos, 90)
+        elif value == 180:
+            currentPos.waypoint = WaypointPosition(wp.x * -1, wp.y * -1)
+        else:
+            currentPos.waypoint = WaypointPosition(wp.y * -1, wp.x)
+
+        return currentPos
+
+    def rotate_waypoint_right(self, currentPos: Position, value: int) -> Position:
+        wp = currentPos.waypoint
+        if value == 270:
+            return self.rotate_waypoint_left(currentPos, 90)
+        elif value == 180:
+            currentPos.waypoint = WaypointPosition(wp.x * -1, wp.y * -1)
+        else:
+            currentPos.waypoint = WaypointPosition(wp.y, wp.x * -1)
+
+        return currentPos
 
 
-#-------------------------------------------------------------------------------
+    def full_steam_ahead(self, currentPos: Position, value: int) -> Position:
+        currentPos.x += currentPos.waypoint.x * value
+        currentPos.y += currentPos.waypoint.y * value
+        return currentPos
 
 
-def move_ship(currentPos: Position, code: str, value: int, path: List[Position]) -> Position:
-    if code == "F":
-        code = currentPos.heading
-
-    newPos = actions[code](currentPos, value)
-    path.append(newPos)
-    return newPos
-    
-
-def calc_manhattan_distance(x: int, y: int) -> int:
-    return abs(x) + abs(y)
+    #-------------------------------------------------------------------------------
 
 
-def run_part1(title: str, input: List[str], correctResult: int):
-    currentPos = Position(0, 0, "E")
-    path: List[Position] = []
-    path.append(currentPos)
+    def move_ship(self, currentPos: Position, code: str, value: int, path: List[Position]) -> Position:
+        if code == "F":
+            code = currentPos.heading
 
-    for data in input:
-        code = data[0]
-        num = int(data[1:])
-        currentPos = move_ship(currentPos, code, num, path)
+        newPos = self.actions[code](self, currentPos, value)
+        path.append(newPos)
+        return newPos
+        
 
-    result = calc_manhattan_distance(currentPos.x, currentPos.y)
-    helper.validate_result(title, result, correctResult)
+    def calc_manhattan_distance(self, x: int, y: int) -> int:
+        return abs(x) + abs(y)
 
 
-def run_part2(title: str, input: List[str], correctResult: int):
-    currentPos = Position(0, 0, "E")
-    currentPos.waypoint = WaypointPosition(10, 1)
-
-    path: List[Position] = []
-    path.append(currentPos)
-
-    for data in input:
-        code = data[0]
-        num = int(data[1:])
-        currentPos = actionsP2[code](currentPos, num)
+    def run_part1(self, data: InputData) -> str:
+        currentPos = Position(0, 0, "E")
+        path: List[Position] = []
         path.append(currentPos)
 
-    result = calc_manhattan_distance(currentPos.x, currentPos.y)
-    helper.validate_result(title, result, correctResult)
+        for line in data.input:
+            code = line[0]
+            num = int(line[1:])
+            currentPos = self.move_ship(currentPos, code, num, path)
+
+        result = self.calc_manhattan_distance(currentPos.x, currentPos.y)
+        return helper.validate_result('What is the Manhattan distance between that location and the ship''s starting position?', result, data.expectedAnswer)
 
 
-cardinals = ["N", "E", "S", "W"]
+    def run_part2(self, data: InputData) -> str:
+        currentPos = Position(0, 0, "E")
+        currentPos.waypoint = WaypointPosition(10, 1)
 
-actions = {
-    "N": move_north,
-    "S": move_south,
-    "E": move_east,
-    "W": move_west,
-    "L": turn_left,
-    "R": turn_right
-}
+        path: List[Position] = []
+        path.append(currentPos)
 
-actionsP2 = {
-    "N": move_waypoint_north,
-    "S": move_waypoint_south,
-    "E": move_waypoint_east,
-    "W": move_waypoint_west,
-    "L": rotate_waypoint_left,
-    "R": rotate_waypoint_right,
-    "F": full_steam_ahead
-}
+        for line in data.input:
+            code = line[0]
+            num = int(line[1:])
+            currentPos = self.actionsP2[code](self, currentPos, num)
+            path.append(currentPos)
 
-def solve():
-    day = 12
-    print(f"Day {day}: Rain Risk")
-    print("")
-
-    # run_part1("Test Case 1",
-    #     inputHelper.read_input_as_list(day, "example1"),
-    #     25)
-    run_part1("Part 1)",
-        inputHelper.read_input_as_list(day, "input"),
-        1441)
-
-    print("")
-
-    # run_part2("Test Case 1",
-    #     inputHelper.read_input_as_list(day, "example1"),
-    #     286)
-    run_part2("Part 2)",
-        inputHelper.read_input_as_list(day, "input"),
-        61616)
+        result = self.calc_manhattan_distance(currentPos.x, currentPos.y)
+        return helper.validate_result('What is the Manhattan distance between that location and the ship''s starting position?', result, data.expectedAnswer)
 
 
-if __name__ == "__main__":
-    solve()
+    actions = {
+        "N": move_north,
+        "S": move_south,
+        "E": move_east,
+        "W": move_west,
+        "L": turn_left,
+        "R": turn_right
+    }
+
+    actionsP2 = {
+        "N": move_waypoint_north,
+        "S": move_waypoint_south,
+        "E": move_waypoint_east,
+        "W": move_waypoint_west,
+        "L": rotate_waypoint_left,
+        "R": rotate_waypoint_right,
+        "F": full_steam_ahead
+    }
+
+
+    def solve(self):
+        print("Day 12: Rain Risk")
+        print("")
+
+        self.run_example(lambda: "P1 Ex1) " + self.run_part1(InputData('example1', 1)))
+        self.run_problem(lambda: "Part 1) " + self.run_part1(InputData('input', 1)))
+
+        print("")
+
+        self.run_example(lambda: "P2 Ex1) " + self.run_part2(InputData('example1', 2)))
+        self.run_problem(lambda: "Part 2) " + self.run_part2(InputData('input', 2)))
