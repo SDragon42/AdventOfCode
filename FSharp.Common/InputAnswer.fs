55d4eb2edc7492209278b80c2a91ad44c7fabe5d
SDragon42 <nathen.silver@gmail.com>
2021-11-29 16:28:37 -0700

    Removed commented C# code

--- FSharp.Common/InputAnswer.fs
+++ FSharp.Common/InputAnswer.fs
@@ -10,36 +10,3 @@ type InputAnswer<'TI, 'TA> (input: 'TI, expectedAnswer: 'TA) =
     
     member this.Input = input
     member this.ExpectedAnswer = expectedAnswer
-
-
-//public class InputAnswer<TI, TA>
-//{
-//    public InputAnswer() { }
-//    public InputAnswer(TI input, TA expectedAnswer)
-//    {
-//        Input = input;
-//        ExpectedAnswer = expectedAnswer;
-//    }
-
-//    private TI _input;
-//    public TI Input
-//    {
-//        get => _input;
-//        set => SetInput(value);
-//    }
-//    protected virtual void SetInput(TI value)
-//    {
-//        _input = value;
-//    }
-
-//    private TA _expectedAnswer;
-//    public TA ExpectedAnswer
-//    {
-//        get => _expectedAnswer;
-//        set => SetExpectedAnswer(value);
-//    }
-//    protected virtual void SetExpectedAnswer(TA value)
-//    {
-//        _expectedAnswer = value;
-//    }
-//}
