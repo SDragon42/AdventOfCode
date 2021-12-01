e1769a942af16a80ec1b39e0dff976312d1a4bdb
SDragon42 <nathen.silver@gmail.com>
2021-11-30 20:59:24 -0700

    Tweaked the PuzzleBase.

--- 2019/FSharp/Day01.fs
+++ 2019/FSharp/Day01.fs
@@ -59,26 +59,16 @@ type Day01 (runBenchmarks, runExamples) =
         this.ShowResult(totalFuel, puzzleData.ExpectedAnswer)
 
 
-    member private this.Example1 : string = " Ex. 1) " + this.RunPart1(this.GetPuzzleInput(1, "example1"))
-    member private this.Example2 : string = " Ex. 2) " + this.RunPart1(this.GetPuzzleInput(1, "example2"))
-    member private this.Example3 : string = " Ex. 3) " + this.RunPart1(this.GetPuzzleInput(1, "example3"))
-    member private this.Example4 : string = " Ex. 4) " + this.RunPart1(this.GetPuzzleInput(1, "example4"))
-    member private this.Part1 : string = "Part 1) " + this.RunPart1(this.GetPuzzleInput(1, "input"))
-
-    member private this.Example2P2 : string = " Ex. 2) " + this.RunPart2(this.GetPuzzleInput(2, "example2"))
-    member private this.Example3P2 : string = " Ex. 3) " + this.RunPart2(this.GetPuzzleInput(2, "example3"))
-    member private this.Part2 : string = "Part 2) " + this.RunPart2(this.GetPuzzleInput(2, "input"))
-
     override this.SolvePuzzle _ = seq {
         yield "Day 1: The Tyranny of the Rocket Equation"
-        yield this.RunExample(fun _ -> this.Example1)
-        yield this.RunExample(fun _ -> this.Example2)
-        yield this.RunExample(fun _ -> this.Example3)
-        yield this.RunExample(fun _ -> this.Example4)
-        yield this.Run(fun _ -> this.Part1)
+        yield this.RunExample(fun _ -> " Ex. 1) " + this.RunPart1(this.GetPuzzleInput(1, "example1")))
+        yield this.RunExample(fun _ -> " Ex. 2) " + this.RunPart1(this.GetPuzzleInput(1, "example2")))
+        yield this.RunExample(fun _ -> " Ex. 3) " + this.RunPart1(this.GetPuzzleInput(1, "example3")))
+        yield this.RunExample(fun _ -> " Ex. 4) " + this.RunPart1(this.GetPuzzleInput(1, "example4")))
+        yield this.RunProblem(fun _ -> "Part 1) " + this.RunPart1(this.GetPuzzleInput(1, "input")))
 
         yield ""
-        yield this.RunExample(fun _ -> this.Example2P2)
-        yield this.RunExample(fun _ -> this.Example3P2)
-        yield this.Run(fun _ -> this.Part2)
+        yield this.RunExample(fun _ -> " Ex. 2) " + this.RunPart2(this.GetPuzzleInput(2, "example2")))
+        yield this.RunExample(fun _ -> " Ex. 3) " + this.RunPart2(this.GetPuzzleInput(2, "example3")))
+        yield this.RunProblem(fun _ -> "Part 2) " + this.RunPart2(this.GetPuzzleInput(2, "input")))
         }
