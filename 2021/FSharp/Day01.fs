302638d632a4514782abcdd0cee0e27029e6186c
SDragon42 <nathen.silver@gmail.com>
2021-11-30 23:07:56 -0700

    Renamed Day01PuzzleInput type

--- 2021/FSharp/Day01.fs
+++ 2021/FSharp/Day01.fs
@@ -6,7 +6,7 @@ open System
 open System.Linq
 
 
-type private Day01PuzzleInput(input: int[], expectedAnswer: int option) = 
+type private PuzzleInput(input: int[], expectedAnswer: int option) = 
     inherit InputAnswer<int [], int option>(input, expectedAnswer)
 
 
@@ -27,7 +27,7 @@ type Day01 (runBenchmarks, runExamples) =
                 | ex -> None
         let answer = GetAnswer(name)
         
-        new Day01PuzzleInput(input, answer)
+        new PuzzleInput(input, answer)
 
 
     member private this.CheckMeasurement (windowSize: int, last: int, remaining: int[]) =
@@ -39,12 +39,12 @@ type Day01 (runBenchmarks, runExamples) =
             value + this.CheckMeasurement(windowSize, next, remaining[1..])
 
 
-    member private this.RunPart1 (puzzleData: Day01PuzzleInput) =
+    member private this.RunPart1 (puzzleData: PuzzleInput) =
         let result = this.CheckMeasurement(1, 0, puzzleData.Input)
         Helper.GetPuzzleResultText("How many measurements are larger than the previous measurement?", result, puzzleData.ExpectedAnswer)
 
 
-    member private this.RunPart2 (puzzleData: Day01PuzzleInput) =
+    member private this.RunPart2 (puzzleData: PuzzleInput) =
         let result = this.CheckMeasurement(3, 0, puzzleData.Input)
         Helper.GetPuzzleResultText("How many sums are larger than the previous sum?", result, puzzleData.ExpectedAnswer)
 
