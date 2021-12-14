3b18cb017643833445f59ead2f7011f8381ffdc3
SDragon42 <nathen.silver@gmail.com>
2021-12-13 21:27:08 -0700

    Day 1 - Changed to function composition

--- 2021/FSharp/Day01.fs
+++ 2021/FSharp/Day01.fs
@@ -12,7 +12,7 @@ type Day01 (runBenchmarks, runExamples) =
     inherit PuzzleBase(runBenchmarks, runExamples)
 
 
-    member private this.GetPuzzleInput (part: int, name: string) =
+    member private this.GetPuzzleInput (part:int) (name:string) =
         let day = 1
 
         let input = 
@@ -27,44 +27,34 @@ type Day01 (runBenchmarks, runExamples) =
         new PuzzleInput(input, answer)
 
 
-    // member private this.CheckMeasurement (windowSize: int, last: int, remaining: int list) =
-    //     if remaining.Length < windowSize then
-    //         0
-    //     else
-    //         let next = remaining[..windowSize - 1] |> List.sum
-    //         let value = if next > last && last > 0 then 1 else 0
-    //         value + this.CheckMeasurement(windowSize, next, remaining[1..])
-
-
-    member private this.CountIncreases (input: int list) =
-        input
-            |> List.pairwise
-            |> List.where (fun (a,b) -> a < b)
-            |> List.length
+    member private this.CountIncreases = 
+        List.pairwise 
+        >> List.filter (fun (a,b) -> a < b) 
+        >> List.length
 
 
     member private this.RunPart1 (puzzleData: PuzzleInput) =
-        // let result = this.CheckMeasurement(1, 0, puzzleData.Input)
-        let result = puzzleData.Input |> this.CountIncreases
+        let result = 
+            puzzleData.Input
+            |> this.CountIncreases
         Helper.GetPuzzleResultText("How many measurements are larger than the previous measurement?", result, puzzleData.ExpectedAnswer)
 
 
     member private this.RunPart2 (puzzleData: PuzzleInput) =
-        // let result = this.CheckMeasurement(3, 0, puzzleData.Input)
         let result =
             puzzleData.Input
             |> List.windowed(3)
-            |> List.map (fun t -> t |> List.sum)
+            |> List.map List.sum
             |> this.CountIncreases
         Helper.GetPuzzleResultText("How many sums are larger than the previous sum?", result, puzzleData.ExpectedAnswer)
 
 
     override this.SolvePuzzle _ = seq {
         yield "Day 1: Sonar Sweep"
-        yield this.RunExample(fun _ -> " Ex. 1) " + this.RunPart1(this.GetPuzzleInput(1, "example1")))
-        yield this.RunProblem(fun _ -> "Part 1) " + this.RunPart1(this.GetPuzzleInput(1, "input")))
+        yield this.RunExample(fun _ -> " Ex. 1) " + this.RunPart1 (this.GetPuzzleInput 1 "example1"))
+        yield this.RunProblem(fun _ -> "Part 1) " + this.RunPart1 (this.GetPuzzleInput 1 "input"))
 
         yield ""
-        yield this.RunExample(fun _ -> " Ex. 1) " + this.RunPart2(this.GetPuzzleInput(2, "example1")))
-        yield this.RunProblem(fun _ -> "Part 2) " + this.RunPart2(this.GetPuzzleInput(2, "input")))
-        }
+        yield this.RunExample(fun _ -> " Ex. 1) " + this.RunPart2 (this.GetPuzzleInput 2 "example1"))
+        yield this.RunProblem(fun _ -> "Part 2) " + this.RunPart2 (this.GetPuzzleInput 2 "input"))
+       }
