95cf1404859e46520484fb7e6aaadd38e6e15509
SDragon42 <nathen.silver@gmail.com>
2022-04-17 21:05:28 -0600

    Changed Day01 to be unit test based.

---
+++ 2019/CSharp-Tests/Day01Test.cs
@@ -0,0 +1,69 @@
+namespace AdventOfCode.CSharp.Year2019.Tests;
+
+public class Day01Test
+{
+    const int DAY = 1;
+    private readonly ITestOutputHelper output;
+
+
+    public Day01Test(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
+    List<int> GetInput(string name)
+    {
+        var input = InputHelper.LoadInputFile(DAY, name)
+            .Select(l => l.ToInt32())
+            .ToList();
+        return input;
+    }
+    int? GetExpectedAnswer(int part, string name)
+    {
+        var expected = InputHelper.LoadAnswerFile(DAY, part, name)
+            ?.FirstOrDefault()
+            ?.ToInt32();
+        return expected;
+    }
+
+
+    [Theory]
+    [InlineData("example1")]
+    [InlineData("example2")]
+    [InlineData("example3")]
+    [InlineData("example4")]
+    [InlineData("input")]
+    public void Part1(string inputName)
+    {
+        var puzzle = new Day01();
+
+        var input = GetInput(inputName);
+        var expected = GetExpectedAnswer(1, inputName);
+
+        var value = puzzle.CalcFuelForMasses(input);
+
+        output.WriteLine($"Answer: {value}");
+        
+        Assert.Equal(expected, value);
+    }
+
+    [Theory]
+    [InlineData("example2")]
+    [InlineData("example3")]
+    [InlineData("input")]
+    public void Part2(string inputName)
+    {
+        var puzzle = new Day01();
+
+        var input = GetInput(inputName);
+        var expected = GetExpectedAnswer(2, inputName);
+
+        var value = puzzle.CalcTotalFuelForMasses(input);
+
+        output.WriteLine($"Answer: {value}");
+
+        Assert.Equal(expected, value);
+    }
+
+
+}
---
+++ 2019/CSharp-Tests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using System.Collections.Generic;
+global using System.Linq;
+
+global using Xunit;
+global using Xunit.Abstractions;
+
+global using AdventOfCode.CSharp.Common;
+global using AdventOfCode.CSharp.Year2019;
