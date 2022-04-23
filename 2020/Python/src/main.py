ac6fff359c783f6c1fb93f0b01866fce82fe71f6
Nathen Silver <nathen.silver@gmail.com>
2022-04-22 20:52:06 -0600

    converted day 3 & 4 to unit tests

--- 2020/Python/src/main.py
+++ 2020/Python/src/main.py
@@ -4,14 +4,12 @@ from typing import Dict
 from puzzleRunner import PuzzleRunner
 from puzzleBase import PuzzleBase
 
-import day03, day04, day05, day06, day07, day08, day09, day10, \
+import day05, day06, day07, day08, day09, day10, \
        day11, day12, day13, day14, day15, day16, day17, day18, day19, day20, \
        day21, day22, day23, day24, day25
 import day22fast
 
 puzzleDict: Dict[int, PuzzleBase] = {
-    3: day03.Puzzle,
-    4: day04.Puzzle,
     5: day05.Puzzle,
     6: day06.Puzzle,
     7: day07.Puzzle,
---
+++ 2020/Python/tests/test_day03.py
@@ -0,0 +1,45 @@
+import unittest
+from typing import List
+
+# this adds the 'src' folder to the path. Needed to get the src imports to work in unit tests.
+import config
+
+import inputHelper
+import src.day03 as puzzle
+
+
+_DAY = 3
+
+class Day03(unittest.TestCase):
+    def get_test_data(self, name:str, part:int) -> tuple[List[str], List[str], int]:
+        input = inputHelper.load_file(_DAY, name).splitlines()
+
+        slopes = inputHelper.load_file(_DAY, f'slopes{part}').split(',')
+
+        expectedAnswer = inputHelper.load_file(_DAY, f"{name}-answer{part}")
+        if expectedAnswer is not None:
+            expectedAnswer = int(expectedAnswer)
+
+        return input, slopes, expectedAnswer
+
+
+    def test_part1_example1(self):
+        input, slopes, expected = self.get_test_data("example1", 1)
+        answer = puzzle.do_it(input, slopes)
+        self.assertEqual(answer, expected)
+
+    def test_part1(self):
+        input, slopes, expected = self.get_test_data("input", 1)
+        answer = puzzle.do_it(input, slopes)
+        self.assertEqual(answer, expected)
+
+
+    def test_part2_example1(self):
+        input, slopes, expected = self.get_test_data("example1", 2)
+        answer = puzzle.do_it(input, slopes)
+        self.assertEqual(answer, expected)
+
+    def test_part2(self):
+        input, slopes, expected = self.get_test_data("input", 2)
+        answer = puzzle.do_it(input, slopes)
+        self.assertEqual(answer, expected)
