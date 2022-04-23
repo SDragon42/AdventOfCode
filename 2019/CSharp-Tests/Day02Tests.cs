0f4adbcac5883d66c215a2b38f3e142e67931fc8
SDragon42 <nathen.silver@gmail.com>
2022-04-22 21:18:50 -0600

    Combined test data methods.

--- 2019/CSharp-Tests/Day02Tests.cs
+++ 2019/CSharp-Tests/Day02Tests.cs
@@ -4,21 +4,19 @@ public class Day02Tests : TestBase
 {
     public Day02Tests(ITestOutputHelper output) : base(output, 2) { }
 
-    List<long> GetInput(string name)
+    private (List<long>, long?) GetTestData(string name, int part)
     {
         var input = InputHelper.LoadInputFile(DAY, name)
             .First()
             .Split(',')
             .Select(v => v.ToInt64())
             .ToList();
-        return input;
-    }
-    long? GetExpectedAnswer(int part, string name)
-    {
+
         var expected = InputHelper.LoadAnswerFile(DAY, part, name)
             ?.FirstOrDefault()
             ?.ToInt64();
-        return expected;
+
+        return (input, expected);
     }
 
     [Theory]
@@ -26,11 +24,9 @@ public class Day02Tests : TestBase
     [InlineData("input", 12, 2)]
     public void Part1(string inputName, int valueAt1, int valueAt2)
     {
+        var (input, expected) = GetTestData(inputName, 1);
+
         var puzzle = new Day02();
-
-        var input = GetInput(inputName);
-        var expected = GetExpectedAnswer(1, inputName);
-
         var value = puzzle.RunCode(input, valueAt1, valueAt2);
 
         output.WriteLine($"Value as position 0 : {value}");
@@ -42,11 +38,9 @@ public class Day02Tests : TestBase
     [InlineData("input", 19690720)]
     public void Part2(string inputName, long valueAt0)
     {
+        var (input, expected) = GetTestData(inputName, 2);
+
         var puzzle = new Day02();
-
-        var input = GetInput(inputName);
-        var expected = GetExpectedAnswer(2, inputName);
-
         var value = puzzle.FindNounVerb(input, valueAt0);
 
         output.WriteLine($"Noun-Verb pair is : {value}");
