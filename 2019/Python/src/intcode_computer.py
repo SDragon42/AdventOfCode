c2ca8baad04fec5ab73a04b634551ba95127ef91
nathen.silver <nathen.silver@ruralsourcing.com>
2022-04-27 10:35:48 -0600

    Added custom exceptions, output listener support (initial), and testing for exceptions.

--- 2019/Python/src/intcode_computer.py
+++ 2019/Python/src/intcode_computer.py
@@ -1,10 +1,5 @@
-from cgitb import reset
-from distutils.log import error
-from typing import Callable, Dict, List, Tuple
+from typing import Callable, Dict, List
 from enum import Enum
-
-# CallTuple = Tuple[Callable, int]
-# InstructionDict = Dict[str, Callable]
 
 
 class IntCodeState(Enum):
@@ -16,21 +11,44 @@ class IntCodeState(Enum):
     NeedsInput = 2
 
 
+class IntCodeStateException(Exception):
+    MESSAGE_FINISHED = 'Already Finished'
+    MESSAGE_NEEDS_INPUT = 'Needs Input'
+    MESSAGE_UNKNOWN = 'Unknown state issue'
+
+    def __init__(self, state:IntCodeState) -> None:
+        if state == IntCodeState.Finished:
+            super().__init__(self.MESSAGE_FINISHED)
+        elif state == IntCodeState.NeedsInput:
+            super().__init__(self.MESSAGE_NEEDS_INPUT)
+        else:
+            super().__init__(self.MESSAGE_UNKNOWN)
+    
+
+class IntCodeOpcodeException(Exception):
+    MESSAGE_UNKNOWN_OPCODE = 'Unknown OpCode'
+    def __init__(self, opcode:int) -> None:
+        super().__init__(self.MESSAGE_UNKNOWN_OPCODE + f' ({opcode})')
+
+
 class IntCode:
     """
     IntCode computer.
     """
 
     _memory:List[int]
-    _instuctions:Dict[str, Callable]
-    _state:IntCodeState
     _position:int
+    _state:IntCodeState
+
+    _instuctions:Dict[str, Callable]
+
+    _outputListeners:List[Callable]
 
     def __init__(self, memory:List[int]) -> None:
-        # self._memory = memory
-        self._memory = list(memory) # maybe use this to make a local copy 
+        self._memory = list(memory)
         self._position = 0
-
+        self._state = IntCodeState.Running
+        
         # setup instructions
         self._instuctions = {
             # <OpCode>: (<function>, <num_parameters>),
@@ -39,20 +57,31 @@ class IntCode:
             99: self._opcode_quit
         }
 
-        self._state = IntCodeState.Running
+        # listeners
+        self._outputListeners = []
+        
+
+
+    def _on_output(self, value:int) -> None:
+        for listener in self._outputListeners:
+            listener(value)
+
+    def bind_output(self, listener:Callable) -> None:
+        if listener not in self._outputListeners:
+            self._outputListeners.append(listener)
 
 
 
     def run(self) -> None:
         if self._state == IntCodeState.Finished:
-            raise error('Already Finished')
+            raise IntCodeStateException(self._state)
         if self._state == IntCodeState.NeedsInput:
-            raise error('Needs Input')
+            raise IntCodeStateException(self._state)
 
         while self._state == IntCodeState.Running:
             opCode = self._read_instruction()
             if opCode not in self._instuctions:
-                raise error('Unknown OpCode', opCode)
+                raise IntCodeOpcodeException(opCode)
                 
             func = self._instuctions[opCode]
             func()
@@ -86,6 +115,5 @@ class IntCode:
 
     
     def _read_instruction(self) -> int:
-        # opCode = self._memory[self._position]
-        # return opCode
-        return self.read_value_at(self._position)
+        opCode = self.read_value_at(self._position)
+        return opCode
