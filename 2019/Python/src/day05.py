02d0dc699b683661e0ad21223b48f1c6fc4074f0
nathen.silver <nathen.silver@ruralsourcing.com>
2022-04-27 17:19:09 -0600

    day 5 part 2, not working fully.

--- 2019/Python/src/day05.py
+++ 2019/Python/src/day05.py
@@ -3,7 +3,7 @@ from typing import List
 from intcode_computer import IntCode, IntCodeState
 
 
-def part1(input:List[int], inputValue:int) -> int:
+def run_code(input:List[int], inputValue:int) -> int:
 
     def output_handler(outputValue):
         nonlocal result
@@ -20,22 +20,3 @@ def part1(input:List[int], inputValue:int) -> int:
         comp.run()
 
     return result
-
-
-# def part2(input:List[int]) -> int:
-    
-#     def output_handler(outputValue):
-#         nonlocal result
-#         result = outputValue
-
-#     result = 0
-
-#     comp = IntCode(input)
-#     comp.add_output_callback(output_handler)
-#     comp.run()
-
-#     if comp.state == IntCodeState.NeedsInput:
-#         comp.add_input(5)
-#         comp.run()
-
-#     return result
--- 2019/Python/tests/test_day05.py
+++ 2019/Python/tests/test_day05.py
@@ -29,38 +29,51 @@ class Day05(unittest.TestCase):
         return input, expectedAnswer
 
 
-    def part1(self, inputName:str, inputValue:int) -> None:
+    def run_day5(self, inputName:str, inputValue:int) -> None:
         input, expected = self._get_test_data(inputName, 1)
-        value = puzzle.part1(input, inputValue)
+        value = puzzle.run_code(input, inputValue)
         self.assertEqual(value, expected)
 
 
     def test_part1_example_1(self):
-        self.part1('example1', 69)
+        self.run_day5('example1', 69)
 
     def test_part1_example_2(self):
-        self.part1('example2', 1)
+        self.run_day5('example2', 1)
 
     def test_part1(self):
-        self.part1('input', 1)
+        self.run_day5('input', 1)
 
 
-    # def part2(self, inputName:str, inputValue:int) -> None:
-    #     input, expected = self._get_test_data(inputName, 1)
-    #     value = puzzle.part1(input, inputValue)
-    #     self.assertEqual(value, expected)
+    def test_part2_examples_3(self):
+        data = [
+            ('3,9,8,9,10,9,4,9,99,-1,8', 8, 1),
+            ('3,9,7,9,10,9,4,9,99,-1,8', 8, 0),
+            ('3,3,1108,-1,8,3,4,3,99', 8, 1),
+            ('3,3,1107,-1,8,3,4,3,99', 8, 0),
 
-    # # def test_part2_example_3a(self):
-    # #     self.part2('example3')
+            ('3,9,8,9,10,9,4,9,99,-1,8', 7, 0),
+            ('3,9,7,9,10,9,4,9,99,-1,8', 7, 1),
+            ('3,3,1108,-1,8,3,4,3,99', 7, 0),
+            ('3,3,1107,-1,8,3,4,3,99', 7, 1),
+        ]
+        for inputData, inputValue, expected in data:
+            input = string_to_int_list(inputData)
+            value = puzzle.run_code(input, inputValue)
+            self.assertEqual(value, expected)
 
-    # # def test_part2_example_3b(self):
-    # #     self.part2('example3')
+    def test_part2_example_4(self):
+        data = [
+            ('3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9', 0, 0),
+            ('3,3,1105,-1,9,1101,0,0,12,4,12,99,1', 0, 0),
 
-    # # def test_part2_example_4a(self):
-    # #     self.part2('example4')
+            ('3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9', 42, 1),
+            ('3,3,1105,-1,9,1101,0,0,12,4,12,99,1', 42, 1),
+        ]
+        for inputData, inputValue, expected in data:
+            input = string_to_int_list(inputData)
+            value = puzzle.run_code(input, inputValue)
+            self.assertEqual(value, expected)
 
-    # # def test_part2_example_4b(self):
-    # #     self.part2('example4')
-
-    # def test_part2(self):
-    #     self.part2('input', 5)
+    def test_part2(self):
+        self.run_day5('input', 5)
