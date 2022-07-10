3214dc130d6c37a53824350ee0dc9c31dab8d0f9
Nathen Silver <nathen.silver@gmail.com>
2022-07-10 15:31:22 -0800

    converted day18 initial, other minor fixes

--- 2019/CSharp/Day18.cs
+++ 2019/CSharp/Day18.cs
@@ -1,57 +1,57 @@
 ﻿namespace AdventOfCode.CSharp.Year2019;
 
 /// <summary>
-/// https://adventofcode.com/2019/day/17
+/// https://adventofcode.com/2019/day/18
 /// </summary>
-class Day18 : PuzzleBase
+public class Day18 : TestBase
 {
-    const int DAY = 18;
+    public Day18(ITestOutputHelper output) : base(output, 18) { }
 
 
-    public override IEnumerable<string> SolvePuzzle()
+    private (List<List<char>>, int?) GetTestData(string name, int part)
     {
-        yield return "Day 18: Many-Worlds Interpretation";
-        yield return string.Empty;
+        var input = InputHelper.LoadInputFile(DAY, name)
+            .Select(l => l.ToList())
+            .ToList();
 
-        yield return RunExample(() => " Ex. 1) " + RunPart1(GetPuzzleData(1, "example1")));
-        yield return RunExample(() => " Ex. 2) " + RunPart1(GetPuzzleData(1, "example2")));
-        yield return RunExample(() => " Ex. 3) " + RunPart1(GetPuzzleData(1, "example3")));
-        yield return RunExample(() => " Ex. 4) " + RunPart1(GetPuzzleData(1, "example4")));
-        yield return RunExample(() => " Ex. 5) " + RunPart1(GetPuzzleData(1, "example5")));
-        //yield return RunProblem(() => "Part 1) " + RunPart1(GetPuzzleData(1, "input")));
+        var expected = InputHelper.LoadAnswerFile(DAY, part, name)
+            ?.FirstOrDefault()
+            ?.ToInt32();
 
-        //yield return string.Empty;
-
-        //yield return RunExample(() => " Ex. 1) " + RunPart1(GetPuzzleData(2, "example1")));
-        //yield return RunProblem(() => "Part 2) " + RunPart1(GetPuzzleData(2, "input")));
+        return (input, expected);
     }
 
 
-    class InputAnswer : InputAnswer<List<List<char>>, int?> { }
-    InputAnswer GetPuzzleData(int part, string name)
+    [Theory]
+    [InlineData("example1")]
+    [InlineData("example2")]
+    [InlineData("example3")]
+    [InlineData("example4")]
+    [InlineData("example5")]
+    // [InlineData("input")]
+    public void Part1(string inputName)
     {
-        var result = new InputAnswer()
-        {
-            Input = InputHelper.LoadInputFile(DAY, name)
-                .Select(l => l.ToList())
-                .ToList(),
-            ExpectedAnswer = InputHelper.LoadAnswerFile(DAY, part, name)?.FirstOrDefault()?.ToInt32()
-        };
+        var (input, expected) = GetTestData(inputName, 1);
 
-        return result;
+        var result = RunPart1(input);
+        output.WriteLine($"How many steps is the shortest path that collects all of the keys? {result}");
+
+        Assert.Equal(expected, result);
     }
 
 
-    string RunPart1(InputAnswer puzzleData)
-    {
-        var answer = 0;
-        return Helper.GetPuzzleResultText($"How many steps is the shortest path that collects all of the keys? {answer}", answer, puzzleData.ExpectedAnswer);
-    }
 
-    string RunPart2(InputAnswer puzzleData)
+    int RunPart1(List<List<char>> input)
     {
         var answer = 0;
-        return Helper.GetPuzzleResultText($": {answer}", answer, puzzleData.ExpectedAnswer);
+        // return Helper.GetPuzzleResultText($"How many steps is the shortest path that collects all of the keys? {answer}", answer, puzzleData.ExpectedAnswer);
+        return answer;
     }
+
+    // string RunPart2(InputAnswer puzzleData)
+    // {
+    //     var answer = 0;
+    //     return Helper.GetPuzzleResultText($": {answer}", answer, puzzleData.ExpectedAnswer);
+    // }
 
 }
