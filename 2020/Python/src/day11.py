cbeada13b975e650608b0d3b5a3f463bd86c6375
SDragon42 <nathen.silver@gmail.com>
2022-07-28 23:38:30 -0600

    day 11 tests

--- 2020/Python/src/day11.py
+++ 2020/Python/src/day11.py
@@ -1,21 +1,6 @@
 from typing import List, Dict
 
 import helper
-import inputHelper
-from puzzleBase import PuzzleBase
-
-
-
-class InputData:
-    input: List[str]
-    expectedAnswer: int
-
-    def __init__(self, name: str, part: int) -> None:
-        day = 11
-        self.input = inputHelper.load_file(day, name).splitlines()
-        
-        answer = inputHelper.load_file(day, f"{name}-answer{part}")
-        self.expectedAnswer = int(answer) if answer is not None else None
 
 
 
@@ -33,177 +18,152 @@ Table = List[List[str]]
 
 
 
-class Puzzle(PuzzleBase):
+# class Puzzle(PuzzleBase):
 
-    directionOffsets: List[Offsets] = [
-        Offsets(-1, -1),
-        Offsets(0, -1),
-        Offsets(1, -1),
-        Offsets(1, 0),
-        Offsets(1, 1),
-        Offsets(0, 1),
-        Offsets(-1, 1),
-        Offsets(-1, 0),
-    ]
+directionOffsets: List[Offsets] = [
+    Offsets(-1, -1),
+    Offsets(0, -1),
+    Offsets(1, -1),
+    Offsets(1, 0),
+    Offsets(1, 1),
+    Offsets(0, 1),
+    Offsets(-1, 1),
+    Offsets(-1, 0),
+]
 
-    # helper functions
-    #--------------------------------------------------------------------------------
-    def clone_table(self, seats: Table) -> Table:
-        cloned = [y.copy() for y in seats]
-        return cloned
-
-
-    def input_list_to_table(self, input: List[str]) -> Table:
-        seats = [list(y) for y in input] 
-        return seats
+# helper functions
+#--------------------------------------------------------------------------------
+def clone_table(seats: Table) -> Table:
+    cloned = [y.copy() for y in seats]
+    return cloned
 
 
-    def show_seat_map(self, seats: Table, title: str):
-        helper.dprint(title)
-        for y in seats:
-            helper.dprint("".join(y))
-        helper.dprint("")
+def input_list_to_table(input: List[str]) -> Table:
+    seats = [list(y) for y in input] 
+    return seats
 
 
-    def count_occupied_seats(self, seats: Table) -> int:
-        total = 0
-        for y in seats:
-            for x in y:
-                if x == "#":
-                    total += 1
-        return total
+def show_seat_map(seats: Table, title: str):
+    helper.dprint(title)
+    for y in seats:
+        helper.dprint("".join(y))
+    helper.dprint("")
 
 
-    def is_inbounds(self, x: int, y: int, seats: Table) -> bool:
-        if x < 0 or y < 0:
-            return False
-        if y >= len(seats):
-            return False
-        if x >= len(seats[y]):
-            return False
-        return True
-
-    def get_seat_in_path(self, x: int, y: int, seats: Table, offset: Offsets) -> str:
-        x += offset.x
-        y += offset.y
-        if not self.is_inbounds(x, y, seats):
-            return ""
-        return seats[y][x]
-        
-
-    def get_first_seat_in_path(self, x: int, y: int, seats: Table, offset: Offsets) -> str:
-        found = self.get_seat_in_path(x, y, seats, offset)
-        if found == "":
-            return "."
-        if found == ".":
-            return self.get_first_seat_in_path(x + offset.x, y + offset.y, seats, offset)
-        return found
+def count_occupied_seats(seats: Table) -> int:
+    total = 0
+    for y in seats:
+        for x in y:
+            if x == "#":
+                total += 1
+    return total
 
 
-    # common rules
-    #--------------------------------------------------------------------------------
-    def rule_floor(self, x: int, y: int, seats: Table) -> str:
-        return seats[y][x]
+def is_inbounds(x: int, y: int, seats: Table) -> bool:
+    if x < 0 or y < 0:
+        return False
+    if y >= len(seats):
+        return False
+    if x >= len(seats[y]):
+        return False
+    return True
+
+def get_seat_in_path(x: int, y: int, seats: Table, offset: Offsets) -> str:
+    x += offset.x
+    y += offset.y
+    if not is_inbounds(x, y, seats):
+        return ""
+    return seats[y][x]
+    
+
+def get_first_seat_in_path(x: int, y: int, seats: Table, offset: Offsets) -> str:
+    found = get_seat_in_path(x, y, seats, offset)
+    if found == "":
+        return "."
+    if found == ".":
+        return get_first_seat_in_path(x + offset.x, y + offset.y, seats, offset)
+    return found
 
 
-    # rules for Part 1
-    #--------------------------------------------------------------------------------
-    def rule_empty_seat_part1(self, x: int, y: int, seats: Table) -> str:
-        for offset in self.directionOffsets:
-            if self.get_seat_in_path(x, y, seats, offset) == "#":
-                return seats[y][x]
-        return "#"
+# common rules
+#--------------------------------------------------------------------------------
+def rule_floor(x: int, y: int, seats: Table) -> str:
+    return seats[y][x]
 
 
-    def rule_occupied_seat_part1(self, x: int, y: int, seats: Table) -> str:
-        numOccupied = 0
-        for offset in self.directionOffsets:
-            if self.get_seat_in_path(x, y, seats, offset) == "#":
-                numOccupied += 1
-        if numOccupied >= 4:
-            return "L"
-        return seats[y][x]
+# rules for Part 1
+#--------------------------------------------------------------------------------
+def rule_empty_seat_part1(x: int, y: int, seats: Table) -> str:
+    for offset in directionOffsets:
+        if get_seat_in_path(x, y, seats, offset) == "#":
+            return seats[y][x]
+    return "#"
 
 
-    # rules for Part 2
-    #--------------------------------------------------------------------------------
-    def rule_empty_seat_part2(self, x: int, y: int, seats: Table) -> str:
-        for offset in self.directionOffsets:
-            if self.get_first_seat_in_path(x, y, seats, offset) == "#":
-                return seats[y][x]
-        return "#"
+def rule_occupied_seat_part1(x: int, y: int, seats: Table) -> str:
+    numOccupied = 0
+    for offset in directionOffsets:
+        if get_seat_in_path(x, y, seats, offset) == "#":
+            numOccupied += 1
+    if numOccupied >= 4:
+        return "L"
+    return seats[y][x]
 
 
-    def rule_occupied_seat_part2(self, x: int, y: int, seats: Table) -> str:
-        numOccupied = 0
-        for offset in self.directionOffsets:
-            if self.get_first_seat_in_path(x, y, seats, offset) == "#":
-                numOccupied += 1
-        if numOccupied >= 5:
-            return "L"
-        return seats[y][x]
+# rules for Part 2
+#--------------------------------------------------------------------------------
+def rule_empty_seat_part2(x: int, y: int, seats: Table) -> str:
+    for offset in directionOffsets:
+        if get_first_seat_in_path(x, y, seats, offset) == "#":
+            return seats[y][x]
+    return "#"
 
 
-    # Common
-    #--------------------------------------------------------------------------------
-    def apply_rules_to_seats(self, seats: Table, rules: Dict) -> int:
-        numChanges = 0
-        y = 0
-
-        oldSeats = self.clone_table(seats)
-        while y < len(oldSeats):
-            x = 0
-            while x < len(oldSeats[y]):
-                currentSeat = oldSeats[y][x]
-                newSeat = currentSeat
-                if currentSeat in rules:
-                    newSeat = rules[currentSeat](x, y, oldSeats)
-
-                if newSeat != currentSeat:
-                    seats[y][x] = newSeat
-                    numChanges += 1
-                
-                x += 1
-            y += 1
-
-        return numChanges
+def rule_occupied_seat_part2(x: int, y: int, seats: Table) -> str:
+    numOccupied = 0
+    for offset in directionOffsets:
+        if get_first_seat_in_path(x, y, seats, offset) == "#":
+            numOccupied += 1
+    if numOccupied >= 5:
+        return "L"
+    return seats[y][x]
 
 
+# Common
+#--------------------------------------------------------------------------------
+def apply_rules_to_seats(seats: Table, rules: Dict) -> int:
+    numChanges = 0
+    y = 0
 
-    def run_part(self, data: InputData, rules: Dict) -> str:
-        seats = self.input_list_to_table(data.input)
+    oldSeats = clone_table(seats)
+    while y < len(oldSeats):
+        x = 0
+        while x < len(oldSeats[y]):
+            currentSeat = oldSeats[y][x]
+            newSeat = currentSeat
+            if currentSeat in rules:
+                newSeat = rules[currentSeat](x, y, oldSeats)
 
-        numChanges = -1
-        numPasses = 0
+            if newSeat != currentSeat:
+                seats[y][x] = newSeat
+                numChanges += 1
+            
+            x += 1
+        y += 1
 
-        while(numChanges != 0):
-            numPasses += 1
-            numChanges = self.apply_rules_to_seats(seats, rules)
-
-        result = self.count_occupied_seats(seats)
-        return helper.validate_result('How many seats end up occupied?', result, data.expectedAnswer)
+    return numChanges
 
 
-    def solve(self):
-        print("Day 11: Seating System")
-        print("")
 
-        rules_part1 = {
-            ".": self.rule_floor,
-            "L": self.rule_empty_seat_part1,
-            "#": self.rule_occupied_seat_part1,
-        }
+def get_number_occupied_seats(input: List[str], rules: Dict) -> str:
+    seats = input_list_to_table(input)
 
-        self.run_example(lambda: "P1 Ex1) " + self.run_part(InputData('example1', 1), rules_part1))
-        self.run_problem(lambda: "Part 1) " + self.run_part(InputData('input', 1), rules_part1))
+    numChanges = -1
+    numPasses = 0
 
-        print("")
+    while(numChanges != 0):
+        numPasses += 1
+        numChanges = apply_rules_to_seats(seats, rules)
 
-        rules_part2 = {
-            ".": self.rule_floor,
-            "L": self.rule_empty_seat_part2,
-            "#": self.rule_occupied_seat_part2,
-        }
-
-        self.run_example(lambda: "P2 Ex1) " + self.run_part(InputData('example1', 2), rules_part2))
-        self.run_problem(lambda: "Part 2) " + self.run_part(InputData('input', 2), rules_part2))
+    result = count_occupied_seats(seats)
+    return result
---
+++ 2020/Python/tests/test_day11.py
@@ -0,0 +1,64 @@
+import unittest
+from typing import Dict, List
+
+# this adds the 'src' folder to the path. Needed to get the src imports to work in unit tests.
+import config
+
+import inputHelper
+import src.day11 as puzzle
+
+
+_DAY = 11
+
+class Day11(unittest.TestCase):
+    def get_test_data(self, name:str, part:int) -> tuple[List[int], int]:
+        input = inputHelper.load_file(_DAY, name).splitlines()
+
+        expectedAnswer = inputHelper.load_file(_DAY, f"{name}-answer{part}")
+        if expectedAnswer is not None:
+            expectedAnswer = int(expectedAnswer)
+
+        return input, expectedAnswer
+
+
+    def get_rules_1(self):
+        rules = {
+            ".": puzzle.rule_floor,
+            "L": puzzle.rule_empty_seat_part1,
+            "#": puzzle.rule_occupied_seat_part1,
+        }
+        return rules
+
+
+    def test_part1_example1(self):
+        input, expected = self.get_test_data("example1", 1)
+        rules = self.get_rules_1()
+        answer = puzzle.get_number_occupied_seats(input, rules)
+        self.assertEqual(answer, expected)
+
+    def test_part1(self):
+        input, expected = self.get_test_data("input", 1)
+        rules = self.get_rules_1()
+        answer = puzzle.get_number_occupied_seats(input, rules)
+        self.assertEqual(answer, expected)
+
+
+    def get_rules_2(self):
+        rules = {
+            ".": puzzle.rule_floor,
+            "L": puzzle.rule_empty_seat_part2,
+            "#": puzzle.rule_occupied_seat_part2,
+        }
+        return rules
+
+    def test_part2_example1(self):
+        input, expected = self.get_test_data("example1", 2)
+        rules = self.get_rules_2()
+        answer = puzzle.get_number_occupied_seats(input, rules)
+        self.assertEqual(answer, expected)
+
+    def test_part2(self):
+        input, expected = self.get_test_data("input", 2)
+        rules = self.get_rules_2()
+        answer = puzzle.get_number_occupied_seats(input, rules)
+        self.assertEqual(answer, expected)
