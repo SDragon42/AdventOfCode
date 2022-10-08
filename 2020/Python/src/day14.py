80d278ae60ce76e2d40c44d46bfff525bc3b636f
SDragon42 <nathen.silver@gmail.com>
2022-10-08 16:18:54 -0600

    day 14 tests

--- 2020/Python/src/day14.py
+++ 2020/Python/src/day14.py
@@ -1,159 +1,127 @@
 import math
 from typing import List, Dict
 
-import helper
-import inputHelper
-from puzzleBase import PuzzleBase
+
+
+bitWidth: int = 36
+memory: Dict[int, int] = {}
+mask: str = "".rjust(bitWidth, "X")
 
 
 
-class InputData:
-    input: List[str]
-    expectedAnswer: int
-
-    def __init__(self, name: str, part: int) -> None:
-        day = 14
-        self.input = inputHelper.load_file(day, name).splitlines()
-        
-        answer = inputHelper.load_file(day, f"{name}-answer{part}")
-        self.expectedAnswer = int(answer) if answer is not None else None
+def int_to_binary(value: int) -> List[str]:
+    ba = list(bin(value)[2:].rjust(bitWidth, "0"))
+    return ba
 
 
-
-class Puzzle(PuzzleBase):
-    bitWidth: int = 36
-    memory: Dict[int, int] = {}
-    mask: str = "".rjust(bitWidth, "X")
+def binary_to_int(bValue: List[str]) -> int:
+    value = int("".join(bValue), 2)
+    return value
 
 
-    def int_to_binary(self, value: int) -> List[str]:
-        ba = list(bin(value)[2:].rjust(self.bitWidth, "0"))
-        return ba
+def set_mask(line: str):
+    global mask
+    mask = line[7:]
 
 
-    def binary_to_int(self, bValue: List[str]) -> int:
-        value = int("".join(bValue), 2)
-        return value
+def apply_mask_v1(bValue: List[str]):
+    i = 0
+    while i < len(mask):
+        if mask[i] != "X":
+            bValue[i] = mask[i]
+        i += 1
 
 
-    def set_mask(self, line: str):
-        self.mask = line[7:]
+def set_memory_v1(line: str):
+    x = line.index("]")
+    address = int(line[4:x])
+    x += 4
+    value = int(line[x:])
+
+    bValue = int_to_binary(value)
+    apply_mask_v1(bValue)
+    value = binary_to_int(bValue)
+
+    memory[address] = value
 
 
-    def apply_mask_v1(self, bValue: List[str]):
-        i = 0
-        while i < len(self.mask):
-            if self.mask[i] != "X":
-                bValue[i] = self.mask[i]
-            i += 1
+def apply_mask_v2(bValue: List[str]):
+    i = 0
+    while i < len(mask):
+        if mask[i] != "0":
+            bValue[i] = mask[i]
+        i += 1
 
 
-    def set_memory_v1(self, line: str):
-        x = line.index("]")
-        address = int(line[4:x])
-        x += 4
-        value = int(line[x:])
-
-        bValue = self.int_to_binary(value)
-        self.apply_mask_v1(bValue)
-        value = self.binary_to_int(bValue)
-
-        self.memory[address] = value
+def flip_address_bits(bValue: List[str], replacements: str) -> List[str]:
+    i = 0
+    while i < len(bValue):
+        if bValue[i] == "X":
+            bValue[i] = replacements[0]
+            replacements = replacements[1:]
+        i += 1
+    return bValue
 
 
-    def apply_mask_v2(self, bValue: List[str]):
-        i = 0
-        while i < len(self.mask):
-            if self.mask[i] != "0":
-                bValue[i] = self.mask[i]
-            i += 1
+def get_all_possible_addresses(bValue: List[str]) -> List[int]:
+    addresses: List[int] = []
+    count = mask.count("X")
+    num = int(math.pow(2, count))
+    rangeNums = range(num)
+    for r in rangeNums:
+        br = bin(r)[2:].rjust(count, "0")
+        newAddr = flip_address_bits(bValue.copy(), br)
+        newAddr2 = binary_to_int(newAddr)
+        addresses.append(newAddr2)
+    return addresses
 
 
-    def flip_address_bits(self, bValue: List[str], replacements: str) -> List[str]:
-        i = 0
-        while i < len(bValue):
-            if bValue[i] == "X":
-                bValue[i] = replacements[0]
-                replacements = replacements[1:]
-            i += 1
-        return bValue
+def set_memory_v2(line: str):
+    x = line.index("]")
+    address = int(line[4:x])
+    x += 4
+    value = int(line[x:])
+
+    bAddress = int_to_binary(address)
+    apply_mask_v2(bAddress)
+    addresses = get_all_possible_addresses(bAddress)
+    for addr in addresses:
+        memory[addr] = value
 
 
-    def get_all_possible_addresses(self, bValue: List[str]) -> List[int]:
-        addresses: List[int] = []
-        count = self.mask.count("X")
-        num = int(math.pow(2, count))
-        rangeNums = range(num)
-        for r in rangeNums:
-            br = bin(r)[2:].rjust(count, "0")
-            newAddr = self.flip_address_bits(bValue.copy(), br)
-            newAddr2 = self.binary_to_int(newAddr)
-            addresses.append(newAddr2)
-            pass
-        return addresses
+def run_part1(input: List[str]) -> int:
+    memory.clear()
+
+    actions = {
+        "mas" : set_mask,
+        "mem" : set_memory_v1
+    }
+
+    for line in input:
+        key = line[:3]
+        actions[key](line)
+
+    result = 0
+    for key in memory:
+        result += memory[key]
+
+    return result
 
 
-    def set_memory_v2(self, line: str):
-        x = line.index("]")
-        address = int(line[4:x])
-        x += 4
-        value = int(line[x:])
+def run_part2(input: List[str]) -> int:
+    memory.clear()
 
-        bAddress = self.int_to_binary(address)
-        self.apply_mask_v2(bAddress)
-        addresses = self.get_all_possible_addresses(bAddress)
-        for addr in addresses:
-            self.memory[addr] = value
+    actions = {
+        "mas" : set_mask,
+        "mem" : set_memory_v2
+    }
 
+    for line in input:
+        key = line[:3]
+        actions[key](line)
 
-    def run_part1(self, data: InputData) -> str:
-        self.memory.clear()
+    result = 0
+    for key in memory:
+        result += memory[key]
 
-        actions = {
-            "mas" : self.set_mask,
-            "mem" : self.set_memory_v1
-        }
-
-        for line in data.input:
-            key = line[:3]
-            actions[key](line)
-
-        result = 0
-        for key in self.memory:
-            result += self.memory[key]
-            pass
-
-        return helper.validate_result('What is the sum of all values left in memory after it completes?', result, data.expectedAnswer)
-
-
-    def run_part2(self, data: InputData) -> str:
-        self.memory.clear()
-
-        actions = {
-            "mas" : self.set_mask,
-            "mem" : self.set_memory_v2
-        }
-
-        for line in data.input:
-            key = line[:3]
-            actions[key](line)
-
-        result = 0
-        for key in self.memory:
-            result += self.memory[key]
-            pass
-
-        return helper.validate_result('What is the sum of all values left in memory after it completes?', result, data.expectedAnswer)
-
-
-    def solve(self):
-        print("Day 14: Docking Data")
-        print("")
-
-        self.run_example(lambda: "P1 Ex1) " + self.run_part1(InputData('example1', 1)))
-        self.run_problem(lambda: "Part 1) " + self.run_part1(InputData('input', 1)))
-
-        print("")
-
-        self.run_example(lambda: "P2 Ex1) " + self.run_part2(InputData('example2', 2)))
-        self.run_problem(lambda: "Part 2) " + self.run_part2(InputData('input', 2)))
+    return result
