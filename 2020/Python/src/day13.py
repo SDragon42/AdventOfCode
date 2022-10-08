fef929ee4c2f5fe92aca5cb026f198266344e974
SDragon42 <nathen.silver@gmail.com>
2022-10-08 15:41:29 -0600

    day 13 tests

--- 2020/Python/src/day13.py
+++ 2020/Python/src/day13.py
@@ -1,22 +1,6 @@
 from typing import List
 
 import helper
-import inputHelper
-
-from puzzleBase import PuzzleBase
-
-
-
-class InputData:
-    input: List[str]
-    expectedAnswer: int
-
-    def __init__(self, name: str, part: int) -> None:
-        day = 13
-        self.input = inputHelper.load_file(day, name).splitlines()
-        
-        answer = inputHelper.load_file(day, f"{name}-answer{part}")
-        self.expectedAnswer = int(answer) if answer is not None else None
 
 
 
@@ -30,78 +14,58 @@ class BusDepatureInfo:
 
 
 
-class Puzzle(PuzzleBase):
-
-    def get_next_time(self, currentTime: int, busId: int) -> int:
-        while currentTime % busId != 0:
-            currentTime += 1
-        return currentTime
+def get_next_time(currentTime: int, busId: int) -> int:
+    while currentTime % busId != 0:
+        currentTime += 1
+    return currentTime
 
 
-    def get_soonest_bus(self, earliestTime: int, buses: List[str]) -> BusDepatureInfo:
-        nextDepartures: List[BusDepatureInfo] = []
+def get_soonest_bus(earliestTime: int, buses: List[str]) -> BusDepatureInfo:
+    nextDepartures: List[BusDepatureInfo] = []
 
-        for x in buses:
-            if x == "x":
-                continue
-            busId = int(x)
-            time = self.get_next_time(earliestTime, busId)
-            nextDepartures.append(BusDepatureInfo(busId, time))
+    for x in buses:
+        if x == "x":
+            continue
+        busId = int(x)
+        time = get_next_time(earliestTime, busId)
+        nextDepartures.append(BusDepatureInfo(busId, time))
 
-        result = BusDepatureInfo(-1, 0)
-        for z in nextDepartures:
-            if z.departureTime < result.departureTime or result.busId < 0:
-                result = z
+    result = BusDepatureInfo(-1, 0)
+    for z in nextDepartures:
+        if z.departureTime < result.departureTime or result.busId < 0:
+            result = z
 
-        return result
+    return result
 
 
-    def get_next_matching_time(self, time: int, busId: int, increment: int, offset: int) -> int:
+def get_next_matching_time(time: int, busId: int, increment: int, offset: int) -> int:
+    time += increment
+    while (time + offset) % busId != 0:
         time += increment
-        while (time + offset) % busId != 0:
-            time += increment
-        return time
+    return time
 
 
-    def run_part1(self, data: InputData) -> str:
-        earliestTime = int(data.input[0])
-        buses = data.input[1].split(",")
-        bus = self.get_soonest_bus(earliestTime, buses)
-        result = (bus.departureTime - earliestTime) * bus.busId
-        return helper.validate_result("What is the ID of the earliest bus you can take to the airport multiplied by the number of minutes you'll need to wait for that bus?", result, data.expectedAnswer)
+def run_part1(input: List[int]) -> int:
+    earliestTime = int(input[0])
+    buses = input[1].split(",")
+    bus = get_soonest_bus(earliestTime, buses)
+    result = (bus.departureTime - earliestTime) * bus.busId
+    return result
 
 
-    def run_part2(self, data: InputData) -> str:
-        buses = data.input[1].split(",")
-        increment = 0
-        lcmValue = 1
-        time = 0
-        i = 0
-        while i < len(buses):
-            if buses[i] != "x":
-                busId = int(buses[i])
-                if increment == 0:
-                    increment = 1
-                time = self.get_next_matching_time(time, busId, increment, i)
-                lcmValue = helper.get_lcm(lcmValue, busId)
-                increment = lcmValue
-            i += 1
-        return helper.validate_result("What is the earliest timestamp such that all of the listed bus IDs depart at offsets matching their positions in the list?", time, data.expectedAnswer)
-
-
-    def solve(self):
-        print("Day 13: Shuttle Search")
-        print("")
-
-        self.run_example(lambda: "P1 Ex1) " + self.run_part1(InputData('example1', 1)))
-        self.run_problem(lambda: "Part 1) " + self.run_part1(InputData('input', 1)))
-
-        print("")
-
-        self.run_example(lambda: "P2 Ex1) " + self.run_part2(InputData('example1', 2)))
-        self.run_example(lambda: "P2 Ex2) " + self.run_part2(InputData('example2', 2)))
-        self.run_example(lambda: "P2 Ex3) " + self.run_part2(InputData('example3', 2)))
-        self.run_example(lambda: "P2 Ex4) " + self.run_part2(InputData('example4', 2)))
-        self.run_example(lambda: "P2 Ex5) " + self.run_part2(InputData('example5', 2)))
-        self.run_example(lambda: "P2 Ex6) " + self.run_part2(InputData('example6', 2)))
-        self.run_problem(lambda: "Part 2) " + self.run_part2(InputData('input', 2)))
+def run_part2(input: List[int]) -> int:
+    buses = input[1].split(",")
+    increment = 0
+    lcmValue = 1
+    time = 0
+    i = 0
+    while i < len(buses):
+        if buses[i] != "x":
+            busId = int(buses[i])
+            if increment == 0:
+                increment = 1
+            time = get_next_matching_time(time, busId, increment, i)
+            lcmValue = helper.get_lcm(lcmValue, busId)
+            increment = lcmValue
+        i += 1
+    return time
---
+++ 2020/Python/tests/test_day13.py
@@ -0,0 +1,68 @@
+import unittest
+from typing import Dict, List
+
+# this adds the 'src' folder to the path. Needed to get the src imports to work in unit tests.
+import config
+
+import inputHelper
+import src.day13 as puzzle
+
+
+_DAY = 13
+
+class Day13(unittest.TestCase):
+    def get_test_data(self, name:str, part:int) -> tuple[List[int], int]:
+        input = inputHelper.load_file(_DAY, name).splitlines()
+
+        expectedAnswer = inputHelper.load_file(_DAY, f"{name}-answer{part}")
+        if expectedAnswer is not None:
+            expectedAnswer = int(expectedAnswer)
+
+        return input, expectedAnswer
+
+
+    def test_part1_example1(self):
+        input, expected = self.get_test_data("example1", 1)
+        answer = puzzle.run_part1(input)
+        self.assertEqual(answer, expected)
+
+    def test_part1(self):
+        input, expected = self.get_test_data("input", 1)
+        answer = puzzle.run_part1(input)
+        self.assertEqual(answer, expected)
+
+
+    def test_part2_example1(self):
+        input, expected = self.get_test_data("example1", 2)
+        answer = puzzle.run_part2(input)
+        self.assertEqual(answer, expected)
+
+    def test_part2_example2(self):
+        input, expected = self.get_test_data("example2", 2)
+        answer = puzzle.run_part2(input)
+        self.assertEqual(answer, expected)
+
+    def test_part2_example3(self):
+        input, expected = self.get_test_data("example3", 2)
+        answer = puzzle.run_part2(input)
+        self.assertEqual(answer, expected)
+
+    def test_part2_example4(self):
+        input, expected = self.get_test_data("example4", 2)
+        answer = puzzle.run_part2(input)
+        self.assertEqual(answer, expected)
+
+    def test_part2_example5(self):
+        input, expected = self.get_test_data("example5", 2)
+        answer = puzzle.run_part2(input)
+        self.assertEqual(answer, expected)
+
+    def test_part2_example6(self):
+        input, expected = self.get_test_data("example6", 2)
+        answer = puzzle.run_part2(input)
+        self.assertEqual(answer, expected)
+
+    def test_part2(self):
+        input, expected = self.get_test_data("input", 2)
+        answer = puzzle.run_part2(input)
+        self.assertEqual(answer, expected)
