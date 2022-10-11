a97cc34f30beb32330272112d6976d5219f6a141
SDragon42 <nathen.silver@gmail.com>
2022-10-11 17:57:11 -0600

    day 21 tests

--- 2020/Python/src/day21.py
+++ 2020/Python/src/day21.py
@@ -2,21 +2,6 @@
 
 from typing import List, Dict, Set, Tuple
 
-import helper
-import inputHelper
-from puzzleBase import PuzzleBase
-
-
-
-class InputData:
-    input: List[str]
-    expectedAnswer: str
-
-    def __init__(self, name: str, part: int) -> None:
-        day = 21
-        self.input = inputHelper.load_file(day, name).splitlines()
-        self.expectedAnswer = inputHelper.load_file(day, f"{name}-answer{part}")
-
 
 
 # Types
@@ -27,89 +12,74 @@ IngredientCounterDict = Dict[str, int]
 
 
 
-class Puzzle(PuzzleBase):
-
-    def parse_food_line(self, line: str) -> Tuple[IngredientsSet, AllergenList]:
-        parts = line.split(" (contains ")
-        ingredients = set(parts[0].split())
-        allergens =parts[1].replace(")", "").split(", ")
-        return ingredients, allergens
+def parse_food_line(line: str) -> Tuple[IngredientsSet, AllergenList]:
+    parts = line.split(" (contains ")
+    ingredients = set(parts[0].split())
+    allergens =parts[1].replace(")", "").split(", ")
+    return ingredients, allergens
 
 
-    def process_food_lines(self, foodLineList: List[str]) -> Tuple[AllergenIngredientDict, IngredientCounterDict]:
-        allergen_ingredients: AllergenIngredientDict = {}
-        ingredient_counter: IngredientCounterDict = {}
+def process_food_lines(foodLineList: List[str]) -> Tuple[AllergenIngredientDict, IngredientCounterDict]:
+    allergen_ingredients: AllergenIngredientDict = {}
+    ingredient_counter: IngredientCounterDict = {}
 
-        for foodLine in foodLineList:
-            ingredients, allergens = self.parse_food_line(foodLine)
+    for foodLine in foodLineList:
+        ingredients, allergens = parse_food_line(foodLine)
 
-            for a in allergens:
-                if a not in allergen_ingredients:
-                    allergen_ingredients[a] = ingredients.copy()
-                else:
-                    allergen_ingredients[a].intersection_update(ingredients)
+        for a in allergens:
+            if a not in allergen_ingredients:
+                allergen_ingredients[a] = ingredients.copy()
+            else:
+                allergen_ingredients[a].intersection_update(ingredients)
 
-            for i in ingredients:
-                if i not in ingredient_counter:
-                    ingredient_counter[i] = 0
-                ingredient_counter[i] += 1
+        for i in ingredients:
+            if i not in ingredient_counter:
+                ingredient_counter[i] = 0
+            ingredient_counter[i] += 1
 
-        return allergen_ingredients, ingredient_counter
-
-
-    def get_ingredients_without_allergens(self, allergen_ingredients: AllergenIngredientDict, ingredient_counter: IngredientCounterDict) -> Set[str]:
-        without_allergens = set(ingredient_counter.keys())
-        for v in allergen_ingredients.values():
-            for allergen in v:
-                without_allergens.discard(allergen)
-        return without_allergens
+    return allergen_ingredients, ingredient_counter
 
 
-    def run_part1(self, data: InputData) -> str:
-        allergen_ingredients, ingredient_counter = self.process_food_lines(data.input)
-
-        without_allergens = self.get_ingredients_without_allergens(allergen_ingredients, ingredient_counter)
-        result = sum(ingredient_counter[ingredient] for ingredient in without_allergens)
-        result = str(result)
-
-        return helper.validate_result('How many times do any of those ingredients appear?', result, data.expectedAnswer)
+def get_ingredients_without_allergens(allergen_ingredients: AllergenIngredientDict, ingredient_counter: IngredientCounterDict) -> Set[str]:
+    without_allergens = set(ingredient_counter.keys())
+    for v in allergen_ingredients.values():
+        for allergen in v:
+            without_allergens.discard(allergen)
+    return without_allergens
 
 
-    def run_part2(self, data: InputData) -> str:
-        allergen_ingredients, ingredient_counter = self.process_food_lines(data.input)
+def run_part1(input:List[str]) -> str:
+    allergen_ingredients, ingredient_counter = process_food_lines(input)
 
-        finished = False
-        finalIngredients = set()
-        while not finished:
-            finished = True
-            for allergen, ingredients in allergen_ingredients.items():
-                if len(ingredients) != 1:
-                    finished = False
-                    continue
+    without_allergens = get_ingredients_without_allergens(allergen_ingredients, ingredient_counter)
+    result = sum(ingredient_counter[ingredient] for ingredient in without_allergens)
+    result = str(result)
 
-                ingredient = next(iter(ingredients))
-                if ingredient not in finalIngredients:
-                    finalIngredients.add(ingredient)
-
-                    for a, i in allergen_ingredients.items():
-                        if a != allergen:
-                            i.discard(ingredient)
-
-        result = [(k, *v) for k, v in allergen_ingredients.items()]
-        result.sort(key=lambda x: x[0])
-        result = ",".join(x[1] for x in result)
-
-        return helper.validate_result('What is your canonical dangerous ingredient list?', result, data.expectedAnswer)
+    return result
 
 
-    def solve(self):
-        print("Day 21: Allergen Assessment")
-        print("")
+def run_part2(input:List[str]) -> str:
+    allergen_ingredients, ingredient_counter = process_food_lines(input)
 
-        self.run_example(lambda: "P1 Ex1) " + self.run_part1(InputData('example1', 1)))
-        self.run_problem(lambda: "Part 1) " + self.run_part1(InputData('input', 1)))
+    finished = False
+    finalIngredients = set()
+    while not finished:
+        finished = True
+        for allergen, ingredients in allergen_ingredients.items():
+            if len(ingredients) != 1:
+                finished = False
+                continue
 
-        print("")
+            ingredient = next(iter(ingredients))
+            if ingredient not in finalIngredients:
+                finalIngredients.add(ingredient)
 
-        self.run_example(lambda: "P2 Ex1) " + self.run_part2(InputData('example1', 2)))
-        self.run_problem(lambda: "Part 2) " + self.run_part2(InputData('input', 2)))
+                for a, i in allergen_ingredients.items():
+                    if a != allergen:
+                        i.discard(ingredient)
+
+    result = [(k, *v) for k, v in allergen_ingredients.items()]
+    result.sort(key=lambda x: x[0])
+    result = ",".join(x[1] for x in result)
+
+    return result
