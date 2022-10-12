a4f61d3f6c95a4c148c5724258f36bfffaf38c8f
SDragon42 <nathen.silver@gmail.com>
2022-10-11 19:52:05 -0600

    day 22 tests

--- 2020/Python/src/day22.py
+++ 2020/Python/src/day22.py
@@ -1,9 +1,5 @@
 from typing import List, Tuple
 
-import helper
-import inputHelper
-from puzzleBase import PuzzleBase
-
 
 
 CardList = List[int]
@@ -15,165 +11,154 @@ Const_Player2: str = "Player 2"
 
 
 
-class InputData:
-    expectedAnswer: int
-    player1Deck: CardList
-    player2Deck: CardList
+
+nextGame: int = 1
 
 
-    def __init__(self, name: str, part: int) -> None:
-        day = 22
-
-        decks = {}
-        playerblocks = inputHelper.load_file(day, name).split("\n\n")
-        for player in playerblocks:
-            bits = player.splitlines()
-            key = bits[0].replace(":", "")
-            decks[key] = [int(x) for x in bits[1:]]
-        self.player1Deck = decks[Const_Player1]
-        self.player2Deck = decks[Const_Player2]
-
-        answer = inputHelper.load_file(day, f"{name}-answer{part}")
-        self.expectedAnswer = int(answer) if answer is not None else None
-
-
-
-class Puzzle(PuzzleBase):
-    nextGame: int = 1
-
-
-    def play_combat(self, p1Deck: CardList, p2Deck: CardList) -> CardList:
-        round = 0
-        while len(p1Deck) > 0 and len(p2Deck) > 0:
-            round += 1
-            helper.dprint(f"-- Round {round} --")
-            helper.dprint(f"{Const_Player1}'s deck: " + ", ".join(str(x) for x in p1Deck))
-            helper.dprint(f"{Const_Player2}'s deck: " + ", ".join(str(x) for x in p2Deck))
-
-            p1Card = p1Deck.pop(0)
-            p2Card = p2Deck.pop(0)
-
-            helper.dprint(f"{Const_Player1} plays {p1Card}")
-            helper.dprint(f"{Const_Player2} plays {p2Card}")
-            if p1Card > p2Card:
-                helper.dprint(f"{Const_Player1} wins the round!")
-                # add_to_deck(p1Deck, p1Card, p2Card)
-                p1Deck.append(p1Card)
-                p1Deck.append(p2Card)
-            else:
-                helper.dprint(f"{Const_Player2} wins the round!")
-                # add_to_deck(p2Deck, p2Card, p1Card)
-                p2Deck.append(p2Card)
-                p2Deck.append(p1Card)
-            helper.dprint("")
-
-        helper.dprint("== Post-game results ==")
-        helper.dprint(f"{Const_Player1}'s deck: " + ", ".join(str(x) for x in p1Deck))
-        helper.dprint(f"{Const_Player2}'s deck: " + ", ".join(str(x) for x in p2Deck))
-
-        return p1Deck if len(p1Deck) > 0 else p2Deck
+def build_decks(input: List[str]) -> Tuple[CardList, CardList]:
+    it = iter(input)
+    line = next(it)
+    
+    assert line.rstrip() == 'Player 1:'
+    player1Deck:CardList = []
+    while (line := next(it)).rstrip():
+        player1Deck.append(int(line))
+    line = next(it)
+    
+    assert line.rstrip() == 'Player 2:'
+    player2Deck:CardList = []
+    while True:
+        try:
+            line = next(it)
+        except StopIteration:
+            break
+        player2Deck.append(int(line))
+    
+    return player1Deck, player2Deck
 
 
-    def add_to_history(self, game: int, p1Deck: CardList, p2Deck: CardList, history: List[str]) -> bool:
-        p1DeckStr = ",".join(str(x) for x in p1Deck)
-        p2DeckStr = ",".join(str(x) for x in p2Deck)
-        state = f"Game {game}|{Const_Player1}:{p1DeckStr}|{Const_Player2}:{p2DeckStr}"
-        if state in history:
-            return False
-        history.append(state)
-        return True
+def play_combat(p1Deck: CardList, p2Deck: CardList) -> CardList:
+    round = 0
+    while len(p1Deck) > 0 and len(p2Deck) > 0:
+        round += 1
+        # helper.dprint(f"-- Round {round} --")
+        # helper.dprint(f"{Const_Player1}'s deck: " + ", ".join(str(x) for x in p1Deck))
+        # helper.dprint(f"{Const_Player2}'s deck: " + ", ".join(str(x) for x in p2Deck))
 
+        p1Card = p1Deck.pop(0)
+        p2Card = p2Deck.pop(0)
 
-    def play_recursive_combat(self, p1Deck: CardList, p2Deck: CardList, history: List[str]) -> Tuple[str, CardList]:
-        game = self.nextGame
-        self.nextGame += 1
-
-        if game == 5:
-            helper.dprint("")
-
-        helper.dprint(f"=== Game {game} ===")
-        helper.dprint("")
-
-        round = 0
-        winner = ""
-        winningDeck: CardList = []
-        while len(p1Deck) > 0 and len(p2Deck) > 0:
-            round += 1
-
-            if not self.add_to_history(game, p1Deck, p2Deck, history):
-                helper.dprint(f"{Const_Player1} wins round {round} of game {game}!")
-                return Const_Player1, p1Deck
-
-            helper.dprint(f"-- Round {round} (Game {game}) --")
-            helper.dprint(f"{Const_Player1}'s deck: " + ", ".join(str(x) for x in p1Deck))
-            helper.dprint(f"{Const_Player2}'s deck: " + ", ".join(str(x) for x in p2Deck))
-
-            p1Card = p1Deck.pop(0)
-            p2Card = p2Deck.pop(0)
-
-            helper.dprint(f"{Const_Player1} plays {p1Card}")
-            helper.dprint(f"{Const_Player2} plays {p2Card}")
-
-            if p1Card <= len(p1Deck) and p2Card <= len(p2Deck):
-                helper.dprint("Playing a sub-game to determine the winner...")
-                helper.dprint("")
-                p1SubDeck = p1Deck[:p1Card]
-                p2SubDeck = p2Deck[:p2Card]
-                winner, winningDeck = self.play_recursive_combat(p1SubDeck, p2SubDeck, history)
-            else:
-                winner = Const_Player1 if p1Card > p2Card else Const_Player2
-
-            helper.dprint(f"{winner} wins round {round} of game {game}!")
-
-            if winner == Const_Player1:
-                p1Deck.append(p1Card)
-                p1Deck.append(p2Card)
-            else:
-                p2Deck.append(p2Card)
-                p2Deck.append(p1Card)
-            helper.dprint("")
-
-        if game == 1:
-            helper.dprint("== Post-game results ==")
-            helper.dprint(f"{Const_Player1}'s deck: " + ", ".join(str(x) for x in p1Deck))
-            helper.dprint(f"{Const_Player2}'s deck: " + ", ".join(str(x) for x in p2Deck))
+        # helper.dprint(f"{Const_Player1} plays {p1Card}")
+        # helper.dprint(f"{Const_Player2} plays {p2Card}")
+        if p1Card > p2Card:
+            # helper.dprint(f"{Const_Player1} wins the round!")
+            # add_to_deck(p1Deck, p1Card, p2Card)
+            p1Deck.append(p1Card)
+            p1Deck.append(p2Card)
         else:
-            helper.dprint(f"The winner of game {game} is {winner}!")
+            # helper.dprint(f"{Const_Player2} wins the round!")
+            # add_to_deck(p2Deck, p2Card, p1Card)
+            p2Deck.append(p2Card)
+            p2Deck.append(p1Card)
+        # helper.dprint("")
 
-        return (Const_Player1, p1Deck) if winner == Const_Player1 else (Const_Player2, p2Deck)
+    # helper.dprint("== Post-game results ==")
+    # helper.dprint(f"{Const_Player1}'s deck: " + ", ".join(str(x) for x in p1Deck))
+    # helper.dprint(f"{Const_Player2}'s deck: " + ", ".join(str(x) for x in p2Deck))
 
-
-    def calculate_score(self, deck: CardList, multiplier: int) -> int:
-        if len(deck) == 0:
-            return 0
-
-        card = deck.pop(len(deck) - 1)
-        result = (card * multiplier) + self.calculate_score(deck, multiplier + 1)
-        return result
-
-
-    def run_part1(self, data: InputData) -> str:
-        winnerDeck = self.play_combat(data.player1Deck, data.player2Deck)
-        result = self.calculate_score(winnerDeck, 1)
-        return helper.validate_result("What is the winning player's score?", result, data.expectedAnswer)
+    return p1Deck if len(p1Deck) > 0 else p2Deck
 
 
-    def run_part2(self, data: InputData) -> str:
-        history: List[str] = []
-        winner, winnerDeck = self.play_recursive_combat(data.player1Deck, data.player2Deck, history)
-        result = self.calculate_score(winnerDeck, 1)
-        return helper.validate_result("What is the winning player's score?", result, data.expectedAnswer)
+def add_to_history(game: int, p1Deck: CardList, p2Deck: CardList, history: List[str]) -> bool:
+    p1DeckStr = ",".join(str(x) for x in p1Deck)
+    p2DeckStr = ",".join(str(x) for x in p2Deck)
+    state = f"Game {game}|{Const_Player1}:{p1DeckStr}|{Const_Player2}:{p2DeckStr}"
+    if state in history:
+        return False
+    history.append(state)
+    return True
 
 
-    def solve(self):
-        print("Day 22: Crab Combat")
-        print("")
+def play_recursive_combat(p1Deck: CardList, p2Deck: CardList, history: List[str]) -> Tuple[str, CardList]:
+    global nextGame
 
-        self.run_example(lambda: "P1 Ex1) " + self.run_part1(InputData('example1', 1)))
-        self.run_problem(lambda: "Part 1) " + self.run_part1(InputData('input', 1)))
+    game = nextGame
+    nextGame += 1
 
-        print("")
+    # if game == 5:
+    #     helper.dprint("")
 
-        self.run_example(lambda: "P2 Ex1) " + self.run_part2(InputData('example1', 2)))
-        self.run_example(lambda: "P2 Ex2) " + self.run_part2(InputData('example2', 2)))
-        self.run_problem(lambda: "Part 2) " + self.run_part2(InputData('input', 2)))
+    # helper.dprint(f"=== Game {game} ===")
+    # helper.dprint("")
+
+    round = 0
+    winner = ""
+    winningDeck: CardList = []
+    while len(p1Deck) > 0 and len(p2Deck) > 0:
+        round += 1
+
+        if not add_to_history(game, p1Deck, p2Deck, history):
+            # helper.dprint(f"{Const_Player1} wins round {round} of game {game}!")
+            return Const_Player1, p1Deck
+
+        # helper.dprint(f"-- Round {round} (Game {game}) --")
+        # helper.dprint(f"{Const_Player1}'s deck: " + ", ".join(str(x) for x in p1Deck))
+        # helper.dprint(f"{Const_Player2}'s deck: " + ", ".join(str(x) for x in p2Deck))
+
+        p1Card = p1Deck.pop(0)
+        p2Card = p2Deck.pop(0)
+
+        # helper.dprint(f"{Const_Player1} plays {p1Card}")
+        # helper.dprint(f"{Const_Player2} plays {p2Card}")
+
+        if p1Card <= len(p1Deck) and p2Card <= len(p2Deck):
+            # helper.dprint("Playing a sub-game to determine the winner...")
+            # helper.dprint("")
+            p1SubDeck = p1Deck[:p1Card]
+            p2SubDeck = p2Deck[:p2Card]
+            winner, winningDeck = play_recursive_combat(p1SubDeck, p2SubDeck, history)
+        else:
+            winner = Const_Player1 if p1Card > p2Card else Const_Player2
+
+        # helper.dprint(f"{winner} wins round {round} of game {game}!")
+
+        if winner == Const_Player1:
+            p1Deck.append(p1Card)
+            p1Deck.append(p2Card)
+        else:
+            p2Deck.append(p2Card)
+            p2Deck.append(p1Card)
+        # helper.dprint("")
+
+    # if game == 1:
+    #     helper.dprint("== Post-game results ==")
+    #     helper.dprint(f"{Const_Player1}'s deck: " + ", ".join(str(x) for x in p1Deck))
+    #     helper.dprint(f"{Const_Player2}'s deck: " + ", ".join(str(x) for x in p2Deck))
+    # else:
+    #     helper.dprint(f"The winner of game {game} is {winner}!")
+
+    return (Const_Player1, p1Deck) if winner == Const_Player1 else (Const_Player2, p2Deck)
+
+
+def calculate_score(deck: CardList, multiplier: int) -> int:
+    if len(deck) == 0:
+        return 0
+
+    card = deck.pop(len(deck) - 1)
+    result = (card * multiplier) + calculate_score(deck, multiplier + 1)
+    return result
+
+
+def run_part1(input: List[str]) -> int:
+    player1Deck, player2Deck = build_decks(input)
+    winnerDeck = play_combat(player1Deck, player2Deck)
+    result = calculate_score(winnerDeck, 1)
+    return result
+
+
+def run_part2(input: List[str]) -> int:
+    player1Deck, player2Deck = build_decks(input)
+    history: List[str] = []
+    winner, winnerDeck = play_recursive_combat(player1Deck, player2Deck, history)
+    result = calculate_score(winnerDeck, 1)
+    return result
--- 2020/Python/src/day22fast.py
+++ 2020/Python/src/day22fast.py
@@ -3,100 +3,72 @@
 from typing import List, Tuple
 from collections import deque
 
-import helper
-import inputHelper
-from puzzleBase import PuzzleBase
 
 
-
-class InputData:
-    player1Deck: deque
-    player2Deck: deque
-    expectedAnswer: int
-
-    def __init__(self, name: str, part: int) -> None:
-        day = 22
-        
-        lines = inputHelper.load_file(day, name).splitlines()
-        it = iter(lines)
-        line = next(it)
-        
-        assert line.rstrip() == 'Player 1:'
-        self.player1Deck = deque()
-        while (line := next(it)).rstrip():
-            self.player1Deck.append(int(line))
-        line = next(it)
-        
-        assert line.rstrip() == 'Player 2:'
-        self.player2Deck = deque()
-        while True:
-            try:
-                line = next(it)
-            except StopIteration:
-                break
-            self.player2Deck.append(int(line))
-        
-        answer = inputHelper.load_file(day, f"{name}-answer{part}")
-        self.expectedAnswer = int(answer) if answer is not None else None
+def build_decks(input: List[str]) -> Tuple[deque, deque]:
+    it = iter(input)
+    line = next(it)
+    
+    assert line.rstrip() == 'Player 1:'
+    player1Deck = deque()
+    while (line := next(it)).rstrip():
+        player1Deck.append(int(line))
+    line = next(it)
+    
+    assert line.rstrip() == 'Player 2:'
+    player2Deck = deque()
+    while True:
+        try:
+            line = next(it)
+        except StopIteration:
+            break
+        player2Deck.append(int(line))
+    
+    return player1Deck, player2Deck
 
 
-
-class Puzzle(PuzzleBase):
-
-    def go(self, deck1: deque, deck2: deque) -> bool:
-        seen = set()
-        while deck1 and deck2:
-            state = tuple(deck1), tuple(deck2)
-            if state in seen:
-                return False
-            seen.add(state)
-            card1 = deck1.popleft()
-            card2 = deck2.popleft()
-            if (self.go(deque(list(deck1)[:card1]), deque(list(deck2)[:card2]))
-                    if card1 <= len(deck1) and card2 <= len(deck2) else
-                    card1 < card2):
-                deck2.append(card2)
-                deck2.append(card1)
-            else:
-                deck1.append(card1)
-                deck1.append(card2)
-        return not deck1
+def go(deck1: deque, deck2: deque) -> bool:
+    seen = set()
+    while deck1 and deck2:
+        state = tuple(deck1), tuple(deck2)
+        if state in seen:
+            return False
+        seen.add(state)
+        card1 = deck1.popleft()
+        card2 = deck2.popleft()
+        if (go(deque(list(deck1)[:card1]), deque(list(deck2)[:card2]))
+                if card1 <= len(deck1) and card2 <= len(deck2) else
+                card1 < card2):
+            deck2.append(card2)
+            deck2.append(card1)
+        else:
+            deck1.append(card1)
+            deck1.append(card2)
+    return not deck1
 
 
-    def run_part1(self, data: InputData) -> str:
-        while data.player1Deck and data.player2Deck:
-            card1 = data.player1Deck.popleft()
-            card2 = data.player2Deck.popleft()
-            if card1 < card2:
-                data.player2Deck.append(card2)
-                data.player2Deck.append(card1)
-            else:
-                data.player1Deck.append(card1)
-                data.player1Deck.append(card2)
-        winner = data.player1Deck or data.player2Deck
+def run_part1(input: List[str]) -> int:
+    player1Deck, player2Deck = build_decks(input)
+    while player1Deck and player2Deck:
+        card1 = player1Deck.popleft()
+        card2 = player2Deck.popleft()
+        if card1 < card2:
+            player2Deck.append(card2)
+            player2Deck.append(card1)
+        else:
+            player1Deck.append(card1)
+            player1Deck.append(card2)
+    winner = player1Deck or player2Deck
 
-        result = sum((len(winner) - i) * x for i, x in enumerate(winner))
+    result = sum((len(winner) - i) * x for i, x in enumerate(winner))
 
-        return helper.validate_result("What is the winning player's score?", result, data.expectedAnswer)
-
-
-    def run_part2(self, data: InputData) -> str:
-        self.go(data.player1Deck, data.player2Deck)
-        winner = data.player1Deck or data.player2Deck
-        result = sum((len(winner) - i) * x for i, x in enumerate(winner))
-        
-        return helper.validate_result("What is the winning player's score?", result, data.expectedAnswer)
+    return result
 
 
-    def solve(self):
-        print("Day 22: Crab Combat")
-        print("")
-
-        self.run_example(lambda: "P1 Ex1) " + self.run_part1(InputData('example1', 1)))
-        self.run_problem(lambda: "Part 1) " + self.run_part1(InputData('input', 1)))
-
-        print("")
-
-        self.run_example(lambda: "P2 Ex1) " + self.run_part2(InputData('example1', 2)))
-        self.run_example(lambda: "P2 Ex2) " + self.run_part2(InputData('example2', 2)))
-        self.run_problem(lambda: "Part 2) " + self.run_part2(InputData('input', 2)))
+def run_part2(input: List[str]) -> int:
+    player1Deck, player2Deck = build_decks(input)
+    go(player1Deck, player2Deck)
+    winner = player1Deck or player2Deck
+    result = sum((len(winner) - i) * x for i, x in enumerate(winner))
+    
+    return result
