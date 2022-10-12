b7e6f16675fe9673a206e6648147784f0b85fd27
SDragon42 <nathen.silver@gmail.com>
2022-10-11 20:27:00 -0600

    day 25 tests

--- 2020/Python/src/day25.py
+++ 2020/Python/src/day25.py
@@ -1,76 +1,49 @@
 from typing import List
 
-import helper
-import inputHelper
-from puzzleBase import PuzzleBase
+# import helper
 
 
 
-class InputData:
-    input: List[int]
-    expectedAnswer: int
-
-    def __init__(self, name: str, part: int) -> None:
-        day = 25
-
-        lines = inputHelper.load_file(day, name).splitlines()
-        self.input = [int(l) for l in lines]
-
-        answer = inputHelper.load_file(day, f"{name}-answer{part}")
-        self.expectedAnswer = int(answer) if answer is not None else None
+def loop_transform(value: int, subjectNumber: int) -> int:
+    value *= subjectNumber
+    value = value % 20201227
+    return value
 
 
-
-class Puzzle(PuzzleBase):
-
-    def loop_transform(self, value: int, subjectNumber: int) -> int:
-        value *= subjectNumber
-        value = value % 20201227
-        return value
-
-
-    def find_loop_size(self, publicKey: int) -> int:
-        loopSize = 0
-        calcKey = 1
-        while True:
-            loopSize += 1
-            calcKey = self.loop_transform(calcKey, 7)
-            if calcKey == publicKey:
-                break
-        return loopSize
+def find_loop_size(publicKey: int) -> int:
+    loopSize = 0
+    calcKey = 1
+    while True:
+        loopSize += 1
+        calcKey = loop_transform(calcKey, 7)
+        if calcKey == publicKey:
+            break
+    return loopSize
 
 
-    def calculate_encryption_key(self, publicKey: int, loopSize: int) -> int:
-        l = 0
-        encryptKey = 1
-        while l < loopSize:
-            encryptKey = self.loop_transform(encryptKey, publicKey)
-            l += 1
-        return encryptKey
+def calculate_encryption_key(publicKey: int, loopSize: int) -> int:
+    l = 0
+    encryptKey = 1
+    while l < loopSize:
+        encryptKey = loop_transform(encryptKey, publicKey)
+        l += 1
+    return encryptKey
 
 
-    def run_part1(self, data: InputData) -> str:
-        cardPublicKey = data.input[0]
-        doorPublicKey = data.input[1]
+def run_part1(input: List[int]) -> int:
+    cardPublicKey = input[0]
+    doorPublicKey = input[1]
 
-        cardLoopSize = self.find_loop_size(cardPublicKey)
-        doorLoopSize = self.find_loop_size(doorPublicKey)
-        helper.dprint(f"card key: {cardPublicKey}   loop size: {cardLoopSize}")
-        helper.dprint(f"door key: {doorPublicKey}   loop size: {doorLoopSize}")
+    cardLoopSize = find_loop_size(cardPublicKey)
+    doorLoopSize = find_loop_size(doorPublicKey)
+    # helper.dprint(f"card key: {cardPublicKey}   loop size: {cardLoopSize}")
+    # helper.dprint(f"door key: {doorPublicKey}   loop size: {doorLoopSize}")
 
-        result1 = self.calculate_encryption_key(cardPublicKey, doorLoopSize)
-        result2 = self.calculate_encryption_key(doorPublicKey, cardLoopSize)
+    result1 = calculate_encryption_key(cardPublicKey, doorLoopSize)
+    result2 = calculate_encryption_key(doorPublicKey, cardLoopSize)
 
-        if result1 == result2:
-            helper.dprint("    KEY MATCH")
-        helper.dprint(f"key: {result1} - {result2}")
+    # if result1 == result2:
+    #     helper.dprint("    KEY MATCH")
+    # helper.dprint(f"key: {result1} - {result2}")
 
-        return helper.validate_result('What encryption key is the handshake trying to establish?', result1, data.expectedAnswer)
-
-
-    def solve(self):
-        print("Day 25: Combo Breaker")
-        print("")
-
-        self.run_example(lambda: "P1 Ex1) " + self.run_part1(InputData('example1', 1)))
-        self.run_problem(lambda: "Part 1) " + self.run_part1(InputData('input', 1)))
+    return result1
---
+++ 2020/Python/tests/test_day25.py
@@ -0,0 +1,33 @@
+import unittest
+from typing import Dict, List
+
+# this adds the 'src' folder to the path. Needed to get the src imports to work in unit tests.
+import config
+
+import inputHelper
+import src.day25 as puzzle
+
+
+_DAY = 25
+
+class Day25(unittest.TestCase):
+    def get_test_data(self, name:str, part:int) -> tuple[List[int], int]:
+        lines = inputHelper.load_file(_DAY, name).splitlines()
+        input = [int(l) for l in lines]
+
+        expectedAnswer = inputHelper.load_file(_DAY, f"{name}-answer{part}")
+        if expectedAnswer is not None:
+            expectedAnswer = int(expectedAnswer)
+
+        return input, expectedAnswer
+
+
+    def test_part1_example1(self):
+        input, expected = self.get_test_data("example1", 1)
+        answer = puzzle.run_part1(input)
+        self.assertEqual(answer, expected)
+
+    def test_part1(self):
+        input, expected = self.get_test_data("input", 1)
+        answer = puzzle.run_part1(input)
+        self.assertEqual(answer, expected)
