8ab7aa2aa2864d82315c1f504710eba577c55daf
SDragon42 <nathen.silver@gmail.com>
2022-10-21 20:23:49 -0600

    Changed project to use XUnit

--- 2021/FSharp/template.fs
+++ 2021/FSharp/template.fs
@@ -1,47 +1,66 @@
-﻿module Template
+﻿namespace AdventOfCode.FSharp.Year2021
 
 open FSharp.Common
 open System
+open Xunit
+
+type private Puzzle () =
+
+    member private this.CountIncreases = 
+        List.pairwise 
+        >> List.filter (fun (a,b) -> a < b) 
+        >> List.length
 
 
-type private PuzzleInput (input, expectedAnswer) =
-    inherit InputAnswer<string list, int option> (input, expectedAnswer)
+    member this.RunPart1 (input:string list) =
+        // <Puzzle Question>
+        let result = 0
+        result
 
 
-type Template (runBenchmarks, runExamples) =
-    inherit PuzzleBase (runBenchmarks, runExamples)
+    member this.RunPart2 (input:string list) =
+        // <Puzzle Question>
+        let result = 0
+        result
 
 
-    member private this.GetPuzzleInput (part:int) (name: string) =
+
+module Template =
+    let private GetPuzzleInput (part:int) (name:string) =
         let day = 0
 
-        let input =
+        let input = 
             InputHelper.LoadLines (day, name)
             |> Seq.toList
 
         let answer = 
             InputHelper.LoadAnswer (day, $"%s{name}-answer%i{part}")
             |> InputHelper.AsInt
-
-        new PuzzleInput (input, answer)
-
-
-    member private this.RunPart1 (puzzleData: PuzzleInput) =
-        let result = 0
-        Helper.GetPuzzleResultText ("", result, puzzleData.ExpectedAnswer)
+    
+        input, answer
 
 
-    member private this.RunPart2 (puzzleData: PuzzleInput) =
-        let result = 0
-        Helper.GetPuzzleResultText ("", result, puzzleData.ExpectedAnswer)
+    [<Theory>]
+    [<InlineData("example1")>]
+    [<InlineData("input")>]
+    let Part1 (name:string) =
+        let input, expected = GetPuzzleInput 1 name
+
+        let actual = (new Puzzle()).RunPart1 (input)
+
+        match expected with
+        | None -> Assert.Null(actual)
+        | _ -> Assert.Equal(expected.Value, actual)
 
 
-    override this.SolvePuzzle _ = seq {
-        yield "TITLE"
-        yield this.RunExample (fun _ -> " Ex. 1) " + this.RunPart1 (this.GetPuzzleInput 1 "example1") )
-        yield this.RunProblem (fun _ -> "Part 1) " + this.RunPart1 (this.GetPuzzleInput 1 "input") )
+    [<Theory>]
+    [<InlineData("example1")>]
+    [<InlineData("input")>]
+    let Part2 (name:string) =
+        let input, expected = GetPuzzleInput 2 name
 
-        yield ""
-        yield this.RunExample (fun _ -> " Ex. 1) " + this.RunPart2 (this.GetPuzzleInput 2 "example1") )
-        yield this.RunProblem (fun _ -> "Part 2) " + this.RunPart2 (this.GetPuzzleInput 2 "input") )
-        }
+        let actual = (new Puzzle()).RunPart2 (input)
+
+        match expected with
+        | None -> Assert.Null(actual)
+        | _ -> Assert.Equal(expected.Value, actual)
