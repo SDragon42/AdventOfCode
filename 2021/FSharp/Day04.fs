e8d7053ebcc9423012474c88adb8cdcfbb402e64
SDragon42 <nathen.silver@gmail.com>
2022-10-21 22:21:09 -0600

    Day 4 unit tests

--- 2021/FSharp/Day04.fs
+++ 2021/FSharp/Day04.fs
@@ -1,15 +1,16 @@
-﻿module Day04
+﻿namespace AdventOfCode.FSharp.Year2021
 
 open FSharp.Common
 open System
+open Xunit
+
 
 
 type BingoBoard (data: string list) =
     [<Literal>]
     let MARK = "X"
-
+    
     let mutable boardData: string list = data
-
     let mutable excluded: bool = false
 
     member this.MarkTile(value: string) = 
@@ -33,29 +34,82 @@ type BingoBoard (data: string list) =
         let size = Math.Sqrt(boardData.Length) |> int
         let colRange = [0..size - 1]
         let rows = boardData |> List.splitInto(size)
-        let columns =  colRange |> List.map (fun i -> GetdigitsAt(i, rows))
+        let columns = colRange |> List.map (fun i -> GetdigitsAt(i, rows))
 
-        rows @ columns |> List.map HasLineWon |> List.exists (fun l -> l)
+        rows @ columns
+        |> List.map HasLineWon
+        |> List.exists (fun l -> l)
 
     member this.SumUnmarkedNumbers() =
-        boardData |> List.where (fun x -> x <> MARK) |> List.map int |> List.sum
+        boardData
+        |> List.where (fun x -> x <> MARK)
+        |> List.map int
+        |> List.sum
                 
 
 
-type private PuzzleInput(input, expectedAnswer, boards: BingoBoard list) = 
-    inherit InputAnswer<string list, int option>(input, expectedAnswer)
+type Puzzle04 () =
 
-    member this.Boards = boards
+    // What will your final score be?
+    member this.RunPart1 (input: string list) (boards: BingoBoard list) =
+        let rec Play (numberDraw: string) (remaining: string list) =
+            boards
+            |> List.map (fun b -> b.MarkTile(numberDraw))
+            |> ignore
+
+            let winningBoards =
+                boards 
+                |> List.where (fun b -> b.CheckIfWon())
+
+            match winningBoards.Length with
+            | 0 -> Play remaining[0] remaining[1..]
+            | _ -> (winningBoards[0], numberDraw)
+
+        let winningBoard, winningDraw = Play input[0] input[1..]
+
+        winningBoard.SumUnmarkedNumbers() * int(winningDraw)
+
+
+    // What would its final score be?
+    member this.RunPart2 (input: string list) (boards: BingoBoard list) =
+        let rec Play (numberDraw: string) (remaining: string list) =
+            let numWonBoards = 
+                boards 
+                |> List.where (fun b -> b.IsExcluded())
+                |> List.length
+
+            boards 
+                |> List.where (fun b -> b.IsExcluded() = false)
+                |> List.map (fun b -> b.MarkTile(numberDraw))
+                |> ignore
+
+            let winningBoards =
+                boards
+                |> List.where (fun b -> b.IsExcluded() = false)
+                |> List.where (fun b -> b.CheckIfWon())
+
+            winningBoards
+            |> List.map (fun b -> b.MarkExcluded())
+            |> ignore
+
+            match winningBoards with
+            | _ when winningBoards.Length = 0 -> 
+                Play remaining[0] remaining[1..]
+            | _ when numWonBoards + 1 < boards.Length -> 
+                Play remaining[0] remaining[1..]
+            | _ -> 
+                winningBoards[0], numberDraw
+
+        let winningBoard, winningDraw = Play input[0] input[1..]
+
+        winningBoard.SumUnmarkedNumbers() * int(winningDraw)
 
 
 
-type Day04 (runBenchmarks, runExamples) =
-    inherit PuzzleBase(runBenchmarks, runExamples)
-
-
-    member private this.GetPuzzleInput (part: int, name: string) =
+module ``Day 4: Giant Squid`` =
+    let private GetPuzzleInput (part:int) (name:string) =
         let day = 4
-
+        
         let tempInput =
             InputHelper.LoadText(day, name).Split(Environment.NewLine+Environment.NewLine)
             |> Array.toList
@@ -69,67 +123,35 @@ type Day04 (runBenchmarks, runExamples) =
             tempInput[1..]
             |> List.map (fun b -> b.Split(splitValue, StringSplitOptions.RemoveEmptyEntries) |> Array.toList)
             |> List.map (fun b -> new BingoBoard(b))
-
-        let answer = 
-            InputHelper.LoadAnswer(day, $"%s{name}-answer%i{part}")
-            |> InputHelper.AsInt
         
-        new PuzzleInput(input, answer, boardsData)
-
-
-    member private this.RunPart1 (puzzleData: PuzzleInput) =
-        let rec Play(numberDraw: string, remaining: string list) =
-            puzzleData.Boards |> List.map (fun b -> b.MarkTile(numberDraw)) |> ignore
-
-            let winningBoards =
-                puzzleData.Boards 
-                |> List.where (fun b -> b.CheckIfWon())
-
-            if (winningBoards.Length = 0)
-                then Play(remaining[0], remaining[1..])
-                else winningBoards[0], numberDraw
-
-        let winningBoard, winningDraw = Play(puzzleData.Input[0], puzzleData.Input[1..])
-
-        let result = winningBoard.SumUnmarkedNumbers() * int(winningDraw)
-        Helper.GetPuzzleResultText("What will your final score be?", result, puzzleData.ExpectedAnswer)
-
-
-    member private this.RunPart2 (puzzleData: PuzzleInput) =
-        let rec Play(numberDraw: string, remaining: string list) =
-            let numWonBoards = 
-                puzzleData.Boards 
-                |> List.where (fun b -> b.IsExcluded())
-                |> List.length
-
-            puzzleData.Boards 
-                |> List.where (fun b -> b.IsExcluded() = false)
-                |> List.map (fun b -> b.MarkTile(numberDraw)) |> ignore
-
-            let winningBoards =
-                puzzleData.Boards
-                |> List.where (fun b -> b.IsExcluded() = false)
-                |> List.where (fun b -> b.CheckIfWon())
-
-            winningBoards |> List.map (fun b -> b.MarkExcluded()) |> ignore
-
-            if winningBoards.Length = 0 || numWonBoards + 1 < puzzleData.Boards.Length then
-                Play(remaining[0], remaining[1..])
-            else
-                winningBoards[0], numberDraw
-
-        let winningBoard, winningDraw = Play(puzzleData.Input[0], puzzleData.Input[1..])
-
-        let result = winningBoard.SumUnmarkedNumbers() * int(winningDraw)
-        Helper.GetPuzzleResultText("What would its final score be?", result, puzzleData.ExpectedAnswer)
-
-
-    override this.SolvePuzzle _ = seq {
-        yield "Day 4: Giant Squid"
-        yield this.RunExample(fun _ -> " Ex. 1) " + this.RunPart1(this.GetPuzzleInput(1, "example1")))
-        yield this.RunProblem(fun _ -> "Part 1) " + this.RunPart1(this.GetPuzzleInput(1, "input")))
-
-        yield ""
-        yield this.RunExample(fun _ -> " Ex. 1) " + this.RunPart2(this.GetPuzzleInput(2, "example1")))
-        yield this.RunProblem(fun _ -> "Part 2) " + this.RunPart2(this.GetPuzzleInput(2, "input")))
-        }
+        let answer = 
+            InputHelper.LoadAnswer (day, $"%s{name}-answer%i{part}")
+            |> InputHelper.AsInt
+            
+        input, answer, boardsData
+        
+        
+    [<Theory>]
+    [<InlineData("example1")>]
+    [<InlineData("input")>]
+    let Part1 (name:string) =
+        let input, expected, boardsData = GetPuzzleInput 1 name
+        
+        let actual = (new Puzzle04()).RunPart1 input boardsData
+        
+        match expected with
+        | None -> Assert.Null actual
+        | _ -> Assert.Equal (expected.Value, actual)
+        
+        
+    [<Theory>]
+    [<InlineData("example1")>]
+    [<InlineData("input")>]
+    let Part2 (name:string) =
+        let input, expected, boardsData = GetPuzzleInput 2 name
+        
+        let actual = (new Puzzle04()).RunPart2 input boardsData
+        
+        match expected with
+        | None -> Assert.Null actual
+        | _ -> Assert.Equal (expected.Value, actual)
