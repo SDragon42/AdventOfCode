f03a857fc05a14945f7f43da46abd50b674b1aba
SDragon42 <nathen.silver@gmail.com>
2022-10-21 20:56:10 -0600

    Day 2 unit tests

--- 2021/FSharp/Day02.fs
+++ 2021/FSharp/Day02.fs
@@ -1,7 +1,9 @@
-﻿module Day02
+﻿namespace AdventOfCode.FSharp.Year2021
 
 open FSharp.Common
 open System
+open Xunit
+
 
 
 type private PilotAction =
@@ -12,54 +14,50 @@ type private PilotAction =
 
 
 
-type private PuzzleInput(input, expectedAnswer) =
-    inherit InputAnswer<PilotAction list, int option>(input, expectedAnswer)
+type private Puzzle02 () =
 
-
-
-type Day02 (runBenchmarks, runExamples) =
-    inherit PuzzleBase(runBenchmarks, runExamples)
-
-    member private this.GetPuzzleInput (part: int, name: string) =
-        let day = 2
-
-        let makePilotAction (value: string) =
-            let parts = value.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            new PilotAction(parts[0], parts[1] |> int)
-
-        let input = 
-            InputHelper.LoadLines(day, name)
-            |> Seq.map makePilotAction
-            |> Seq.toList
-
-        let answer = 
-            InputHelper.LoadAnswer(day, $"%s{name}-answer%i{part}")
-            |> InputHelper.AsInt
-
-        new PuzzleInput(input, answer)
-
-
-    member private this.CalculateSubPosition_Flawed(input: PilotAction list, (position, depth): int * int) =
-        if (input.Length = 0) then
+    member private this.CalculateSubPosition_Flawed (input: PilotAction list, (position, depth): int * int) =
+        match input with
+        | _ when input.Length = 0 -> 
             (position, depth)
-        else
+        | _ ->
             let next = input[0]
             let remaining = input[1..]
 
             let values =
                 match next.Direction with
-                | "forward" -> (
-                    position + next.Amount,
-                    depth)
-                | "down" -> (
-                    position,
-                    depth + next.Amount)
-                | "up" -> (
-                    position,
-                    depth - next.Amount)
-                | _ -> (position, depth)
+                | "forward" ->
+                    (position + next.Amount, depth)
+                | "down" ->
+                    (position, depth + next.Amount)
+                | "up" ->
+                    (position, depth - next.Amount)
+                | _ -> 
+                    (position, depth)
 
-            this.CalculateSubPosition_Flawed(remaining, values)
+            this.CalculateSubPosition_Flawed (remaining, values)
+
+
+        //if (input.Length = 0) then
+        //    (position, depth)
+        //else
+        //    let next = input[0]
+        //    let remaining = input[1..]
+
+        //    let values =
+        //        match next.Direction with
+        //        | "forward" -> (
+        //            position + next.Amount,
+        //            depth)
+        //        | "down" -> (
+        //            position,
+        //            depth + next.Amount)
+        //        | "up" -> (
+        //            position,
+        //            depth - next.Amount)
+        //        | _ -> (position, depth)
+
+        //    this.CalculateSubPosition_Flawed (remaining, values)
 
 
     member private this.CalculateSubPosition(input: PilotAction list, (position, depth, aim): int * int * int) =
@@ -88,28 +86,63 @@ type Day02 (runBenchmarks, runExamples) =
             this.CalculateSubPosition(remaining, values)
 
 
-    member private this.RunPart1 (puzzleData: PuzzleInput) =
-        let horizontal, depth = this.CalculateSubPosition_Flawed(puzzleData.Input, (0, 0))
-
+    member this.RunPart1 (input: PilotAction list) =
+        // What do you get if you multiply your final horizontal position by your final depth?
+        let horizontal, depth = this.CalculateSubPosition_Flawed (input, (0, 0))
         let result = horizontal * depth
+        result
 
-        Helper.GetPuzzleResultText("What do you get if you multiply your final horizontal position by your final depth?", result, puzzleData.ExpectedAnswer)
 
-
-    member private this.RunPart2 (puzzleData: PuzzleInput) =
-        let horizontal, depth, _ = this.CalculateSubPosition(puzzleData.Input, (0, 0, 0))
-
+    member this.RunPart2 (input: PilotAction list) =
+        // What do you get if you multiply your final horizontal position by your final depth?
+        let horizontal, depth, _ = this.CalculateSubPosition(input, (0, 0, 0))
         let result = horizontal * depth
+        result
 
-        Helper.GetPuzzleResultText("What do you get if you multiply your final horizontal position by your final depth?", result, puzzleData.ExpectedAnswer)
 
 
-    override this.SolvePuzzle _ = seq {
-        yield "Day 2: Dive!"
-        yield this.RunExample(fun _ -> " Ex. 1) " + this.RunPart1(this.GetPuzzleInput(1, "example1")))
-        yield this.RunProblem(fun _ -> "Part 1) " + this.RunPart1(this.GetPuzzleInput(1, "input")))
+module ``Day 2: Dive!`` =
 
-        yield ""
-        yield this.RunExample(fun _ -> " Ex. 1) " + this.RunPart2(this.GetPuzzleInput(2, "example1")))
-        yield this.RunProblem(fun _ -> "Part 2) " + this.RunPart2(this.GetPuzzleInput(2, "input")))
-        }
+    let private GetPuzzleInput (part:int) (name:string) =
+        let day = 2
+
+        let makePilotAction (value: string) =
+            let parts = value.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            new PilotAction(parts[0], parts[1] |> int)
+
+        let input = 
+            InputHelper.LoadLines(day, name)
+            |> Seq.map makePilotAction
+            |> Seq.toList
+
+        let answer = 
+            InputHelper.LoadAnswer(day, $"%s{name}-answer%i{part}")
+            |> InputHelper.AsInt
+
+        input, answer
+
+
+    [<Theory>]
+    [<InlineData("example1")>]
+    [<InlineData("input")>]
+    let Part1 (name:string) =
+        let input, expected = GetPuzzleInput 1 name
+
+        let actual = (new Puzzle02()).RunPart1 (input)
+
+        match expected with
+        | None -> Assert.Null(actual)
+        | _ -> Assert.Equal(expected.Value, actual)
+
+
+    [<Theory>]
+    [<InlineData("example1")>]
+    [<InlineData("input")>]
+    let Part2 (name:string) =
+        let input, expected = GetPuzzleInput 2 name
+
+        let actual = (new Puzzle02()).RunPart2 (input)
+
+        match expected with
+        | None -> Assert.Null(actual)
+        | _ -> Assert.Equal(expected.Value, actual)
