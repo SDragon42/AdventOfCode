2e79ff38b709f96be6a1eab31474d6ac26989ad8
SDragon42 <nathen.silver@gmail.com>
2022-10-23 14:35:28 -0600

    FS 2019 convert to unit tests

--- 2019/FSharp/Day01.fs
+++ 2019/FSharp/Day01.fs
@@ -1,21 +1,51 @@
-﻿module Day01
+﻿namespace AdventOfCode.FSharp.Year2019
 
-open FSharp.Common
-open Microsoft.FSharp.Linq
 open System
-open System.Linq
+open Xunit
+open FSharp.Common
 
 
-type private Day01PuzzleInput(input, expectedAnswer) = 
-    inherit InputAnswer<int list, int option>(input, expectedAnswer)
+
+module ``Day 1: The Tyranny of the Rocket Equation`` =
+    let day = 1
 
 
-type Day01 (runBenchmarks, runExamples) =
-    inherit PuzzleBase(runBenchmarks, runExamples)
+
+    //-------------------------------------------------------------------------
 
 
-    member private this.GetPuzzleInput (part: int, name: string) =
-        let day = 1
+
+    type Puzzle () =
+
+        member private this.CalcFuel mass : int =
+            (mass / 3) - 2
+
+
+        member private this.CalcFuelCorrectly mass : int =
+            let fuelMass = this.CalcFuel(mass)
+            match fuelMass with
+            | _ when fuelMass > 0 -> fuelMass + this.CalcFuelCorrectly(fuelMass)
+            | _ -> 0
+
+
+        // What is the sum of the fuel requirements?
+        member this.RunPart1 (input:int list) =
+            let totalFuel = input |> List.sumBy this.CalcFuel
+            totalFuel
+
+
+        // What is the sum of the fuel requirements?
+        member this.RunPart2 (input:int list) =
+            let totalFuel = input |> List.sumBy this.CalcFuelCorrectly
+            totalFuel
+
+
+
+    //-------------------------------------------------------------------------
+
+
+
+    let private GetPuzzleInput (part:int) (name:string) =
         let input = 
             InputHelper.LoadLines(day, name)
             |> Seq.map int
@@ -24,45 +54,36 @@ type Day01 (runBenchmarks, runExamples) =
         let answer =
             InputHelper.LoadAnswer(day, $"%s{name}-answer%i{part}")
             |> InputHelper.AsInt
-        
-        new Day01PuzzleInput(input, answer)
+
+        input, answer
 
 
-    member private this.CalcFuel mass : int =
-        (mass / 3) - 2
+
+    [<Theory>]
+    [<InlineData("example1")>]
+    [<InlineData("example2")>]
+    [<InlineData("example3")>]
+    [<InlineData("example4")>]
+    [<InlineData("input")>]
+    let Part1 (name:string) =
+        let input, expected = GetPuzzleInput 1 name
+
+        let actual = (new Puzzle()).RunPart1 input
+
+        match expected with
+        | None -> Assert.Null actual
+        | _ -> Assert.Equal (expected.Value, actual)
 
 
-    member private this.CalcFuelCorrectly mass : int =
-        let fuelMass = this.CalcFuel(mass)
-        match fuelMass with
-        | _ when fuelMass > 0 -> fuelMass + this.CalcFuelCorrectly(fuelMass)
-        | _ -> 0
-        //if (fuelMass > 0) then
-        //    fuelMass + this.CalcFuelCorrectly(fuelMass)
-        //else
-        //    0
+    [<Theory>]
+    [<InlineData("example2")>]
+    [<InlineData("example3")>]
+    [<InlineData("input")>]
+    let Part2 (name:string) =
+        let input, expected = GetPuzzleInput 2 name
 
+        let actual = (new Puzzle()).RunPart2 input
 
-    member private this.RunPart1 (puzzleData: Day01PuzzleInput) =
-        let totalFuel = puzzleData.Input |> List.sumBy this.CalcFuel
-        Helper.GetPuzzleResultText($"What is the sum of the fuel requirements?", totalFuel, puzzleData.ExpectedAnswer)
-
-
-    member private this.RunPart2 (puzzleData: Day01PuzzleInput) =
-        let totalFuel = puzzleData.Input |> List.sumBy this.CalcFuelCorrectly
-        Helper.GetPuzzleResultText($"What is the sum of the fuel requirements?", totalFuel, puzzleData.ExpectedAnswer)
-
-
-    override this.SolvePuzzle _ = seq {
-        yield "Day 1: The Tyranny of the Rocket Equation"
-        yield this.RunExample(fun _ -> " Ex. 1) " + this.RunPart1(this.GetPuzzleInput(1, "example1")))
-        yield this.RunExample(fun _ -> " Ex. 2) " + this.RunPart1(this.GetPuzzleInput(1, "example2")))
-        yield this.RunExample(fun _ -> " Ex. 3) " + this.RunPart1(this.GetPuzzleInput(1, "example3")))
-        yield this.RunExample(fun _ -> " Ex. 4) " + this.RunPart1(this.GetPuzzleInput(1, "example4")))
-        yield this.RunProblem(fun _ -> "Part 1) " + this.RunPart1(this.GetPuzzleInput(1, "input")))
-
-        yield ""
-        yield this.RunExample(fun _ -> " Ex. 2) " + this.RunPart2(this.GetPuzzleInput(2, "example2")))
-        yield this.RunExample(fun _ -> " Ex. 3) " + this.RunPart2(this.GetPuzzleInput(2, "example3")))
-        yield this.RunProblem(fun _ -> "Part 2) " + this.RunPart2(this.GetPuzzleInput(2, "input")))
-        }
+        match expected with
+        | None -> Assert.Null actual
+        | _ -> Assert.Equal (expected.Value, actual)
