10f382f4d732e767b2ff456a401959d7d637be7a
Nathen Silver <nathen.silver@gmail.com>
2022-11-09 17:44:32 -0700

    Added the 2022 C# project

---
+++ 2022/CSharp/Template.cs
@@ -0,0 +1,62 @@
+namespace AdventOfCode.CSharp.Year2022;
+
+public class Template
+{
+    private const int DAY = 0;
+
+    private readonly ITestOutputHelper output;
+    public Template(ITestOutputHelper output) => this.output = output;
+
+
+
+    private (List<int>, int?) GetTestData(int part, string inputName)
+    {
+        var input = InputHelper.LoadInputFile(DAY, inputName)
+            .Select(l => l.ToInt32())
+            .ToList();
+
+        var expected = InputHelper.LoadAnswerFile(DAY, part, inputName)
+            ?.FirstOrDefault()
+            ?.ToInt32();
+
+        return (input, expected);
+    }
+
+
+
+    [Theory]
+    [InlineData(1, "example1")]
+    [InlineData(1, "input")]
+    public void Part1(int part, string inputName)
+    {
+        var (input, expected) = GetTestData(part, inputName);
+
+        var value = DoWork(input);
+
+        output.WriteLine($"Answer: {value}");
+
+        Assert.Equal(expected, value);
+    }
+
+
+    [Theory]
+    [InlineData(2, "example1")]
+    [InlineData(2, "input")]
+    public void Part2(int part, string inputName)
+    {
+        var (input, expected) = GetTestData(part, inputName);
+
+        var value = DoWork(input);
+
+        output.WriteLine($"Answer: {value}");
+
+        Assert.Equal(expected, value);
+    }
+
+
+
+    private int DoWork(List<int> input)
+    {
+        return -1;
+    }
+}
