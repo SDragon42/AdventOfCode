b599b12bfe971408785d0c187115bd06ca2cf3b6
Nathen Silver <nathen.silver@gmail.com>
2022-11-09 17:37:41 -0700

    Started to add unit tests for CSharp.Common

---
+++ CSharp.Common.Tests/HelperTests.cs
@@ -0,0 +1,62 @@
+﻿using Xunit.Abstractions;
+
+namespace CSharp.Common.Tests;
+
+public class HelperTests
+{
+    private ITestOutputHelper _output;
+    public HelperTests(ITestOutputHelper output)
+    {
+        _output= output;
+    }
+
+    [Theory]
+    [InlineData(123456789, 1, 9)]
+    [InlineData(123456789, 2, 8)]
+    public void GetDigitRight_INT(int value, int offset, int expected)
+    {
+        var result = Helper.GetDigitRight(value, offset);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(123456789L, 1, 9)]
+    [InlineData(123456789L, 2, 8)]
+    public void GetDigitRight_LONG(long value, int offset, int expected)
+    {
+        var result = Helper.GetDigitRight(value, offset);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(123456789, 1, 1)]
+    [InlineData(123456789, 2, 2)]
+    [InlineData(42, 2, 2)]
+    [InlineData(123456, 2, 2)]
+    public void GetDigitLeft_INT(int value, int offset, int expected)
+    {
+        var result = Helper.GetDigitLeft(value, offset);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void GetPermutations()
+    {
+        var input = new[] { 1, 2, 3 };
+        var expected = 6;
+
+        var results = Helper.GetPermutations(input)
+            .ToList();
+
+        foreach (var item in results)
+        {
+            var text = string.Join(",", item);
+            _output.WriteLine($"({text})");
+        }
+
+        Assert.Equal(expected, results.Count());
+    }
+}
---
+++ CSharp.Common.Tests/Usings.cs
@@ -0,0 +1,10 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Drawing;
+global using System.IO;
+global using System.Linq;
+global using System.Text;
+
+global using Xunit;
+
+global using AdventOfCode.CSharp.Common;
