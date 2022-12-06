16541251833de1c45887ff1d4109bb22ac720698
Nathen Silver <nathen.silver@gmail.com>
2022-12-06 14:22:38 -0700

    split extension methods into separate classes. removed tests as they were testing .NET functionality.

---
+++ CSharp.Common/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode.CSharp.Common;
+
+public static class IEnumerableExtensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="elements"></param>
+    /// <param name="actionMethod"></param>
+    public static void ForEach<T>(this IEnumerable<T> elements, Action<T> actionMethod)
+    {
+        foreach (var item in elements)
+            actionMethod(item);
+    }
+
+}
