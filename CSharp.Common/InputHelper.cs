116a4c176d87a8b0df4abcede0d7c6783a36bfa4
Nathen Silver <nathen.silver@gmail.com>
2022-12-06 16:41:18 -0700

    inital change for the input helper

--- CSharp.Common/InputHelper.cs
+++ CSharp.Common/InputHelper.cs
@@ -2,17 +2,45 @@
 
 public static class InputHelper
 {
+    [Obsolete]
     public static IEnumerable<string> LoadInputFile(int day, string name)
     {
-        var filename = $@".\input\Day{day:00}\{name}.txt";
+        var filename = GetDataFilePath(day, name);
         return File.ReadLines(filename);
     }
+
+    [Obsolete]
     public static IEnumerable<string> LoadAnswerFile(int day, int part, string name)
     {
         try
         {
-            var filename = $@".\input\Day{day:00}\{name}-answer{part}.txt";
+            var filename = GetDataFilePath(day, $"{name}-answer{part}");
             return File.ReadLines(filename);
+        }
+        catch { return null; }
+    }
+
+
+    public static string GetDataFilePath(int day, string name)
+    {
+        var filename = $@".\input\Day{day:00}\{name}.txt";
+        return filename;
+    }
+
+    public static IEnumerable<string> ReadLines(string path)
+    {
+        try
+        {
+            return File.ReadLines(path);
+        }
+        catch { return null; }
+    }
+
+    public static string ReadText(string path)
+    {
+        try
+        {
+            return File.ReadAllText(path);
         }
         catch { return null; }
     }
