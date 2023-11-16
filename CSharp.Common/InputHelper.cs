a5224cb6a98e507ff3d188477da18e5709f5af4d
SDragon42 <nathen.silver@gmail.com>
2023-11-15 20:29:01 -0700

    Added support for specifying a root path. Needed for NUnit to read data files.

--- CSharp.Common/InputHelper.cs
+++ CSharp.Common/InputHelper.cs
@@ -30,10 +30,11 @@ namespace AdventOfCode.CSharp.Common
         /// </summary>
         /// <param name="day">The puzzle day for the data files.</param>
         /// <param name="name">The name of the data file without extension.</param>
+        /// <param name="rootPath">The root path.</param>
         /// <returns></returns>
-        private static string GetDataFilePath(int day, string name)
+        private static string GetDataFilePath(int day, string name, string rootPath = ".")
         {
-            var filename = $@".\input\Day{day:00}\{name}.txt";
+            var filename = $@"{rootPath}\input\Day{day:00}\{name}.txt";
             return filename;
         }
 
@@ -42,12 +43,13 @@ namespace AdventOfCode.CSharp.Common
         /// </summary>
         /// <param name="day">The puzzle day for the data files.</param>
         /// <param name="name">The name of the data file without extension.</param>
+        /// <param name="rootPath">The root path.</param>
         /// <returns></returns>
-        public static IEnumerable<string> ReadLines(int day, string name)
+        public static IEnumerable<string> ReadLines(int day, string name, string rootPath = ".")
         {
             try
             {
-                var path = GetDataFilePath(day, name);
+                var path = GetDataFilePath(day, name, rootPath);
                 return File.ReadLines(path);
             }
             catch { return null; }
@@ -58,10 +60,11 @@ namespace AdventOfCode.CSharp.Common
         /// </summary>
         /// <param name="day">The puzzle day for the data files.</param>
         /// <param name="name">The name of the data file without extension.</param>
+        /// <param name="rootPath">The root path.</param>
         /// <returns></returns>
-        public static string ReadText(int day, string name)
+        public static string ReadText(int day, string name, string rootPath = ".")
         {
-            var lines = ReadLines(day, name);
+            var lines = ReadLines(day, name, rootPath);
             if (lines == null)
                 return null;
 
