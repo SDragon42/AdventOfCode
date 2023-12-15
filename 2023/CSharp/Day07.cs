b8a2960009aa2e411071a525c22b5eb3f2bd5dae
Nathen Silver <nathen.silver@gmail.com>
2023-12-15 10:57:24 -0700

    merged non-joker and joker handling

--- 2023/CSharp/Day07.cs
+++ 2023/CSharp/Day07.cs
@@ -6,8 +6,11 @@ public class Day07
 
     private readonly ITestOutputHelper output;
 
-    private readonly Dictionary<char, long> CardTypes = new();
-    private readonly List<(Func<string, bool> calc, long value)> HandTypes = new();
+    private readonly Dictionary<char, long> cardTypes = new();
+    private readonly List<(Func<string, bool> calc, long value)> handTypes = new();
+    private bool useJokers = false;
+
+    private const char JOKER = 'J';
 
     public Day07(ITestOutputHelper output) => this.output = output;
 
@@ -60,6 +63,7 @@ public class Day07
     [InlineData(2, "input")]
     public void Part2(int part, string inputName)
     {
+        useJokers = true;
         SetCardAndHandTypes(replaceJacksWithJokers: true);
 
         var (input, expected) = GetTestData(part, inputName);
@@ -78,93 +82,78 @@ public class Day07
 
 
     #region Hand Type Tests
-
     [Trait("Method Tests", "")]
     [Theory]
-    [InlineData("AAAAA", true)]
-    [InlineData("AAAJA", false)]
-    [InlineData("JJJJJ", true)]
-    public void Test_IsFiveOfaKind(string cards, bool expected) => Assert.Equal(expected, IsFiveOfaKind(cards));
-
-    [Trait("Method Tests", "")]
-    [Theory]
-    [InlineData("AAAAA", true)]
-    [InlineData("AJAAA", true)]
-    [InlineData("AJAAK", false)]
-    [InlineData("JJJJJ", true)]
-    public void Test_IsFiveOfaKind_WithJokers(string cards, bool expected) => Assert.Equal(expected, IsFiveOfaKindWithJokers(cards));
-
-
-    [Trait("Method Tests", "")]
-    [Theory]
-    [InlineData("AA8AA", true)]
-    [InlineData("AA8A1", false)]
-    public void Test_IsFourOfaKind(string cards, bool expected) => Assert.Equal(expected, IsFourOfaKind(cards));
-
-    [Trait("Method Tests", "")]
-    [Theory]
-    [InlineData("AA8AA", true)]
-    [InlineData("AA8AJ", true)]
-    [InlineData("QJJQ2", true)]
-    [InlineData("QJ1Q2", false)]
-    public void Test_IsFourOfaKind_WithJokers(string cards, bool expected) => Assert.Equal(expected, IsFourOfaKindWithJokers(cards));
+    [InlineData("AAAAA", 5, false, true)]
+    [InlineData("AAAJA", 5, false, false)]
+    [InlineData("JJJJJ", 5, false, true)]
+    [InlineData("AAAAA", 5, true, true)]
+    [InlineData("AJAAA", 5, true, true)]
+    [InlineData("AJAAK", 5, true, false)]
+    [InlineData("JJJJJ", 5, true, true)]
+    [InlineData("AA8AA", 4, false, true)]
+    [InlineData("AA8A1", 4, false, false)]
+    [InlineData("AA8AA", 4, true, true)]
+    [InlineData("AA8AJ", 4, true, true)]
+    [InlineData("QJJQ2", 4, true, true)]
+    [InlineData("QJ1Q2", 4, true, false)]
+    [InlineData("TTT98", 3, false, true)]
+    [InlineData("TJT98", 3, false, false)]
+    [InlineData("TTT98", 3, true, true)]
+    [InlineData("TJT98", 3, true, true)]
+    [InlineData("KJT98", 3, true, false)]
+    public void Test_IsXOfaKind(string cards, int count, bool useJokers, bool expected)
+    {
+        this.useJokers = useJokers;
+        var result = IsXOfaKind(cards, count);
+        Assert.Equal(expected, result);
+    }
 
 
     [Trait("Method Tests", "")]
     [Theory]
-    [InlineData("23332", true)]
-    [InlineData("23331", false)]
-    [InlineData("23312", false)]
-    public void Test_IsFullHouse(string cards, bool expected) => Assert.Equal(expected, IsFullHouse(cards));
-
-    [Trait("Method Tests", "")]
-    [Theory]
-    [InlineData("J3332", true)]
-    [InlineData("23331", false)]
-    [InlineData("23312", false)]
-    [InlineData("JJ332", true)]
-    [InlineData("JJ3J2", true)]
-    public void Test_IsFullHouse_WithJokers(string cards, bool expected) => Assert.Equal(expected, IsFullHouseWithJokers(cards));
-
-
-    [Trait("Method Tests", "")]
-    [Theory]
-    [InlineData("TTT98", true)]
-    [InlineData("TJT98", false)]
-    public void Test_IsThreeOfaKind(string cards, bool expected) => Assert.Equal(expected, IsThreeOfaKind(cards));
-
-    [Trait("Method Tests", "")]
-    [Theory]
-    [InlineData("TTT98", true)]
-    [InlineData("TJT98", true)]
-    [InlineData("KJT98", false)]
-    public void Test_IsThreeOfaKind_WithJokers(string cards, bool expected) => Assert.Equal(expected, IsThreeOfaKindWithJokers(cards));
+    [InlineData("23332", false, true)]
+    [InlineData("23331", false, false)]
+    [InlineData("23312", false, false)]
+    [InlineData("J3332", true, true)]
+    [InlineData("23331", true, false)]
+    [InlineData("23312", true, false)]
+    [InlineData("JJ332", true, true)]
+    [InlineData("JJ3J2", true, true)]
+    public void Test_IsFullHouse(string cards, bool useJokers, bool expected)
+    {
+        this.useJokers = useJokers;
+        var result = IsFullHouse(cards);
+        Assert.Equal(expected, result);
+    }
 
 
     [Trait("Method Tests", "")]
     [Theory]
-    [InlineData("23432", true)]
-    [InlineData("2343J", false)]
-    public void Test_IsTwoPair(string cards, bool expected) => Assert.Equal(expected, IsTwoPair(cards));
+    [InlineData("23432", false, true)]
+    [InlineData("2343J", false, false)]
+    [InlineData("23432", true, true)]
+    [InlineData("2343J", true, true)]
+    public void Test_IsTwoPair(string cards, bool useJokers, bool expected)
+    {
+        this.useJokers = useJokers;
+        var result = IsTwoPair(cards);
+        Assert.Equal(expected, result);
+    }
+
 
     [Trait("Method Tests", "")]
     [Theory]
-    [InlineData("23432", true)]
-    [InlineData("2343J", true)]
-    public void Test_IsTwoPair_WithJokers(string cards, bool expected) => Assert.Equal(expected, IsTwoPairWithJokers(cards));
-
-
-    [Trait("Method Tests", "")]
-    [Theory]
-    [InlineData("A23A4", true)]
-    [InlineData("A23J4", false)]
-    public void Test_IsOnePair(string cards, bool expected) => Assert.Equal(expected, IsOnePair(cards));
-
-    [Trait("Method Tests", "")]
-    [Theory]
-    [InlineData("A23A4", true)]
-    [InlineData("A23J4", true)]
-    public void Test_IsOnePair_WithJokers(string cards, bool expected) => Assert.Equal(expected, IsOnePairWithJokers(cards));
+    [InlineData("A23A4", false, true)]
+    [InlineData("A23J4", false, false)]
+    [InlineData("A23A4", true, true)]
+    [InlineData("A23J4", true, true)]
+    public void Test_IsOnePair(string cards, bool useJokers, bool expected)
+    {
+        this.useJokers = useJokers;
+        var result = IsOnePair(cards);
+        Assert.Equal(expected, result);
+    }
 
     #endregion
 
@@ -172,55 +161,43 @@ public class Day07
 
     private void SetCardAndHandTypes(bool replaceJacksWithJokers = false)
     {
-        CardTypes.Clear();
-        CardTypes.Add('A', 14);
-        CardTypes.Add('K', 13);
-        CardTypes.Add('Q', 12);
-        CardTypes.Add('J', 11);
-        CardTypes.Add('T', 10);
-        CardTypes.Add('9', 9);
-        CardTypes.Add('8', 8);
-        CardTypes.Add('7', 7);
-        CardTypes.Add('6', 6);
-        CardTypes.Add('5', 5);
-        CardTypes.Add('4', 4);
-        CardTypes.Add('3', 3);
-        CardTypes.Add('2', 2);
+        cardTypes.Clear();
+        cardTypes.Add('A', 14);
+        cardTypes.Add('K', 13);
+        cardTypes.Add('Q', 12);
+        cardTypes.Add('J', 11);
+        cardTypes.Add('T', 10);
+        cardTypes.Add('9', 9);
+        cardTypes.Add('8', 8);
+        cardTypes.Add('7', 7);
+        cardTypes.Add('6', 6);
+        cardTypes.Add('5', 5);
+        cardTypes.Add('4', 4);
+        cardTypes.Add('3', 3);
+        cardTypes.Add('2', 2);
 
         if (replaceJacksWithJokers)
-            CardTypes['J'] = 1;
+            cardTypes[JOKER] = 1;
 
-        HandTypes.Clear();
-        HandTypes.Add((
-            replaceJacksWithJokers ? IsFiveOfaKindWithJokers : IsFiveOfaKind,
-            60000000000));
-        HandTypes.Add((
-            replaceJacksWithJokers ? IsFourOfaKindWithJokers : IsFourOfaKind,
-            50000000000));
-        HandTypes.Add((
-            replaceJacksWithJokers ? IsFullHouseWithJokers : IsFullHouse,
-            40000000000));
-        HandTypes.Add((
-            replaceJacksWithJokers ? IsThreeOfaKindWithJokers : IsThreeOfaKind,
-            30000000000));
-        HandTypes.Add((
-            replaceJacksWithJokers ? IsTwoPairWithJokers : IsTwoPair,
-            20000000000));
-        HandTypes.Add((
-            replaceJacksWithJokers ? IsOnePairWithJokers : IsOnePair,
-            10000000000));
+        handTypes.Clear();
+        handTypes.Add((IsFiveOfaKind, 60000000000));
+        handTypes.Add((IsFourOfaKind, 50000000000));
+        handTypes.Add((IsFullHouse, 40000000000));
+        handTypes.Add((IsThreeOfaKind, 30000000000));
+        handTypes.Add((IsTwoPair, 20000000000));
+        handTypes.Add((IsOnePair, 10000000000));
     }
 
 
 
     private CamalHand RankHand(CamalHand hand)
     {
-        var typeScore = HandTypes
+        var typeScore = handTypes
             .FirstOrDefault(ht => ht.calc(hand.Cards))
             .value;
 
         var cardScore = hand.Cards.Reverse()
-            .Select((c, i) => CardTypes[c] * GetPositionModifier(i))
+            .Select((c, i) => cardTypes[c] * GetPositionModifier(i))
             .ToArray()
             .Sum();
 
@@ -232,91 +209,64 @@ public class Day07
 
 
     private bool IsFiveOfaKind(string cards) => IsXOfaKind(cards, 5);
-    private bool IsFiveOfaKindWithJokers(string cards) => IsXOfaKindWithJokers(cards, 5);
 
     private bool IsFourOfaKind(string cards) => IsXOfaKind(cards, 4);
-    private bool IsFourOfaKindWithJokers(string cards) => IsXOfaKindWithJokers(cards, 4);
 
     private bool IsFullHouse(string cards)
     {
         var cardCounts = GetCardCounts(cards);
-
-        var has3 = cardCounts.Any(c => c.Value == 3);
-        var has2 = cardCounts.Any(c => c.Value == 2);
-
-        var result = has3 && has2;
-        return result;
-    }
-    private bool IsFullHouseWithJokers(string cards)
-    {
-        var cardCounts = GetCardCounts(cards);
-        cardCounts.Remove('J', out int jokerCount);
-
-        var keys = cardCounts.Keys.ToArray();
-
-        var orderedCounts = cardCounts.AsEnumerable()
-            .OrderByDescending(kv => kv.Value)
-            .ToArray()
-            ;
+        var jokerCount = 0;
+        if (useJokers)
+            cardCounts.Remove(JOKER, out jokerCount);
 
         var has3 = false;
         var has2 = false;
 
+        var orderedCounts = cardCounts.AsEnumerable()
+            .OrderByDescending(kv => kv.Value)
+            .ToArray();
+
         for (var i = 0; i < orderedCounts.Length; i++)
         {
             var kv = orderedCounts[i];
-            if (!has3)
-            {
-                var needed = 3 - kv.Value;
-                if (needed == 0)
-                {
-                    has3 = true;
-                    continue;
-                }
-                if (needed > 0 && needed <= jokerCount)
-                {
-                    jokerCount -= needed;
-                    has3 = true;
-                    continue;
-                }
-            }
-            if (!has2)
-            {
-                var needed = 2 - kv.Value;
-                if (needed == 0)
-                {
-                    has2 = true;
-                    continue;
-                }
-                if (needed > 0 && needed <= jokerCount)
-                {
-                    jokerCount -= needed;
-                    has2 = true;
-                    continue;
-                }
-            }
+            if (HadFoundneeded(3, ref has3, kv))
+                continue;
+            if (HadFoundneeded(2, ref has2, kv))
+                continue;
         }
 
         var result = has3 && has2;
         return result;
+
+        bool HadFoundneeded(int minNeeded, ref bool hasNeeded, KeyValuePair<char, int> kv)
+        {
+            if (hasNeeded)
+                return false;
+
+            var needed = minNeeded - kv.Value;
+            if (needed == 0)
+            {
+                hasNeeded = true;
+                return true;
+            }
+            if (needed > 0 && needed <= jokerCount)
+            {
+                jokerCount -= needed;
+                hasNeeded = true;
+                return true;
+            }
+            return false;
+        }
     }
 
     private bool IsThreeOfaKind(string cards) => IsXOfaKind(cards, 3);
-    private bool IsThreeOfaKindWithJokers(string cards) => IsXOfaKindWithJokers(cards, 3);
 
     private bool IsTwoPair(string cards)
     {
-        var numPairs = GetCardCounts(cards)
-            .Where(kv => kv.Value == 2)
-            .Count();
-
-        var result = (numPairs == 2);
-        return result;
-    }
-    private bool IsTwoPairWithJokers(string cards)
-    {
         var cardCounts = GetCardCounts(cards);
-        cardCounts.Remove('J', out int jokerCount);
+        var jokerCount = 0;
+        if (useJokers)
+            cardCounts.Remove(JOKER, out jokerCount);
 
         var keys = cardCounts.Keys.ToArray();
 
@@ -345,17 +295,10 @@ public class Day07
 
     private bool IsOnePair(string cards)
     {
-        var numPairs = GetCardCounts(cards)
-            .Where(kv => kv.Value == 2)
-            .Count();
-
-        var result = (numPairs == 1);
-        return result;
-    }
-    private bool IsOnePairWithJokers(string cards)
-    {
         var cardCounts = GetCardCounts(cards);
-        cardCounts.Remove('J', out int jokerCount);
+        var jokerCount = 0;
+        if (useJokers)
+            cardCounts.Remove(JOKER, out jokerCount);
 
         var numPairs = cardCounts
             .Where(kv => kv.Value + jokerCount >= 2)
@@ -367,22 +310,19 @@ public class Day07
 
 
 
-    private static bool IsXOfaKind(string cards, int count)
-    {
-        var result = GetCardCounts(cards)
-            .Any(kv => kv.Value == count);
-        return result;
-    }
-    private static bool IsXOfaKindWithJokers(string cards, int count)
+    private bool IsXOfaKind(string cards, int count)
     {
         var cardCounts = GetCardCounts(cards);
-        cardCounts.Remove('J', out int jokerCount);
+        if (useJokers)
+        {
+            cardCounts.TryGetValue(JOKER, out int jokerCount);
 
-        if (jokerCount >= count) 
-            return true;
+            foreach (var key in cardCounts.Keys.Where(k => k != JOKER))
+                cardCounts[key] += jokerCount;
+        }
 
         var result = cardCounts
-            .Any(kv => kv.Value + jokerCount >= count);
+            .Any(kv => kv.Value >= count);
         return result;
     }
 
@@ -390,10 +330,7 @@ public class Day07
     {
         var result = cards.Distinct()
             .Select(c => (card: c, count: cards.Count(z => z == c)))
-            .ToDictionary(
-                c => c.card,
-                c => c.count
-                );
+            .ToDictionary(k => k.card, v => v.count);
         return result;
     }
 }
