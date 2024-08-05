fc58116589beafa8ed71b703388071ae9cf9dd6a
Nathen Silver <nathen.silver@gmail.com>
2024-08-05 11:22:45 -0600

    Day 14, part 2

--- 2015/CSharp/Day14.cs
+++ 2015/CSharp/Day14.cs
@@ -1,9 +1,9 @@
-﻿using AdventOfCode.CSharp.Common;
-using NUnit.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using AdventOfCode.CSharp.Common;
+using NUnit.Framework;
 
 namespace AdventOfCode.CSharp.Year2015
 {
@@ -68,14 +68,25 @@ namespace AdventOfCode.CSharp.Year2015
         {
             var (input, expected) = GetTestData(part, inputName);
 
-            var result = GetTheDistanceWinnerReindeerTraveled(input, raceTime);
+            var result = GetTheDistanceWinningReindeerTraveled(input, raceTime);
+            Output($"Answer: {result}");
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(2, "example1", 1000)]
+        [TestCase(2, "input", 2503)]
+        public void Part2(int part, string inputName, int raceTime)
+        {
+            var (input, expected) = GetTestData(part, inputName);
+
+            var result = GetThePointsWinningReindeerHas(input, raceTime);
             Output($"Answer: {result}");
             Assert.AreEqual(expected, result);
         }
 
 
 
-        private int GetTheDistanceWinnerReindeerTraveled(IDictionary<string, IReindeerStats> input, int raceTime)
+        private int GetTheDistanceWinningReindeerTraveled(IDictionary<string, IReindeerStats> input, int raceTime)
         {
             var result = input
                 .Select(kv => CalculateTotalFightDistance(kv.Value, raceTime))
@@ -84,6 +95,29 @@ namespace AdventOfCode.CSharp.Year2015
 
             return result;
         }
+
+        private int GetThePointsWinningReindeerHas(IDictionary<string, IReindeerStats> input, int raceTime)
+        {
+            var reindeerScores = input.ToDictionary(
+                kv => kv.Key,
+                kv => 0);
+
+            for (int i = 0; i < raceTime; i++)
+            {
+                var distanceAtTime = input.Select(kv => (reindeer: kv.Key, distance: CalculateTotalFightDistance(kv.Value, i + 1)))
+                    .ToArray();
+                var maxDistance = distanceAtTime.Max(x => x.distance);
+                var leadingReindeers = distanceAtTime.Where(x => x.distance == maxDistance);
+                foreach (var reindeer in leadingReindeers)
+                    reindeerScores[reindeer.reindeer] += 1;
+            }
+
+            return reindeerScores
+                .OrderByDescending(rs => rs.Value)
+                .First()
+                .Value;
+        }
+
 
         private int CalculateTotalFightDistance(IReindeerStats stats, int raceTime)
         {
