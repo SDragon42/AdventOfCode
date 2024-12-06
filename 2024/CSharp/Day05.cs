3b1d94900eb1a442751ea480f33bf523e4f634d6
Nathen Silver <nathen.silver@gmail.com>
2024-12-05 20:03:43 -0700

    2024 Day05 part2

--- 2024/CSharp/Day05.cs
+++ 2024/CSharp/Day05.cs
@@ -6,7 +6,7 @@ public class Day05(ITestOutputHelper output)
 
 
 
-    private (IList<(int, int)> pageOrderRules, IList<IList<int>> pageUpdates, int? expected) GetTestData(int part, string inputName)
+    private (IList<(int, int)> pageOrderRules, IList<int[]> pageUpdates, int? expected) GetTestData(int part, string inputName)
     {
         var input = InputHelper.ReadLines(DAY, inputName);
 
@@ -19,12 +19,12 @@ public class Day05(ITestOutputHelper output)
             pageOrderRules.Add((parts[0].ToInt32(), parts[1].ToInt32()));
         }
 
-        IList<IList<int>> pageUpdates = [];
+        IList<int[]> pageUpdates = [];
         while (inputEnumerator.MoveNext())
         {
             var value = inputEnumerator.Current.Split(',')
                                                .Select(int.Parse)
-                                               .ToList();
+                                               .ToArray();
             pageUpdates.Add(value);
         }
 
@@ -42,7 +42,9 @@ public class Day05(ITestOutputHelper output)
     {
         var (pageOrderRules, pageUpdates, expected) = GetTestData(part, inputName);
 
-        var value = pageUpdates.Where(pu => IsCorrectlyOrdered(pu, pageOrderRules))
+        var rulesComparer = new RulesComparer(pageOrderRules);
+
+        var value = pageUpdates.Where(pu => IsValidOrder(pu[0], pu[1..], rulesComparer))
                                .Select(GetMiddlePageNumber)
                                .Sum();
 
@@ -51,33 +53,59 @@ public class Day05(ITestOutputHelper output)
         Assert.Equal(expected, value);
     }
 
-    private bool IsCorrectlyOrdered(IList<int> pageUpdates, IList<(int, int)> pageOrderRules)
+    [Theory]
+    [InlineData(2, "example1")]
+    [InlineData(2, "input")]
+    public void Part2(int part, string inputName)
     {
-        return IsValidOrder(pageUpdates.First(),
-                            pageUpdates.Skip(1),
-                            pageOrderRules);
+        var (pageOrderRules, pageUpdates, expected) = GetTestData(part, inputName);
+
+        var rulesComparer = new RulesComparer(pageOrderRules);
+
+        var value = pageUpdates.Where(pu => !IsValidOrder(pu[0], pu[1..], rulesComparer))
+                               .Select(pu => pu.OrderBy(i => i, rulesComparer).ToArray())
+                               .Select(GetMiddlePageNumber)
+                               .Sum();
+
+        output.WriteLine($"Answer: {value}");
+
+        Assert.Equal(expected, value);
     }
 
-    private bool IsValidOrder(int? first, IEnumerable<int> remainder, IList<(int left, int right)> pageOrderRules)
+
+    private bool IsValidOrder(int left, int[] remainder, RulesComparer rulesComparer)
     {
-        if (remainder.Count() == 0)
+        if (remainder.Length == 0)
         {
             return true;
         }
 
-        var result = remainder.Any(value => pageOrderRules.Where(rule => rule.left == value)
-                                                          .Where(r => r.right == first)
-                                                          .Any());
-        if (result) return false;
+        var result = remainder.Any(right => rulesComparer.Compare(left, right) > 0);
 
-        result = IsValidOrder(remainder.First(), remainder.Skip(1), pageOrderRules);
+        if (result)
+        {
+            return false;
+        }
+
+        result = IsValidOrder(remainder[0], remainder[1..], rulesComparer);
         return result;
     }
-
 
     private int GetMiddlePageNumber(IList<int> pageUpdates)
     {
         var idx = pageUpdates.Count / 2;
         return pageUpdates[idx];
+    }
+
+    private class RulesComparer(IList<(int Left, int Right)> _pageOrderRules) : IComparer<int>
+    {
+        public int Compare(int left, int right)
+        {
+            var result = _pageOrderRules.Where(rule => rule.Left == right)
+                                        .Where(rule => rule.Right == left)
+                                        .Any();
+
+            return result ? 1 : -1;
+        }
     }
 }
