6258b4cd4e643603a89e7cb8ffdfd90d41116697
Nathen Silver <nathen.silver@gmail.com>
2024-12-15 19:37:06 -0700

    2024 Day 9 part 2

--- 2024/CSharp/Day09.cs
+++ 2024/CSharp/Day09.cs
@@ -1,8 +1,4 @@
-﻿
-using System;
-using System.Reflection;
-
-namespace AdventOfCode.CSharp.Year2024;
+﻿namespace AdventOfCode.CSharp.Year2024;
 
 public class Day09(ITestOutputHelper output)
 {
@@ -37,7 +33,21 @@ public class Day09(ITestOutputHelper output)
 
         output.WriteLine($"Answer: {value}");
 
-        if (value >= 6307275793643) output.WriteLine("   Too High");
+        Assert.Equal(expected, value);
+    }
+
+    [Theory]
+    [InlineData(2, "example1")]
+    [InlineData(2, "input")]
+    public void Part2(int part, string inputName)
+    {
+        var (input, expected) = GetTestData(part, inputName);
+
+        var blocks = ConvertDiskMapToBlocks(input).ToArray();
+        ShiftWholeFilesLeft(blocks);
+        var value = CalculateChecksum(blocks);
+
+        output.WriteLine($"Answer: {value}");
 
         Assert.Equal(expected, value);
     }
@@ -67,6 +77,14 @@ public class Day09(ITestOutputHelper output)
         }
     }
 
+    private long CalculateChecksum(IList<int> blocks)
+    {
+        var result = blocks.Select((id, index) => (id, index))
+                           .Where(pair => pair.id != EMPTY_BLOCK)
+                           .Sum(pair => (long)pair.id * pair.index);
+        return result;
+    }
+
     private void ShiftBlocksLeft(IList<int> blocks)
     {
         var freeIdx = 0;
@@ -86,14 +104,6 @@ public class Day09(ITestOutputHelper output)
                 fileIdx--;
             }
         }
-    }
-
-    private long CalculateChecksum(IList<int> blocks)
-    {
-        var result = blocks.Select((id, index) => (id, index))
-                           .Where(pair => pair.id != EMPTY_BLOCK)
-                           .Sum(pair => (long)pair.id * pair.index);
-        return result;
     }
 
     int GetNextFreeBlock(IList<int> blocks, int index)
@@ -121,11 +131,54 @@ public class Day09(ITestOutputHelper output)
         return -1;
     }
 
-    private void ShowDiskBlocks(IList<int> blocks)
+    private void ShiftWholeFilesLeft(IList<int> blocks)
     {
-        var aa = blocks.Select(v => v != EMPTY_BLOCK ? v.ToString() : ".");
-        var line = string.Join("", aa);
-        output.WriteLine(line);
+        var toMove = blocks.Select((id, index) => (id, index))
+                           .Where(p => p.id != EMPTY_BLOCK)
+                           .GroupBy(key => key.id,
+                                    val => val.index)
+                           .Select(g => (fileInfo: (id: g.Key, start: g.Min()), fileSize: g.Max() - g.Min() + 1))
+                           .OrderByDescending(p => p.fileInfo.start)
+                           .Select(x => (x.fileInfo.id, x.fileInfo.start, x.fileSize))
+                           .ToArray();
+
+
+        foreach (var (id, start, size) in toMove)
+        {
+            var (freeStart, freeSize) = GetFreeSpaceBlocks(blocks).FirstOrDefault(fs => fs.start < start && fs.size >= size);
+            if (freeSize != 0)
+            {
+                for (var i = 0; i < size; i++)
+                {
+                    blocks[freeStart + i] = blocks[start + i];
+                    blocks[start + i] = EMPTY_BLOCK;
+                }
+            }
+        }
     }
+
+    private static IOrderedEnumerable<(int start, int size)> GetFreeSpaceBlocks(IList<int> blocks)
+    {
+        return blocks.Select((id, index) => (id, index))
+                     .Where(p => p.id == EMPTY_BLOCK)
+                     .Aggregate(new List<List<int>>(),
+                                (result, item) =>
+                                {
+                                    if (result.Count == 0 || blocks[item.index - 1] != item.id)
+                                        result.Add(new List<int> { item.index });
+                                    else
+                                        result.Last().Add(item.index);
+                                    return result;
+                                })
+                     .Select(x => (start: x.Min(), size: x.Count()))
+                     .OrderBy(x => x.start);
+    }
+
+    //private void ShowDiskBlocks(int x, IList<int> blocks)
+    //{
+    //    var aa = blocks.Select(v => v != EMPTY_BLOCK ? v.ToString() : ".");
+    //    var line = string.Join("", aa);
+    //    output.WriteLine($"{x,3}: {line}");
+    //}
 
 }
